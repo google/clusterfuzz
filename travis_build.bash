3b83281882b72c688e9955fe05751a55800916b9
Oliver Chang <ochang@google.com>
2019-02-18 15:24:46 +1100

    try fix tests

--- travis_build.bash
+++ travis_build.bash
@@ -2,6 +2,6 @@
 
 export IMAGE=gcr.io/clusterfuzz-images/ci
 
-docker run -ti --rm -e TRAVIS_BRANCH=$TRAVIS_BRANCH -v $TRAVIS_BUILD_DIR:/workspace $IMAGE setup
+docker run -ti --rm -v $TRAVIS_BUILD_DIR:/workspace $IMAGE setup
 docker run -ti --rm -e TRAVIS_BRANCH=$TRAVIS_BRANCH -v $TRAVIS_BUILD_DIR:/workspace $IMAGE python butler.py lint
-docker run -ti --rm -v $TRAVIS_BUILD_DIR:/workspace $IMAGE local/tests/run_tests
+docker run -ti --rm --privileged --cap-add=all --v $TRAVIS_BUILD_DIR:/workspace $IMAGE local/tests/run_tests
