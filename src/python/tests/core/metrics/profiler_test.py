cedeaa5d092d080a5e7191c8569c4f28f71bb3f9
Abhishek Arya <inferno@chromium.org>
2019-03-07 10:42:33 -0800

    Fix code typo.

--- src/python/tests/core/metrics/profiler_test.py
+++ src/python/tests/core/metrics/profiler_test.py
@@ -38,7 +38,7 @@ class ProfilerTest(unittest.TestCase):
     helpers.patch(self, [
         'googlecloudprofiler.backoff.Backoff.next_backoff',
         'googlecloudprofiler.client.Client.setup_auth',
-        'googlecloudprofiler.client.Client._build_serivce',
+        'googlecloudprofiler.client.Client._build_service',
         'googlecloudprofiler.client.Client._create_profile',
         'googlecloudprofiler.client.Client._collect_and_upload_profile',
     ])
@@ -46,7 +46,7 @@ class ProfilerTest(unittest.TestCase):
     # Time in seconds used in a sleep call inside profiler loop.
     self.mock.next_backoff.return_value = 0.005
     self.mock.setup_auth.return_value = 'project_id'
-    self.mock._build_serivce.return_value = None  # pylint: disable=protected-access
+    self.mock._build_service.return_value = None  # pylint: disable=protected-access
     self.mock._create_profile.return_value = self.fake_profile  # pylint: disable=protected-access
 
   def test_profiler(self):
