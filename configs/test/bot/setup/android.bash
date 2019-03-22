3817a08c6bbf99d3d82a5e71289932cb735f9ca6
mbarbella-chromium <41697236+mbarbella-chromium@users.noreply.github.com>
2019-03-22 12:20:37 -0700

    Add an Android startup script for open source users. (#292)

--- configs/test/bot/setup/android.bash
+++ configs/test/bot/setup/android.bash
@@ -23,34 +23,24 @@ if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
   exit 1
 fi
 
+if [ -z "$ANDROID_SERIAL" ]; then
+  echo "\$ANDROID_SERIAL is not set."
+  exit 1
+fi
+
 NFS_ROOT=  # Fill in NFS information if available.
 APPENGINE=google_appengine
 APPENGINE_FILE=google_appengine_1.9.75.zip
 GOOGLE_CLOUD_SDK=google-cloud-sdk
-GOOGLE_CLOUD_SDK_ARCHIVE=google-cloud-sdk-232.0.0-darwin-x86_64.tar.gz
+GOOGLE_CLOUD_SDK_ARCHIVE=google-cloud-sdk-232.0.0-linux-x86_64.tar.gz
 INSTALL_DIRECTORY=${INSTALL_DIRECTORY:-${HOME}}
 APPENGINE_DIR="$INSTALL_DIRECTORY/$APPENGINE"
 DEPLOYMENT_BUCKET="deployment.$CLOUD_PROJECT_ID.appspot.com"
 GSUTIL_PATH="$INSTALL_DIRECTORY/$GOOGLE_CLOUD_SDK/bin"
 ROOT_DIR="$INSTALL_DIRECTORY/clusterfuzz"
 PYTHONPATH="$PYTHONPATH:$APPENGINE_DIR:$ROOT_DIR/src"
-
-echo "Disabling macOS crash reporting (requires sudo)."
-sudo -u $USER bash -c "launchctl unload -w /System/Library/LaunchAgents/com.apple.ReportCrash.plist"
-sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.ReportCrash.Root.plist
-
-echo "Disabling kernel message logging (requires sudo)."
-sudo sysctl -w vm.shared_region_unnest_logging=0
-
-echo "Increasing default file limit (requires sudo)."
-sudo launchctl limit maxfiles 2048 unlimited
-sudo ulimit -n 4096
-
-echo "Installing ClusterFuzz package dependencies (requires sudo)."
-
-# pip may fail on some macOS versions if run without "--ignore-installed".
-# For more context, see https://github.com/pypa/pip/issues/3165.
-sudo pip install --ignore-installed crcmod==1.7 psutil==5.4.7 pyOpenSSL==19.0.0
+ADB_PATH="$INSTALL_DIRECTORY/platform-tools"
+PATH="$PATH:$ADB_PATH"
 
 echo "Creating directory $INSTALL_DIRECTORY."
 if [ ! -d "$INSTALL_DIRECTORY" ]; then
@@ -66,20 +56,11 @@ if [ ! -d "$INSTALL_DIRECTORY/$GOOGLE_CLOUD_SDK" ]; then
   rm $GOOGLE_CLOUD_SDK_ARCHIVE
 fi
 
-echo "Activating credentials with the Google Cloud SDK."
-$GSUTIL_PATH/gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
-
-echo "Specifying the proper Boto configuration file."
-
-# Otherwise, gsutil will error out due to multiple types of configured
-# credentials. For more information about this, see
-# https://cloud.google.com/storage/docs/gsutil/commands/config#configuration-file-selection-procedure
-BOTO_CONFIG_PATH=$($GSUTIL_PATH/gsutil -D 2>&1 | grep "config_file_list" | egrep -o "/[^']+gserviceaccount\.com/\.boto")
-
-if [ -f $BOTO_CONFIG_PATH ]; then
-  export BOTO_CONFIG="$BOTO_CONFIG_PATH"
-else
-  echo "WARNING: failed to identify the Boto configuration file and specify BOTO_CONFIG env."
+echo "Fetching Android platform tools for ADB."
+if [ ! -d "$ADB_PATH" ]; then
+  wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+  unzip $INSTALL_DIRECTORY/platform-tools-latest-linux.zip
+  rm $INSTALL_DIRECTORY/platform-tools-latest-linux.zip
 fi
 
 echo "Fetching Google App Engine SDK."
@@ -89,11 +70,35 @@ if [ ! -d "$INSTALL_DIRECTORY/$APPENGINE" ]; then
   rm $APPENGINE_FILE
 fi
 
+echo "Installing ClusterFuzz package dependencies."
+pip install crcmod==1.7 psutil==5.4.7 pyOpenSSL==19.0.0
+
+echo "Ensuring device is connected."
+if [[ $("$ADB_PATH/adb" -s "$ANDROID_SERIAL" get-state) != "device" ]]; then
+  echo "Device $ANDROID_SERIAL is not connected."
+  exit 1
+fi
+
+echo "Activating credentials with the Google Cloud SDK."
+$GSUTIL_PATH/gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+
+# Otherwise, gsutil will error out due to multiple types of configured
+# credentials. For more information about this, see
+# https://cloud.google.com/storage/docs/gsutil/commands/config#configuration-file-selection-procedure
+echo "Specifying the proper Boto configuration file."
+BOTO_CONFIG_PATH=$($GSUTIL_PATH/gsutil -D 2>&1 | grep "config_file_list" | egrep -o "/[^']+gserviceaccount\.com/\.boto") || true
+if [ -f $BOTO_CONFIG_PATH ]; then
+  export BOTO_CONFIG="$BOTO_CONFIG_PATH"
+else
+  echo "WARNING: failed to identify the Boto configuration file and specify BOTO_CONFIG env."
+fi
+
 echo "Downloading ClusterFuzz source code."
-$GSUTIL_PATH/gsutil cp gs://$DEPLOYMENT_BUCKET/macos.zip clusterfuzz-source.zip
+rm -rf clusterfuzz
+$GSUTIL_PATH/gsutil cp gs://$DEPLOYMENT_BUCKET/linux.zip clusterfuzz-source.zip
 unzip -q clusterfuzz-source.zip
 
 echo "Running ClusterFuzz."
-NFS_ROOT="$NFS_ROOT" GOOGLE_APPLICATION_CREDENTIALS="$GOOGLE_APPLICATION_CREDENTIALS" ROOT_DIR="$ROOT_DIR" PYTHONPATH="$PYTHONPATH" GSUTIL_PATH="$GSUTIL_PATH" python $ROOT_DIR/src/python/bot/startup/run.py &
+OS_OVERRIDE="ANDROID" ANDROID_SERIAL="$ANDROID_SERIAL" PATH="$PATH" NFS_ROOT="$NFS_ROOT" GOOGLE_APPLICATION_CREDENTIALS="$GOOGLE_APPLICATION_CREDENTIALS" ROOT_DIR="$ROOT_DIR" PYTHONPATH="$PYTHONPATH" GSUTIL_PATH="$GSUTIL_PATH" python $ROOT_DIR/src/python/bot/startup/run.py &
 
 echo "Success!"
