google/clusterfuzz

commit 46643f0954cfff32a3685e6cbd6c8bb6b3157002

Don't mark builds bad when they should be ignored. (#357)
* Also, remove unneeded crash_state argument from
ignore_stacktrace since it is now always set and
defaults to 'NULL'.
* Don't store result in DB if stack has to be ignored.

src/python/fuzzing/tests.py
@@ -25,7 +25,6 @@ from build_management import revisions
 from crash_analysis import crash_analyzer
 from crash_analysis.crash_comparer import CrashComparer
 from crash_analysis.crash_result import CrashResult
-from crash_analysis.stack_parsing import stack_symbolizer
 from datastore import data_handler
 from datastore import data_types
 from metrics import fuzzer_logs
@@ -917,31 +916,33 @@ def check_for_bad_build(job_type, crash_revision):
   if default_window_argument:
     command = command.replace(' %s' % default_window_argument, '')
 
-  # Warmup timeout.
-  fast_warmup_timeout = environment.get_value('FAST_WARMUP_TIMEOUT')
-
   # TSAN is slow, and boots slow on first startup. Increase the warmup
   # timeout for this case.
   if environment.tool_matches('TSAN', job_type):
     fast_warmup_timeout = environment.get_value('WARMUP_TIMEOUT')
+  else:
+    fast_warmup_timeout = environment.get_value('FAST_WARMUP_TIMEOUT')
 
   # Initialize helper variables.
   is_bad_build = False
   build_run_console_output = ''
-  output = ''
   app_directory = environment.get_value('APP_DIR')
 
-  # Check if the build is bad.
+  # Exit all running instances.
   process_handler.terminate_stale_application_instances()
-  exit_code, _, output = process_handler.run_process(
+
+  # Check if the build is bad.
+  return_code, crash_time, output = process_handler.run_process(
       command,
       timeout=fast_warmup_timeout,
       current_working_directory=app_directory)
-  output = utils.decode_to_unicode(output)
-  if crash_analyzer.is_crash(exit_code, output):
+  crash_result = CrashResult(return_code, crash_time, output)
+  if crash_result.is_crash() and not crash_result.should_ignore():
     is_bad_build = True
-    build_run_console_output = ('%s\n\n%s\n\n%s' % (
-        command, stack_symbolizer.symbolize_stacktrace(output), output))
+    build_run_console_output = utils.get_crash_stacktrace_output(
+        command,
+        crash_result.get_stacktrace(symbolized=True),
+        crash_result.get_stacktrace(symbolized=False))
     logs.log(
         'Bad build for %s detected at r%d.' % (job_type, crash_revision),
         output=build_run_console_output)
@@ -961,7 +962,8 @@ def check_for_bad_build(job_type, crash_revision):
 
   # If none of the other bots have added information about this build,
   # then add it now.
-  if build_state == data_types.BuildState.UNMARKED:
+  if (build_state == data_types.BuildState.UNMARKED and
+      not crash_result.should_ignore()):
     data_handler.add_build_metadata(job_type, crash_revision, is_bad_build,
                                     build_run_console_output)
 

src/python/bot/tasks/analyze_task.py
@@ -317,8 +317,7 @@ def execute_task(testcase_id, job_type):
                                        log_message)
 
   # See if we have to ignore this crash.
-  if crash_analyzer.ignore_stacktrace(state.crash_state,
-                                      state.crash_stacktrace):
+  if crash_analyzer.ignore_stacktrace(state.crash_stacktrace):
     close_invalid_testcase_and_update_status(testcase, metadata, 'Irrelavant')
     return
 

src/python/bot/tasks/corpus_pruning_task.py
@@ -430,7 +430,7 @@ class CorpusPruner(object):
       unit_path = self._quarantine_unit(unit_path, quarantine_corpus_path)
       num_bad_units += 1
 
-      if crash_analyzer.ignore_stacktrace(state.crash_state, state.crash_type):
+      if crash_analyzer.ignore_stacktrace(state.crash_stacktrace):
         continue
 
       # Local de-duplication.

src/python/bot/tasks/fuzz_task.py
@@ -136,7 +136,7 @@ class Crash(object):
     self.key = '%s,%s,%s' % (self.crash_type, self.crash_state,
                              self.security_flag)
     self.should_be_ignored = crash_analyzer.ignore_stacktrace(
-        self.crash_state, self.crash_stacktrace)
+        state.crash_stacktrace)
 
     # self.crash_info gets populated in create_testcase; save what we need.
     self.crash_frames = state.frames

src/python/bot/tasks/symbolize_task.py
@@ -105,8 +105,7 @@ def execute_task(testcase_id, job_type):
         state = crash_result.get_symbolized_data()
         security_flag = crash_result.is_security_issue()
 
-        if (not crash_analyzer.ignore_stacktrace(state.crash_state,
-                                                 state.crash_stacktrace) and
+        if (not crash_analyzer.ignore_stacktrace(state.crash_stacktrace) and
             security_flag == testcase.security_flag and
             state.crash_type == testcase.crash_type and
             (state.crash_type != sym_crash_type or
@@ -221,8 +220,7 @@ def get_symbolized_stacktraces(testcase_file_path, testcase,
       if crash_result.is_crash():
         state = crash_result.get_symbolized_data()
 
-        if crash_analyzer.ignore_stacktrace(state.crash_state,
-                                            state.crash_stacktrace):
+        if crash_analyzer.ignore_stacktrace(state.crash_stacktrace):
           continue
 
         unsymbolized_crash_stacktrace = crash_result.get_stacktrace(
@@ -249,8 +247,7 @@ def get_symbolized_stacktraces(testcase_file_path, testcase,
       if crash_result.is_crash():
         state = crash_result.get_symbolized_data()
 
-        if crash_analyzer.ignore_stacktrace(state.crash_state,
-                                            state.crash_stacktrace):
+        if crash_analyzer.ignore_stacktrace(state.crash_stacktrace):
           continue
 
         if state.crash_state != expected_state:

src/python/crash_analysis/crash_analyzer.py
@@ -112,16 +112,13 @@ def has_marker(stacktrace, marker_list):
   return False
 
 
-def ignore_stacktrace(crash_state, crash_stacktrace):
+def ignore_stacktrace(crash_stacktrace):
   """Return whether the stacktrace needs to be ignored."""
   # Filter crash based on search include / exclude string pattern.
   search_includes = environment.get_value('SEARCH_INCLUDES')
   search_excludes = environment.get_value('SEARCH_EXCLUDES')
   if ((search_includes and not re.search(search_includes, crash_stacktrace)) or
       (search_excludes and re.search(search_excludes, crash_stacktrace))):
-    return True
-
-  if not crash_state:
     return True
 
   crash_exclusions = environment.get_value('CRASH_EXCLUSIONS')

src/python/crash_analysis/crash_result.py
@@ -80,8 +80,7 @@ class CrashResult(object):
   def should_ignore(self):
     """Return True if this crash should be ignored."""
     state = self.get_symbolized_data()
-    return crash_analyzer.ignore_stacktrace(state.crash_state,
-                                            state.crash_stacktrace)
+    return crash_analyzer.ignore_stacktrace(state.crash_stacktrace)
 
   def is_security_issue(self):
     """Return True if this crash is a security issue."""

src/python/tests/core/bot/tasks/fuzz_task_test.py
@@ -371,7 +371,7 @@ class CrashInitTest(fake_filesystem_unittest.TestCase):
     self.assertEqual('type,state,%s' % security_flag, crash.key)
 
     self.assertEqual(should_be_ignored, crash.should_be_ignored)
-    self.mock.ignore_stacktrace.assert_called_once_with('state', 'trace')
+    self.mock.ignore_stacktrace.assert_called_once_with('orig_trace')
 
     self.assertFalse(hasattr(crash, 'fuzzed_key'))
     return crash

commit 08a91250799101d3531c403384569a749e36fd0f

Use crash_result.is_crash since it ignores empty crash state.
This is a regression from
https://github.com/google/clusterfuzz/commit/46643f0954cfff32a3685e6cbd6c8bb6b3157002#diff-6bbe1ef1dddc4f3eedadcee180e3e41f
but we should have done this properly in first place
using crash_result. This also avoid filling up the crash
queue with such crashes which should have been ignored
in the first place.

src/python/fuzzing/tests.py
@@ -436,7 +436,8 @@ def run_testcase_and_return_result_in_queue(crash_queue,
 
     # Analyze the crash.
     crash_output = _get_crash_output(output)
-    if crash_analyzer.is_crash(return_code, crash_output):
+    crash_result = CrashResult(return_code, crash_time, crash_output)
+    if crash_result.is_crash():
       # Initialize resource list with the testcase path.
       resource_list = [file_path]
       resource_list += get_resource_paths(crash_output)
@@ -463,9 +464,10 @@ def run_testcase_and_return_result_in_queue(crash_queue,
       if upload_output:
         upload_testcase(file_path)
 
-    crash_result = CrashResult(return_code, crash_time, output)
     if upload_output:
-      upload_testcase_output(crash_result, file_path)
+      # Include full output for uploaded logs (crash output, merge output, etc).
+      crash_result_full = CrashResult(return_code, crash_time, output)
+      upload_testcase_output(crash_result_full, file_path)
   except Exception:
     logs.log_error('Exception occurred while running '
                    'run_testcase_and_return_result_in_queue.')