google/clusterfuzz

commit 6d9f9e76a1bb37a8c2a5cdd451b14dec4e58dd4a

Part 1: Py2->3 Futurize stage 2 without str-unicode, urllib changes (#406) (#411)

src/python/bot/fuzzers/afl/stats.py
@@ -16,6 +16,7 @@ from __future__ import print_function
 
 import os
 import re
+import six
 
 from bot.fuzzers import dictionary_manager
 from bot.fuzzers import engine_common
@@ -192,12 +193,12 @@ class StatsGetter(object):
     # Read and parse stats from AFL's afl_stats. Then use them to set and
     # calculate our own stats.
     self.set_afl_stats()
-    for afl_stat, clusterfuzz_stat in self.AFL_STATS_MAPPING.iteritems():
+    for afl_stat, clusterfuzz_stat in six.iteritems(self.AFL_STATS_MAPPING):
       self.stats[clusterfuzz_stat] = self.get_afl_stat(afl_stat)
 
     try:
-      self.stats['average_exec_per_sec'] = int(
-          self.get_afl_stat('execs_done') / actual_duration)
+      self.stats['average_exec_per_sec'] = (
+          self.get_afl_stat('execs_done') // actual_duration)
 
     except ZeroDivisionError:  # Fail gracefully if actual_duration is 0.
       self.stats['average_exec_per_sec'] = 0

commit caa6f401edcda286908a39bd5155253daa3603a6

Fix AFL stat upload to Bigquery.
This is regression from
https://github.com/google/clusterfuzz/commit/6d9f9e76a1bb37a8c2a5cdd451b14dec4e58dd4a
a // b is float when b is a float.

src/python/bot/fuzzers/afl/stats.py
@@ -197,7 +197,7 @@ class StatsGetter(object):
       self.stats[clusterfuzz_stat] = self.get_afl_stat(afl_stat)
 
     try:
-      self.stats['average_exec_per_sec'] = (
+      self.stats['average_exec_per_sec'] = int(
           self.get_afl_stat('execs_done') // actual_duration)
 
     except ZeroDivisionError:  # Fail gracefully if actual_duration is 0.