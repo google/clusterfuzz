4c98cd170f23db0199b37f727532ddfd7373fa8f
Oliver Chang <ochang@google.com>
2019-05-21 10:47:16 -0700

    fix print

--- src/python/platforms/fuchsia/device.py
+++ src/python/platforms/fuchsia/device.py
@@ -123,8 +123,9 @@ def initialize_resources_dir():
         'Could not find path for remote'
         'Fuchsia resources bucket (FUCHSIA_RESOURCES_URL')
 
-    import sys
-    print('resources_dir', fuchsia_resources_dir, file=sys.stderr)
+  import sys
+  print('resources_dir', fuchsia_resources_dir, file=sys.stderr)
+
   gsutil_command_arguments = [
       '-m', 'cp', '-r', fuchsia_resources_url, fuchsia_resources_dir
   ]
