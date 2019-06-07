08e168358da50508e188c3e955861f88f7d78e19
Martin Barbella <mbarbella@chromium.org>
2019-06-07 13:38:11 -0700

    Actually set root dir

--- src/local/butler/reproduce.py
+++ src/local/butler/reproduce.py
@@ -65,6 +65,7 @@ def _prepare_environment(testcase, build_directory):
   # bot and configuration directories nested under it.
   old_root_dir = environment.get_value('ROOT_DIR')
   root_dir = tempfile.mkdtemp()
+  environment.set_value('ROOT_DIR', root_dir)
   shutil.copytree(
       os.path.join(old_root_dir, 'bot'), os.path.join(root_dir, 'bot'))
   shutil.copytree(
