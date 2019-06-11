70f9bbdc05ff62098313616e5122eb74e9e89121
Abhishek Arya <inferno@chromium.org>
2019-06-10 18:26:19 -0700

    Verify core_pattern setting for AFL in launcher. (#546)
    * Verify core_pattern setting for AFL in launcher.
    
    Fuzzer.py code runs on host bot, so it does not work in OSS-Fuzz.
    A project code might lead to core_pattern file change, so try
    verifying and change core_pattern to correct value in launcher
    itself (since we run in docker and have sudoer).

--- src/python/bot/fuzzers/afl/fuzzer.py
+++ src/python/bot/fuzzers/afl/fuzzer.py
@@ -18,7 +18,6 @@ import os
 
 from base import utils
 from bot.fuzzers import builtin
-from metrics import logs
 from system import environment
 
 # The name of the file that run.py puts in the input directory so AFL doesn't
@@ -46,23 +45,7 @@ class Afl(builtin.EngineFuzzer):
     """Generate arguments for fuzzer using .options file or default values."""
     return ''
 
-  def _check_system_settings(self):
-    """Check system settings required for AFL."""
-    kernel_core_pattern_file_path = '/proc/sys/kernel/core_pattern'
-    if (os.path.exists(kernel_core_pattern_file_path) and
-        open(kernel_core_pattern_file_path).read().strip() != 'core'):
-      logs.log_fatal_and_exit('AFL needs core_pattern to be set to core.')
-
-    cpu_scaling_file_path = (
-        '/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor')
-    if (os.path.exists(cpu_scaling_file_path) and
-        open(cpu_scaling_file_path).read().strip() != 'performance'):
-      logs.log_warn('For optimal AFL performance, '
-                    'set on-demand cpu scaling to performance.')
-
   def run(self, input_directory, output_directory, no_of_files):
-    self._check_system_settings()
-
     result = super(Afl, self).run(input_directory, output_directory,
                                   no_of_files)
 
