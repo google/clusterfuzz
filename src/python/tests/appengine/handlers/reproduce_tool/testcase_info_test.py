fb50131d345d41d5ee5a4b0d0507ea6f3f32dabb
Martin Barbella <mbarbella@chromium.org>
2019-06-13 09:39:06 -0700

    Add a new test case info handler for the reproduce tool.
    This handler includes the full serialized test case object as well as
    the job definition associated with it.

---
+++ src/python/tests/appengine/handlers/reproduce_tool/testcase_info_test.py
@@ -0,0 +1,49 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for the reproduce tool testcase_info handler."""
+# pylint: disable=protected-accesss
+
+import unittest
+
+from datastore import data_types
+from handlers.reproduce_tool import testcase_info
+from tests.test_libs import test_utils
+
+@test_utils.with_cloud_emulators('datastore')
+class PrepareTestcaseDictTest(unittest.TestCase):
+  """Tests for _prepare_testcase_dict."""
+
+  def setUp(self):
+    job = data_types.Job(name='test_job', environment_string='X = 1\nY = 2\n')
+    job.put()
+
+    testcase = data_types.Testcase()
+    testcase.status = 'Pending'
+    testcase.open = True
+    testcase.job_type = 'test_job'
+    testcase.put()
+
+    self.testcase = testcase
+
+  def test_expected_properties_included(self):
+    """Ensure that a few of the common test case properties are included."""
+    result = testcase_info._prepare_testcase_dict(self.testcase)
+    self.assertEquals(result['status'], 'Pending')
+    self.assertEquals(result['open'], True)
+    self.assertEquals(result['group_id'], 0)
+
+  def test_job_included(self):
+    """Ensure that the job definition has been included."""
+    result = testcase_info._prepare_testcase_dict(self.testcase)
+    self.assertEquals(result['job_definition'], {'X': '1', 'Y': '2'})
