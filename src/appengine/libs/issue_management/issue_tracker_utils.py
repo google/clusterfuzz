1a793c6f5ffe65709c42501d41930a84ce51eb57
Oliver Chang <ochang@google.com>
2019-06-21 12:26:33 +1000

    Call an init function from config modules.
    Modules can define a "module_init" module containing a "appengine"
    function which is called on App Engine startup.
    
    Also expose a way to register issue tracker implementations.

--- src/appengine/libs/issue_management/issue_tracker_utils.py
+++ src/appengine/libs/issue_management/issue_tracker_utils.py
@@ -22,6 +22,14 @@ from libs.issue_management import monorail
 from metrics import logs
 
 _ISSUE_TRACKER_CACHE_CAPACITY = 8
+_ISSUE_TRACKER_CONSTRUCTORS = {
+    'monorail': monorail.get_issue_tracker,
+}
+
+
+def register_issue_tracker(tracker_type, constructor):
+  """Register an issue tracker implementation."""
+  _ISSUE_TRACKER_CONSTRUCTORS[tracker_type] = constructor
 
 
 def _get_issue_tracker_project_name(testcase=None):
@@ -43,10 +51,12 @@ def get_issue_tracker(project_name=None):
   if not issue_project_config:
     raise ValueError('Issue tracker for {} does not exist'.format(project_name))
 
-  if issue_project_config['type'] == 'monorail':
-    return monorail.get_issue_tracker(project_name)
+  constructor = _ISSUE_TRACKER_CONSTRUCTORS.get(issue_project_config['type'])
+  if not constructor:
+    raise ValueError('Invalid issue tracker type ' +
+                     issue_project_config['type'])
 
-  raise ValueError('Invalid issue tracker type ' + issue_project_config['type'])
+  return constructor(project_name)
 
 
 def get_issue_tracker_for_testcase(testcase):
