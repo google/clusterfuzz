42207298e77ad98b7cc7f1e72e9ccc4139d04dc4
Martin Barbella <mbarbella@chromium.org>
2019-07-09 14:56:42 -0700

    Remove unicode hack.

--- src/appengine/handlers/viewer.py
+++ src/appengine/handlers/viewer.py
@@ -54,11 +54,11 @@ class Handler(base_handler.Handler):
     # https://github.com/googleapis/google-cloud-python/issues/6572
     if blob_size:
       try:
-        content = unicode(blobs.read_key(key), errors='replace')
+        content = blobs.read_key(key)
       except Exception:
         raise helpers.EarlyExitException('Failed to read content.', 400)
     else:
-      content = u''
+      content = ''
 
     line_count = len(content.splitlines())
     size = len(content)
