fd4d59fa62e8ca4ef4761dfc5a071da62bb71115
Oliver Chang <ochang@google.com>
2019-07-24 18:29:39 +1000

    fix tests

--- src/python/bot/fuzzers/libfuzzer.py
+++ src/python/bot/fuzzers/libfuzzer.py
@@ -553,8 +553,10 @@ def get_runner(fuzzer_path, temp_dir=None):
   build_dir = environment.get_value('BUILD_DIR')
   dataflow_build_dir = environment.get_value('DATAFLOW_BUILD_DIR')
 
-  # To ensure that we can run the fuzz target.
-  os.chmod(fuzzer_path, stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP)
+  is_fuchsia = environment.platform() == 'FUCHSIA'
+  if not is_fuchsia:
+    # To ensure that we can run the fuzz target.
+    os.chmod(fuzzer_path, stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP)
 
   if use_minijail:
     # Set up chroot and runner.
@@ -597,7 +599,7 @@ def get_runner(fuzzer_path, temp_dir=None):
       shutil.copy(os.path.realpath('/bin/sh'), os.path.join(minijail_bin, 'sh'))
 
     runner = MinijailLibFuzzerRunner(fuzzer_path, minijail_chroot)
-  elif environment.platform() == 'FUCHSIA':
+  elif is_fuchsia:
     runner = FuchsiaQemuLibFuzzerRunner(fuzzer_path)
   else:
     runner = LibFuzzerRunner(fuzzer_path)
