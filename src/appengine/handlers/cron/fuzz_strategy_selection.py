b35f79e6b2684e37dfa710a2b3f2fa6117816012
Mukund Venkateswaran <mukundv@google.com>
2019-07-30 15:54:47 -0700

    Fixed comments.

--- src/appengine/handlers/cron/fuzz_strategy_selection.py
+++ src/appengine/handlers/cron/fuzz_strategy_selection.py
@@ -228,7 +228,7 @@ def _query_and_upload_strategy_probabilities(engine):
   are based on new_edges feature."""
   strategy_data = []
   data = _query_multi_armed_bandit_probabilities(engine)
-  logs.log("Queried distribution for " + engine.name)
+  logs.log("Queried distribution for {}.".format(engine.name))
 
   # TODO(mukundv): Update once we choose a temperature parameter for final
   # implementation.
@@ -249,9 +249,10 @@ def _query_and_upload_strategy_probabilities(engine):
           data_types.FuzzStrategyProbability)
   ])
   ndb.put_multi(strategy_data)
-  logs.log("Uploaded queried distribution to ndb for " + engine.name)
+  logs.log("Uploaded queried distribution to ndb for {}".format(engine.name))
   _store_probabilities_in_bigquery(engine, data)
-  logs.log("Uploaded queried distribution to BigQuery for " + engine.name)
+  logs.log("Uploaded queried distribution to BigQuery for {}".format(
+      engine.name))
 
 
 class Handler(base_handler.Handler):
