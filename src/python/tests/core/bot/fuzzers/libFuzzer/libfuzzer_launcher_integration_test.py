a420a71b4c4f38ea2e5e1a679cce3307ad6075ac
Julia Hansbrough <flowerhack@google.com>
2019-08-12 22:12:37 -0700

    last tweak

--- src/python/tests/core/bot/fuzzers/libFuzzer/libfuzzer_launcher_integration_test.py
+++ src/python/tests/core/bot/fuzzers/libFuzzer/libfuzzer_launcher_integration_test.py
@@ -774,9 +774,10 @@ class TestLauncherFuchsia(BaseLauncherTest):
 
   def tearDown(self):
     shutil.rmtree(self.tmp_resources_dir, ignore_errors=True)
+
   @skipIf(
-    True,
-    'Temporarily disabling the Fuchsia integration test until build size reduced.'
+      True,
+      'Temporarily disabling the Fuchsia integration test until build size reduced.'
   )
   def test_fuzzer_can_boot_and_run(self):
     """Tests running a single round of fuzzing on a Fuchsia target, using
