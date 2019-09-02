google/clusterfuzz

commit 768cedb5649125f9b7111f99f3d9d09d2c1e2385

Recreate FUZZ_DATA as well in shell.clear_data_directories. (#896)

src/python/system/shell.py
@@ -21,6 +21,7 @@ import subprocess
 import sys
 import tempfile
 
+from base import persistent_cache
 from metrics import logs
 from system import environment
 
@@ -104,6 +105,8 @@ def clear_data_directories():
   clear_testcase_directories()
   clear_mutator_plugins_directory()
 
+  persistent_cache.clear_values()
+
 
 def clear_data_directories_on_low_disk_space():
   """Clear all data directories on low disk space. This should ideally never
@@ -175,6 +178,7 @@ def clear_testcase_directories():
   """Clears the testcase directories."""
   remove_directory(environment.get_value('FUZZ_INPUTS'), recreate=True)
   remove_directory(environment.get_value('FUZZ_INPUTS_DISK'), recreate=True)
+  remove_directory(environment.get_value('FUZZ_DATA'), recreate=True)
 
   if environment.platform() == 'ANDROID':
     from platforms import android

src/python/base/persistent_cache.py
@@ -20,7 +20,6 @@ import os
 from base import json_utils
 from metrics import logs
 from system import environment
-from system import shell
 
 # For any given value file, if a file with the same name with this added
 # extension exists, it is not cleared during initialization.
@@ -34,7 +33,7 @@ def initialize():
   if os.path.exists(cache_directory_path):
     clear_values()
   else:
-    shell.create_directory(cache_directory_path, create_intermediates=True)
+    os.makedirs(cache_directory_path)
 
 
 def clear_values():
@@ -59,7 +58,7 @@ def clear_values():
 def delete_value(key):
   """Removes the value for a key."""
   value_path = get_value_file_path(key)
-  shell.remove_file(value_path)
+  os.remove(value_path)
 
 
 def get_value(key, default_value=None, constructor=None):

commit 2a17e589932885354cb5fe2c35246e7171202f05

Move clearing of common data bundles directory to its own function.
clear_testcase_directories is called before start of every task,
so it clears the downloaded web tests bundle evertime. This is
a regression from #896.
Also, add a test for persistent_cache's clear_values function
with clear_all=True. This is not related to fix, but good to have.

src/python/system/shell.py
@@ -84,6 +84,11 @@ def clear_crash_stacktraces_directory():
       environment.get_value('CRASH_STACKTRACES_DIR'), recreate=True)
 
 
+def clear_common_data_bundles_directory():
+  """Clear the common data bundle directory."""
+  remove_directory(environment.get_value('FUZZ_DATA'), recreate=True)
+
+
 def clear_data_bundles_directory():
   """Clears the data bundles directory."""
   remove_directory(environment.get_value('DATA_BUNDLES_DIR'), recreate=True)
@@ -99,6 +104,7 @@ def clear_data_directories():
   clear_build_directory()
   clear_build_urls_directory()
   clear_crash_stacktraces_directory()
+  clear_common_data_bundles_directory()
   clear_data_bundles_directory()
   clear_fuzzers_directories()
   clear_temp_directory()
@@ -178,7 +184,6 @@ def clear_testcase_directories():
   """Clears the testcase directories."""
   remove_directory(environment.get_value('FUZZ_INPUTS'), recreate=True)
   remove_directory(environment.get_value('FUZZ_INPUTS_DISK'), recreate=True)
-  remove_directory(environment.get_value('FUZZ_DATA'), recreate=True)
 
   if environment.platform() == 'ANDROID':
     from platforms import android

src/python/tests/core/base/persistent_cache_test.py
@@ -32,12 +32,12 @@ class PersistentCacheTest(fake_filesystem_unittest.TestCase):
     persistent_cache.initialize()
 
   def test_set_get_string(self):
-    """Ensure it works with string value"""
+    """Ensure it works with string value."""
     persistent_cache.set_value('key', 'value')
     self.assertEqual(persistent_cache.get_value('key'), 'value')
 
   def test_set_get_datetime(self):
-    """Ensure it works with datetime value"""
+    """Ensure it works with datetime value."""
     epoch = datetime.datetime.utcfromtimestamp(0)
     end_time = datetime.datetime.utcfromtimestamp(10)
     diff_time = end_time - epoch
@@ -47,17 +47,17 @@ class PersistentCacheTest(fake_filesystem_unittest.TestCase):
             'key', constructor=datetime.datetime.utcfromtimestamp), end_time)
 
   def test_delete(self):
-    """Ensure it deletes key"""
+    """Ensure it deletes key."""
     persistent_cache.set_value('key', 'value')
     persistent_cache.delete_value('key')
     self.assertEqual(persistent_cache.get_value('key'), None)
 
   def test_get_nonexistence(self):
-    """Ensure it returns default_value when key doesn't exists"""
+    """Ensure it returns default_value when key doesn't exists."""
     self.assertEqual(persistent_cache.get_value('key', default_value=1), 1)
 
   def test_get_invalid(self):
-    """Ensure it returns default_value when constructor fails"""
+    """Ensure it returns default_value when constructor fails."""
     time_now = datetime.datetime.utcnow()
     persistent_cache.set_value('key', 'random')
     self.assertEqual(persistent_cache.get_value('key'), 'random')
@@ -68,9 +68,17 @@ class PersistentCacheTest(fake_filesystem_unittest.TestCase):
             constructor=datetime.datetime.utcfromtimestamp), time_now)
 
   def test_persist_across_reboots(self):
-    """Ensure persist_across_reboots works"""
+    """Ensure persist_across_reboots works."""
     persistent_cache.set_value('key', 'a')
     persistent_cache.set_value('key2', 'b', persist_across_reboots=True)
     persistent_cache.clear_values()
     self.assertEqual(persistent_cache.get_value('key'), None)
     self.assertEqual(persistent_cache.get_value('key2'), 'b')
+
+  def test_clear_all(self):
+    """Ensure clear all works."""
+    persistent_cache.set_value('key', 'a')
+    persistent_cache.set_value('key2', 'b', persist_across_reboots=True)
+    persistent_cache.clear_values(clear_all=True)
+    self.assertEqual(persistent_cache.get_value('key'), None)
+    self.assertEqual(persistent_cache.get_value('key2'), None)