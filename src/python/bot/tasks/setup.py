5e9261185b09216b4defd6783195b77133a74276
Abhishek Arya <inferno@chromium.org>
2019-09-23 13:57:00 -0700

    Make variant task work on AFL with libFuzzer testcases.
    This regressed with new libFuzzer engine impl which does not
    have %TESTCASE% in app arguments.
    
    Fixes https://github.com/google/clusterfuzz/issues/1027.

--- src/python/bot/tasks/setup.py
+++ src/python/bot/tasks/setup.py
@@ -120,6 +120,13 @@ def _get_application_arguments(testcase, task_name):
     if app_args:
       app_args += ' '
     app_args += job_args
+
+  # TODO(aarya): Add %TESTCASE% explicitly since it will not exist with new
+  # engine impl libFuzzer testcases and AFL's launcher.py requires it as the
+  # first argument. Remove once AFL is migrated to the new engine impl.
+  if environment.is_afl_job(testcase.job_type) and not '%TESTCASE%' in app_args:
+    app_args = '%TESTCASE% ' + app_args
+
   return app_args
 
 
