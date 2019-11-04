193fbdce27510923097d8a956845d01051081100
Martin Barbella <mbarbella@chromium.org>
2019-11-04 14:56:44 -0800

    Update comment and variable names for automatic Android deps install

--- reproduce.sh
+++ reproduce.sh
@@ -17,26 +17,26 @@
 CLUSTERFUZZ_CONFIG_DIR=~/.config/clusterfuzz
 ROOT_DIRECTORY=$(dirname $(readlink -f "$0"))
 
-# Process command line arguments.
+# If we're using the emulator, make sure we install the Android SDK.
 original_args="$*"
 while [ "$1" != "" ]; do
   case $1 in
     -e)
-      emulator_install_flag=--install-android-emulator
+      additional_deps_args=--install-android-emulator
       ;;
     --emulator)
-      emulator_install_flag=--install-android-emulator
+      additional_deps_args=--install-android-emulator
       ;;
   esac
   shift
 done
 
 mkdir -p $CLUSTERFUZZ_CONFIG_DIR
-if [ ! -d $ROOT_DIRECTORY/ENV ] || ([ $emulator_install_flag ] && [ ! -d $ROOT_DIRECTORY/local/bin/android-sdk ]); then
+if [ ! -d $ROOT_DIRECTORY/ENV ] || ([ $additional_deps_args ] && [ ! -d $ROOT_DIRECTORY/local/bin/android-sdk ]); then
   echo "Running first time setup. This may take a while, but is only required once."
   echo "You may see several password prompts to install required packages."
   sleep 5
-  $ROOT_DIRECTORY/local/install_deps.bash --only-reproduce $emulator_install_flag || { rm -rf $ROOT_DIRECTORY/ENV && exit 1; }
+  $ROOT_DIRECTORY/local/install_deps.bash --only-reproduce $additional_deps_args || { rm -rf $ROOT_DIRECTORY/ENV && exit 1; }
 fi
 
 source ENV/bin/activate
