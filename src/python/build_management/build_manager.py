48461ca856952c9fc39615bfce7c7098f53dc5ab
Oliver Chang <ochang@google.com>
2019-11-20 11:32:13 +1100

    Don't set FUZZ_TARGET_COUNT when FUZZ_TARGET is already set.
    In the case of split target builds,
    _set_random_fuzz_target_for_fuzzing_if_needed may be called twice: first
    to actually pick the target, and again when we defer to the regular
    build setup, which will incorrectly set the count as there is now only
    1.

--- src/python/build_management/build_manager.py
+++ src/python/build_management/build_manager.py
@@ -264,9 +264,6 @@ def _get_build_directory(bucket_path, job_name):
 
 def _set_random_fuzz_target_for_fuzzing_if_needed(fuzz_targets, target_weights):
   """Sets a random fuzz target for fuzzing."""
-  fuzz_targets = list(fuzz_targets)
-  environment.set_value('FUZZ_TARGET_COUNT', len(fuzz_targets))
-
   fuzz_target = environment.get_value('FUZZ_TARGET')
   if fuzz_target:
     logs.log('Use previously picked fuzz target %s for fuzzing.' % fuzz_target)
@@ -278,6 +275,9 @@ def _set_random_fuzz_target_for_fuzzing_if_needed(fuzz_targets, target_weights):
   if not fuzz_targets:
     logs.log_error('No fuzz targets found. Unable to pick random one.')
     return None
+
+  fuzz_targets = list(fuzz_targets)
+  environment.set_value('FUZZ_TARGET_COUNT', len(fuzz_targets))
 
   fuzz_target = fuzzer_selection.select_fuzz_target(fuzz_targets,
                                                     target_weights)
