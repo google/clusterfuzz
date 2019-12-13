7131f609a11b62038a0eb88d5c330e296d8dec31
Max Moroz <mmoroz@chromium.org>
2019-12-13 11:16:45 -0800

    Corpus pruning task: fix an exception in the result logging. (#1273)
    * Corpus pruning task: fix an exception in the result logging.
    
    * remove unnecessary line
    
    * lint

--- src/python/bot/tasks/corpus_pruning_task.py
+++ src/python/bot/tasks/corpus_pruning_task.py
@@ -502,16 +502,16 @@ class CrossPollinator(object):
                                            [self.context.shared_corpus_path],
                                            self.context.minimized_corpus_path,
                                            self.context.bad_units_path, timeout)
+      symbolized_output = stack_symbolizer.symbolize_stacktrace(result.logs)
+      logs.log(
+          'Shared corpus merge finished successfully.',
+          output=symbolized_output)
     except engine.TimeoutError as e:
       logs.log_error('Corpus pruning timed out while merging shared corpus: ' +
                      e.message)
     except engine.Error as e:
       raise CorpusPruningException(
           'Corpus pruning failed to merge shared corpus\n' + e.message)
-
-    symbolized_output = stack_symbolizer.symbolize_stacktrace(result.logs)
-    logs.log(
-        'Shared corpus merge finished successfully.', output=symbolized_output)
 
 
 def do_corpus_pruning(context, last_execution_failed, revision):
