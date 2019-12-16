cf3749857dd69aa09f009b32d2174d8e45e1b43a
mpherman2 <mitchel.herman26@gmail.com>
2019-12-16 15:38:23 -0800

    addressed comments

--- src/python/tests/core/bot/minimizer/js_minimizer_test.py
+++ src/python/tests/core/bot/minimizer/js_minimizer_test.py
@@ -11,20 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for the Antlr Tokenizer."""
+"""Tests for the Java Script minimizer."""
+
+import unittest
 
 from bot.minimizer import js_minimizer
 from bot.tokenizer import antlr_tokenizer
 from bot.tokenizer.grammars.JavaScriptLexer import JavaScriptLexer
 from tests.test_libs import helpers
-import unittest
 
 
 class JSMinimizerTest(unittest.TestCase):
   """Test for Javascript Minimizer"""
 
   def setUp(self):
-    self._tests_to_queue = []
+    self._hypotheses_tested = []
 
     helpers.patch_environ(self)
     helpers.patch(self, [
@@ -32,7 +33,6 @@ class JSMinimizerTest(unittest.TestCase):
         'bot.minimizer.minimizer.Testcase.process'
     ])
 
-    helpers.patch_environ(self)
     self.mock.prepare_test.side_effect = self._mock_prepare_test
     self.mock.process.side_effect = self._mock_process()
 
@@ -50,7 +50,7 @@ class JSMinimizerTest(unittest.TestCase):
     for index in hypothesis:
       txt += testcase.tokens[index]
 
-    self._tests_to_queue.append(txt)
+    self._hypotheses_tested.append(txt)
 
   def _mock_test_function(self):
     return True
@@ -61,7 +61,7 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertEqual(self._tests_to_queue, [])
+    self.assertEqual(self._hypotheses_tested, [])
 
   def test_if_hypothesis(self):
     """e.g.: if (statement_that_evaluates_to_true) { crash() } -> crash()"""
@@ -69,14 +69,14 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("if(boolean) {}", self._tests_to_queue)
+    self.assertIn("if(boolean) {}", self._hypotheses_tested)
 
   def test_try_catch_hypothesis(self):
     """e.g.: try { crash() } catch(e) {} -> crash()"""
     data = "try{ crash() } catch(e){ }"
     self._minimizer.minimize(data)
 
-    self.assertIn("try{} catch(e){ }", self._tests_to_queue)
+    self.assertIn("try{} catch(e){ }", self._hypotheses_tested)
 
   def test_handle_if_else(self):
     """Make sure the minimizer runs all of the other hypothesis cleanly on
@@ -85,10 +85,10 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("if(boolean) {}", self._tests_to_queue)
+    self.assertIn("if(boolean) {}", self._hypotheses_tested)
     self.assertIn("if(boolean) {} else { do_something_else }",
-                  self._tests_to_queue)
-    self.assertIn("if(boolean) {crash} else {}", self._tests_to_queue)
+                  self._hypotheses_tested)
+    self.assertIn("if(boolean) {crash} else {}", self._hypotheses_tested)
 
   def test_remove_function_call(self):
     """Test that it removes functions calls effectively"""
@@ -96,7 +96,7 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("function name(param1, param2){}", self._tests_to_queue)
+    self.assertIn("function name(param1, param2){}", self._hypotheses_tested)
 
   def test_handle_bracket_with_new_line(self):
     """Test for try/catch with extra whitespace"""
@@ -104,8 +104,8 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("try{}", self._tests_to_queue)
-    self.assertIn("try{}\ncatch(e){\n\n}", self._tests_to_queue)
+    self.assertIn("try{}", self._hypotheses_tested)
+    self.assertIn("try{}\ncatch(e){\n\n}", self._hypotheses_tested)
 
   def test_handle_function_with_new_lines(self):
     """Test for functions with extra whitespace"""
@@ -113,7 +113,8 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("function name(param1,\n\t\tparam2){}", self._tests_to_queue)
+    self.assertIn("function name(param1,\n\t\tparam2){}",
+                  self._hypotheses_tested)
 
   def test_remove_outer_paren(self):
     """e.g.: assertTrue(crash()); -> crash()"""
@@ -121,7 +122,7 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("assertTrue()", self._tests_to_queue)
+    self.assertIn("assertTrue()", self._hypotheses_tested)
 
   def test_remove_inside_paren(self):
     """Hypothesis is Remove everything between the parentheses.
@@ -130,7 +131,7 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("junk, more_junk", self._tests_to_queue)
+    self.assertIn("junk, more_junk", self._hypotheses_tested)
 
   def test_remove_paren_to_start_of_line(self):
     """e.g.: leftover_junk = (function() {
@@ -139,7 +140,7 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("leftover_junk = (function(){\n})", self._tests_to_queue)
+    self.assertIn("leftover_junk = (function(){\n})", self._hypotheses_tested)
 
   def test_remove_paren_with_attached_brackets(self):
     """e.g.: (function(global) { })(this);"""
@@ -147,7 +148,7 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("(function(global) { })(this)", self._tests_to_queue)
+    self.assertIn("(function(global) { })(this)", self._hypotheses_tested)
 
   def test_remove_left_of_comma(self):
     """e.g.: f(whatever, crash()) -> f(crash())"""
@@ -155,7 +156,7 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn("whatever,", self._tests_to_queue)
+    self.assertIn("whatever,", self._hypotheses_tested)
 
   def test_remove_right_of_comma(self):
     """e.g.: f(crash(),whatever) -> f(crash())"""
@@ -163,4 +164,4 @@ class JSMinimizerTest(unittest.TestCase):
 
     self._minimizer.minimize(data)
 
-    self.assertIn(", whatever", self._tests_to_queue)
+    self.assertIn(", whatever", self._hypotheses_tested)
