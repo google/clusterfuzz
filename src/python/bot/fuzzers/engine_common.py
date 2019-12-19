a17075c93bb02fa0d301e9d4d8686bf18be91837
Martin Barbella <mbarbella@chromium.org>
2019-12-19 10:48:00 -0800

    Fix another exception

--- src/python/bot/fuzzers/engine_common.py
+++ src/python/bot/fuzzers/engine_common.py
@@ -17,6 +17,7 @@ from __future__ import print_function
 from builtins import object
 from builtins import range
 from builtins import str
+from future import utils as future_utils
 
 import contextlib
 import glob
@@ -298,7 +299,10 @@ def find_fuzzer_path(build_directory, fuzzer_name):
     legacy_name_prefix = project_name + '_'
 
   fuzzer_filename = environment.get_executable_filename(fuzzer_name)
-  for root, _, files in os.walk(build_directory):
+  # TODO(mbarbella): Remove this after the Python 3 migration. If newstrs are
+  # passed to the os library, this can throw exceptions while attempting to
+  # encode file paths as unicode, as they are not guaranteed to be valid.
+  for root, _, files in os.walk(future_utils.native(str(build_directory))):
     for filename in files:
       if (legacy_name_prefix + filename == fuzzer_name or
           filename == fuzzer_filename):
