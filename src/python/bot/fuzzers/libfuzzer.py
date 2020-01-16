4a1b3ca0b94de74512d888324485f9fc8642e9b4
mpherman2 <mitchel.herman26@gmail.com>
2020-01-16 12:49:38 -0800

    In process radamsa only occurs on linux asan

--- src/python/bot/fuzzers/libfuzzer.py
+++ src/python/bot/fuzzers/libfuzzer.py
@@ -1507,8 +1507,9 @@ def use_radamsa_mutator_plugin(extra_env):
   """Decide whether to use Radamsa in process. If yes, add the path to the
   radamsa shared object to LD_PRELOAD in |extra_env| and return True."""
 
-  # Mutator Plugin doesn't work on windows
-  if environment.platform() == "WINDOWS":
+  # Radamsa will only work on LINUX ASAN jobs
+  if environment.platform() != "LINUX" or environment.get_value(
+      "MEMORY_TOOL") != "ASAN":
     return False
 
   radamsa_path = os.path.join(
