8c87af53325b806d5ad14cef6d03301ebdb83b50
Oliver Chang <ochang@google.com>
2020-01-30 16:00:22 +1100

    Remove App Engine python paths from bots.

--- src/python/base/modules.py
+++ src/python/base/modules.py
@@ -17,34 +17,6 @@
 import os
 import site
 import sys
-import warnings
-
-KNOWN_WARNING_SIGNATURES = [
-    # Known conflicts between datastore and datastore_v1 protos.
-    r'Conflict register for file.*is already defined in file',
-]
-
-
-def disable_known_module_warnings():
-  """Disable known and harmless module warnings."""
-  for message_regex in KNOWN_WARNING_SIGNATURES:
-    warnings.filterwarnings('ignore', message_regex)
-
-
-def _patch_appengine_modules_for_bots():
-  """Patch out App Engine reliant behaviour from bots."""
-  if os.getenv('SERVER_SOFTWARE'):
-    # Not applicable on App Engine.
-    return
-
-  # google.auth uses App Engine credentials based on importability of
-  # google.appengine.api.app_identity.
-  try:
-    from google.auth import app_engine as auth_app_engine
-    if auth_app_engine.app_identity:
-      auth_app_engine.app_identity = None
-  except ImportError:
-    pass
 
 
 def _config_modules_directory(root_directory):
@@ -85,36 +57,7 @@ def fix_module_search_paths():
     sys.path.insert(0, source_directory)
     python_path.insert(0, source_directory)
 
-  # Error logging must be deferred until the end of the function since these
-  # fixes are needed to import the logs module.
-  errors = []
-
-  # Work around google package issues with App Engine SDK.
-  try:
-    import dev_appserver
-    dev_appserver.fix_google_path()
-  except (ImportError, RuntimeError) as error:
-    errors.append('Fixing google import failed: %s' % error)
-
-  # Make protobuf compatible with appengine.
-  # FIXME(unassigned): remove try-except once we confirm it works.
-  try:
-    import google
-    protobuf_directory = os.path.join(third_party_libraries_directory, 'google')
-    google.__path__.append(protobuf_directory)
-
-  except (ImportError, RuntimeError) as error:
-    errors.append('Fixing protobuf-appengine compatibility failed: %s' % error)
-
   os.environ['PYTHONPATH'] = os.pathsep.join(python_path)
 
   # Add site directory to make from imports work in google namespace.
   site.addsitedir(third_party_libraries_directory)
-
-  disable_known_module_warnings()
-  _patch_appengine_modules_for_bots()
-
-  # Since paths have now been fixed, import the custom logs module.
-  from metrics import logs
-  for error in errors:
-    logs.log_error(error)
