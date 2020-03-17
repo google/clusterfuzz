2b8122c85fa55acd1cfb9a9d318df137b4d51bc3
Oliver Chang <ochang@google.com>
2020-03-17 17:38:40 +1100

    Fix unicode exception in run.py

--- src/python/bot/startup/run.py
+++ src/python/bot/startup/run.py
@@ -68,8 +68,9 @@ def start_bot(bot_command):
   except Exception:
     pass
 
-  log_message = ('Command: %s %s (exit=%s)\n%s' % (
-      command, arguments, exit_code, '\n'.join(store_output.output)))
+  log_message = (
+      'Command: %s %s (exit=%s)\n' % (command, arguments, exit_code) +
+      b'\n'.join(store_output.output).decode('utf-8', errors='ignore'))
 
   if exit_code == 0:
     logs.log(log_message)
