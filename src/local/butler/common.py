a45d568e1cdce7d0bb17731aa7ff0f153b78216e
Oliver Chang <ochang@google.com>
2020-03-19 15:26:18 +1100

    type

--- src/local/butler/common.py
+++ src/local/butler/common.py
@@ -229,7 +229,7 @@ def _install_chromedriver():
 
 def _pip():
   """Get the pip binary name."""
-  if sys.version_info.major == '3':
+  if sys.version_info.major == 3:
     return 'pip3'
 
   return 'pip2'
