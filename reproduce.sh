ed42b74872280ef25fe2cb50d23f15c2fd6fb3d5
Martin Barbella <mbarbella@chromium.org>
2020-03-31 20:40:24 +0000

    Address review comments and clean up script.

--- reproduce.sh
+++ reproduce.sh
@@ -31,10 +31,28 @@ while [ "$1" != "" ]; do
   shift
 done
 
+# Later checks will set this to false if dependencies are missing.
+dependencies_installed="true"
+
+# Create the config directory if needed and check the ClusterFuzz version.
 mkdir -p $CLUSTERFUZZ_CONFIG_DIR
-installed_version=$(cat $CLUSTERFUZZ_CONFIG_DIR/version 2>/dev/null || echo -n)
-version=1
-if ! pipenv graph 2>&1 > /dev/null || [ "$installed_version" != "$version" ] || ([ $additional_deps_args ] && [ ! -d $ROOT_DIRECTORY/local/bin/android-sdk ]); then
+current_version=1
+version_file=$CLUSTERFUZZ_CONFIG_DIR/version
+if [ ! -e $version_file ] || [ "$(cat $version_file)" != "$current_version" ]; then
+  dependencies_installed="false"
+fi
+
+# Check if we have a valid pipenv.
+if ! pipenv graph 2>&1 > /dev/null; then
+  dependencies_installed="false"
+fi
+
+# Check to see if we need to install the android emulator.
+if [ $additional_deps_args ] && [ ! -d $ROOT_DIRECTORY/local/bin/android-sdk ]; then
+  dependencies_installed="false"
+fi
+
+if [ "$dependencies_installed" != "true" ]; then
   echo "Running first time setup. This may take a while, but is only required once."
   echo "You may see several password prompts to install required packages."
   sleep 5
