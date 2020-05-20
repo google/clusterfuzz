7abb486e955fd6696f1dbaf609e8a2d6917cf4a4
Abhishek Arya <inferno@chromium.org>
2020-05-20 09:28:18 -0700

    Fix review nits.

--- src/appengine/handlers/cron/project_setup.py
+++ src/appengine/handlers/cron/project_setup.py
@@ -72,7 +72,7 @@ ALLOWED_VIEW_RESTRICTIONS = ['none', 'security', 'all']
 
 PUBSUB_PLATFORMS = ['linux']
 
-MEMORY_SAFE_LANGUAGES = ['go', 'java', 'python', 'rust']
+MEMORY_SAFE_LANGUAGES = {'go', 'java', 'python', 'rust'}
 OSS_FUZZ_DEFAULT_PROJECT_CPU_WEIGHT = 1.0
 OSS_FUZZ_MEMORY_SAFE_LANGUAGE_PROJECT_WEIGHT = 0.2
 
@@ -532,10 +532,10 @@ def create_project_settings(project, info, service_account):
       oss_fuzz_project.ccs = ccs
       oss_fuzz_project.put()
   else:
-    cpu_weight = (
-        OSS_FUZZ_MEMORY_SAFE_LANGUAGE_PROJECT_WEIGHT
-        if language in MEMORY_SAFE_LANGUAGES else
-        OSS_FUZZ_DEFAULT_PROJECT_CPU_WEIGHT)
+    if language in MEMORY_SAFE_LANGUAGES:
+      cpu_weight = OSS_FUZZ_MEMORY_SAFE_LANGUAGE_PROJECT_WEIGHT
+    else:
+      cpu_weight = OSS_FUZZ_DEFAULT_PROJECT_CPU_WEIGHT
 
     data_types.OssFuzzProject(
         id=project,
