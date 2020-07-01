8522c0f6c1bb7a299ec8411d02e32a408de53094
Oliver Chang <oliverchang@users.noreply.github.com>
2020-07-01 12:25:48 +1000

    Filter out more testcases when requesting bisects. (#1872)
    To reduce load, only request bisects for testcases which are
    reproducible security bugs with a bug filed.
    
    As a result, requests for both regressed and fixed are moved to the
    completion of progression_task, as this information isn't available when
    regression_task completes.

--- src/python/tests/core/bot/tasks/task_creation_test.py
+++ src/python/tests/core/bot/tasks/task_creation_test.py
@@ -63,47 +63,78 @@ class RequestBisectionTest(unittest.TestCase):
         }
     })
 
-  def _test(self, sanitizer, bisect_type):
-    task_creation.request_bisection(self.testcase, bisect_type)
-    publish_call = self.mock.publish.call_args[0]
-    topic = publish_call[1]
-    message = publish_call[2]
-    self.assertEqual('/projects/project/topics/topic', topic)
-    self.assertEqual(b'reproducer', message.data)
-    self.assertDictEqual({
-        'crash_type': 'crash-type',
-        'security': 'True',
-        'fuzz_target': 'target',
-        'new_commit': 'new',
-        'old_commit': 'old',
-        'project_name': 'proj',
-        'sanitizer': sanitizer,
-        'testcase_id': 1,
-        'issue_id': '1337',
-        'type': bisect_type,
-    }, message.attributes)
+  def _test(self, sanitizer):
+    """Test task publication."""
+    task_creation.request_bisection(self.testcase)
+    publish_calls = self.mock.publish.call_args_list
+    bisect_types = ('regressed', 'fixed')
 
-  def test_request_bisection_regressed(self):
-    """Basic regressed test."""
-    self.testcase.job_type = 'libfuzzer_asan_proj'
-    self._test('address', 'regressed')
-    self.testcase.job_type = 'libfuzzer_msan_proj'
-    self._test('memory', 'regressed')
-    self.testcase.job_type = 'libfuzzer_ubsan_proj'
-    self._test('undefined', 'regressed')
+    self.assertEqual(2, len(publish_calls))
+    for bisect_type, publish_call in zip(bisect_types, publish_calls):
+      topic = publish_call[0][1]
+      message = publish_call[0][2]
+      self.assertEqual('/projects/project/topics/topic', topic)
+      self.assertEqual(b'reproducer', message.data)
+      self.assertDictEqual({
+          'crash_type': 'crash-type',
+          'security': 'True',
+          'fuzz_target': 'target',
+          'new_commit': 'new',
+          'old_commit': 'old',
+          'project_name': 'proj',
+          'sanitizer': sanitizer,
+          'testcase_id': 1,
+          'issue_id': '1337',
+          'type': bisect_type,
+      }, message.attributes)
 
-  def test_request_bisection_fixed(self):
-    """Basic fixed test."""
+  def test_request_bisection_asan(self):
+    """Basic regressed test (asan)."""
     self.testcase.job_type = 'libfuzzer_asan_proj'
-    self._test('address', 'fixed')
+    self._test('address')
+
+  def test_request_bisection_msan(self):
+    """Basic regressed test (asan)."""
     self.testcase.job_type = 'libfuzzer_msan_proj'
-    self._test('memory', 'fixed')
+    self._test('memory')
+
+  def test_request_bisection_ubsan(self):
+    """Basic regressed test (ubsan)."""
     self.testcase.job_type = 'libfuzzer_ubsan_proj'
-    self._test('undefined', 'fixed')
+    self._test('undefined')
 
   def test_request_bisection_blackbox(self):
     """Test request bisection for blackbox."""
     self.testcase.job_type = 'blackbox'
     self.testcase.overridden_fuzzer_name = None
-    task_creation.request_bisection(self.testcase, 'regressed')
+    task_creation.request_bisection(self.testcase)
+    self.assertEqual(0, self.mock.publish.call_count)
+
+  def test_request_bisection_non_security(self):
+    """Test request bisection for non-security testcases."""
+    self.testcase.job_type = 'libfuzzer_asan_proj'
+    self.testcase.security_flag = False
+    task_creation.request_bisection(self.testcase)
+    self.assertEqual(0, self.mock.publish.call_count)
+
+  def test_request_bisection_flaky(self):
+    """Test request bisection for flaky testcases."""
+    self.testcase.job_type = 'libfuzzer_asan_proj'
+    self.testcase.one_time_crasher_flag = True
+    task_creation.request_bisection(self.testcase)
+    self.assertEqual(0, self.mock.publish.call_count)
+
+  def test_request_bisection_no_bug(self):
+    """Test request bisection for testcases with no bug attached."""
+    self.testcase.job_type = 'libfuzzer_asan_proj'
+    self.testcase.bug_information = ''
+    task_creation.request_bisection(self.testcase)
+    self.assertEqual(0, self.mock.publish.call_count)
+
+  def test_request_bisection_invalid_range(self):
+    """Test request bisection for testcases with no bug attached."""
+    self.testcase.job_type = 'libfuzzer_asan_proj'
+    self.testcase.regression = 'NA'
+    self.testcase.fixed = 'NA'
+    task_creation.request_bisection(self.testcase)
     self.assertEqual(0, self.mock.publish.call_count)
