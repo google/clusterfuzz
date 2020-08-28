google/clusterfuzz

commit eb208896b58bbdddf9bd7085e830d1ce9ee8eb86

Fix android platform check to account for ANDROID_KERNEL. (#1907)
* Fix android platform check to account for ANDROID_KERNEL.

* Address review comments.

src/python/system/process_handler.py
@@ -144,10 +144,10 @@ def run_process(cmdline,
   # This is used when running scripts on native linux OS and not on the device.
   # E.g. running a fuzzer to generate testcases or launcher script.
   plt = environment.platform()
-  if plt in ['ANDROID', 'FUCHSIA'] and (not testcase_run or launcher):
+  is_android = environment.is_android(plt)
+  runs_on_device = is_android or plt == 'FUCHSIA'
+  if runs_on_device and (not testcase_run or launcher):
     plt = 'LINUX'
-  elif plt == 'IOS' and (not testcase_run or launcher):
-    plt = 'MAC'
 
   # Lower down testcase timeout slightly to account for time for crash analysis.
   timeout -= CRASH_ANALYSIS_TIME
@@ -165,8 +165,7 @@ def run_process(cmdline,
   process_poll_interval = environment.get_value('PROCESS_POLL_INTERVAL', 0.5)
   start_time = time.time()
   watch_for_process_exit = (
-      environment.get_value('WATCH_FOR_PROCESS_EXIT')
-      if plt == 'ANDROID' else True)
+      environment.get_value('WATCH_FOR_PROCESS_EXIT') if is_android else True)
   window_list = []
 
   # Get gesture start time from last element in gesture list.
@@ -177,7 +176,7 @@ def run_process(cmdline,
   else:
     gesture_start_time = timeout // 2
 
-  if plt == 'ANDROID':
+  if is_android:
     # Clear the log upfront.
     android.logger.clear_log()
 
@@ -221,7 +220,7 @@ def run_process(cmdline,
         windows.gestures.run_gestures(gestures, process_handle.pid,
                                       process_status, start_time, timeout,
                                       window_list)
-      elif plt == 'ANDROID':
+      elif is_android:
         android.gestures.run_gestures(gestures, start_time, timeout)
 
         # TODO(mbarbella): We add a fake window here to prevent gestures on
@@ -234,7 +233,7 @@ def run_process(cmdline,
     # Collect the process output.
     output = (
         android.logger.log_output()
-        if plt == 'ANDROID' else b'\n'.join(process_output.output))
+        if is_android else b'\n'.join(process_output.output))
     output = utils.decode_to_unicode(output)
     if crash_analyzer.is_memory_tool_crash(output):
       break
@@ -243,7 +242,7 @@ def run_process(cmdline,
     if watch_for_process_exit:
       # If |watch_for_process_exit| is set, then we already completed running
       # our app launch command. So, we can bail out.
-      if plt == 'ANDROID':
+      if is_android:
         break
 
       # On desktop, we bail out as soon as the process finishes.
@@ -253,7 +252,7 @@ def run_process(cmdline,
         break
 
   # Process output based on platform.
-  if plt == 'ANDROID':
+  if is_android:
     # Get current log output. If device is in reboot mode, logcat automatically
     # waits for device to be online.
     time.sleep(ANDROID_CRASH_LOGCAT_WAIT_TIME)
@@ -341,7 +340,7 @@ def run_process(cmdline,
     return_code = 1
 
   # If a crash is found, then we add the memory state as well.
-  if return_code and plt == 'ANDROID':
+  if return_code and is_android:
     ps_output = android.adb.get_ps_output()
     if ps_output:
       output += utils.get_line_seperator('Memory Statistics')
@@ -467,7 +466,7 @@ def terminate_root_and_child_processes(root_pid):
       # Process doesn't exist anymore.
       continue
 
-    if child.name == app_name:
+    if child.name() == app_name:
       # Send SIGTERM to the root APP_NAME process only, and none of its children
       # so that coverage data will be dumped properly (e.g. the browser process
       # of chrome).
@@ -525,7 +524,7 @@ def terminate_stale_application_instances():
     processes_to_kill += additional_process_to_kill.split(' ')
   processes_to_kill = [x for x in processes_to_kill if x]
 
-  if platform == 'ANDROID':
+  if environment.is_android(platform):
     # Cleanup any stale adb connections.
     device_serial = environment.get_value('ANDROID_SERIAL')
     adb_search_string = 'adb -s %s' % device_serial

src/python/bot/fuzzers/libfuzzer.py
@@ -1320,7 +1320,7 @@ def get_runner(fuzzer_path, temp_dir=None, use_minijail=None, use_unshare=None):
 
   build_dir = environment.get_value('BUILD_DIR')
   dataflow_build_dir = environment.get_value('DATAFLOW_BUILD_DIR')
-  is_android = environment.platform() == 'ANDROID'
+  is_android = environment.is_android()
   is_fuchsia = environment.platform() == 'FUCHSIA'
 
   if not is_fuchsia:
@@ -1782,7 +1782,7 @@ def pick_strategies(strategy_pool,
 
   # FIXME: Disable for now to avoid severe battery drainage. Stabilize and
   # re-enable with a lower process count.
-  is_android = environment.platform() == 'ANDROID'
+  is_android = environment.is_android()
   # Fork mode is not supported on Fuchsia platform.
   is_fuchsia = environment.platform() == 'FUCHSIA'
   # Fork mode is disabled on ephemeral bots due to a bug on the platform.

src/python/bot/tasks/fuzz_task.py
@@ -1515,7 +1515,7 @@ class FuzzingSession(object):
 
     # For Android, we need to sync our local testcases directory with the one on
     # the device.
-    if environment.platform() == 'ANDROID':
+    if environment.is_android():
       android.device.push_testcases_to_device()
 
     if environment.is_trusted_host():
@@ -1902,11 +1902,10 @@ class FuzzingSession(object):
 
     logs.log('Finished processing test cases.')
 
-    platform = environment.platform()
     platform_id = environment.get_platform_id()
 
     # For Android, bring back device to a good state before analyzing crashes.
-    if (platform in ('ANDROID', 'ANDROID-KERNEL')) and crashes:
+    if environment.is_android() and crashes:
       # Remove this variable so that application is fully shutdown before every
       # re-run of testcase. This is critical for reproducibility.
       environment.remove_key('CHILD_PROCESS_TERMINATION_PATTERN')

src/python/bot/tasks/impact_task.py
@@ -92,7 +92,7 @@ def get_start_and_end_revision(regression_range, job_type):
       regression_range)
 
   # FIXME: Hack to use chromium revision for android builds.
-  if 'android' in job_type.lower():
+  if environment.is_android():
     return get_chromium_component_start_and_end_revision(
         start_revision, end_revision, job_type)
 

src/python/bot/tasks/minimize_task.py
@@ -286,7 +286,7 @@ class TestRunner(object):
 
     # For Android, we need to sync our local testcases directory with the one on
     # the device.
-    if environment.platform() == 'ANDROID':
+    if environment.is_android():
       android.device.push_testcases_to_device()
     elif environment.is_trusted_host():
       from bot.untrusted_runner import file_host

src/python/bot/tasks/setup.py
@@ -229,7 +229,7 @@ def setup_testcase(testcase, job_type, fuzzer_override=None):
 
   # For Android/Fuchsia, we need to sync our local testcases directory with the
   # one on the device.
-  if environment.platform() == 'ANDROID':
+  if environment.is_android():
     _copy_testcase_to_device_and_setup_environment(testcase, testcase_file_path)
 
   # Push testcases to worker.

src/python/bot/tasks/update_task.py
@@ -173,7 +173,7 @@ def run_platform_init_scripts():
   logs.log('Running platform initialization scripts.')
 
   plt = environment.platform()
-  if plt in ('ANDROID', 'ANDROID_KERNEL'):
+  if environment.is_android():
     android_init.run()
   elif plt == 'CHROMEOS':
     chromeos_init.run()

src/python/bot/testcase_manager.py
@@ -380,7 +380,7 @@ def convert_dependency_url_to_local_path(url):
       local_path = '/' + file_path
 
       # Convert remote to local path for android.
-      if platform == 'ANDROID':
+      if environment.is_android():
         remote_testcases_directory = android.constants.DEVICE_TESTCASES_DIR
         local_testcases_directory = environment.get_value('FUZZ_INPUTS')
         local_path = local_path.replace(remote_testcases_directory,
@@ -888,7 +888,7 @@ def get_command_line_for_application(file_to_run='',
   extension_argument = environment.get_value('EXTENSION_ARG')
   input_directory = environment.get_value('INPUT_DIR')
   launcher = environment.get_value('LAUNCHER_PATH')
-  plt = environment.platform()
+  is_android = environment.is_android()
   root_directory = environment.get_value('ROOT_DIR')
   temp_directory = environment.get_value('BOT_TMPDIR')
   user_profile_argument = environment.get_value('USER_PROFILE_ARG')
@@ -942,7 +942,7 @@ def get_command_line_for_application(file_to_run='',
         launcher_with_interpreter = shell.get_execute_command(
             launcher, is_blackbox_fuzzer=True)
         command += launcher_with_interpreter + ' '
-    elif plt in ['ANDROID']:
+    elif is_android:
       # Android-specific testcase path fixup for fuzzers that don't rely on
       # launcher scripts.
       local_testcases_directory = environment.get_value('FUZZ_INPUTS')
@@ -1017,7 +1017,7 @@ def get_command_line_for_application(file_to_run='',
   command = command.replace('%TMP_DIR%', temp_directory)
   command = command.replace('%USER_PROFILE_DIR%', user_profile_directory)
 
-  if plt == 'ANDROID' and not launcher:
+  if is_android and not launcher:
     # Initial setup phase for command line.
     if write_command_line_file:
       android.adb.write_command_line_file(command, app_path)

commit 40beeb7012d79f95b74cfeb3014fa4ee0694aa76

Fix android platform check in run_process.
This is a regression from
https://github.com/google/clusterfuzz/pull/1907

Fixes
https://github.com/google/clusterfuzz/issues/1993

src/python/system/process_handler.py
@@ -141,13 +141,15 @@ def run_process(cmdline,
   # FIXME(mbarbella): Using LAUNCHER_PATH here is error prone. It forces us to
   # do certain operations before fuzzer setup (e.g. bad build check).
   launcher = environment.get_value('LAUNCHER_PATH')
+
   # This is used when running scripts on native linux OS and not on the device.
   # E.g. running a fuzzer to generate testcases or launcher script.
   plt = environment.platform()
-  is_android = environment.is_android(plt)
-  runs_on_device = is_android or plt == 'FUCHSIA'
+  runs_on_device = environment.is_android(plt) or plt == 'FUCHSIA'
   if runs_on_device and (not testcase_run or launcher):
     plt = 'LINUX'
+
+  is_android = environment.is_android(plt)
 
   # Lower down testcase timeout slightly to account for time for crash analysis.
   timeout -= CRASH_ANALYSIS_TIME