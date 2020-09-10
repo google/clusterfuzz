04da4b64bd82ab43c6aa28a340ebffb1d5f17852
Cameron Finucane <eep@google.com>
2020-09-10 12:58:21 -0700

    [fuchsia] Update integration tests to match changes in example fuzzers
    A few example fuzzers that were being used in the Fuchsia integration
    tests were renamed/updated in fxrev.dev/416542. This updates the tests
    so they once again run successfully.

--- src/python/tests/core/bot/tasks/corpus_pruning_task_test.py
+++ src/python/tests/core/bot/tasks/corpus_pruning_task_test.py
@@ -305,7 +305,7 @@ class CorpusPruningTestFuchsia(unittest.TestCase, BaseTest):
         platform='FUCHSIA',
         environment_string=env_string).put()
     data_types.FuzzTarget(
-        binary='example-fuzzers/trap_fuzzer',
+        binary='example-fuzzers/crash_fuzzer',
         engine='libFuzzer',
         project='fuchsia').put()
 
@@ -321,7 +321,7 @@ class CorpusPruningTestFuchsia(unittest.TestCase, BaseTest):
     """Basic pruning test."""
     self.corpus_dir = self.fuchsia_corpus_dir
     corpus_pruning_task.execute_task(
-        'libFuzzer_fuchsia_example-fuzzers-trap_fuzzer',
+        'libFuzzer_fuchsia_example-fuzzers-crash_fuzzer',
         'libfuzzer_asan_fuchsia')
     corpus = os.listdir(self.corpus_dir)
     self.assertEqual(2, len(corpus))
