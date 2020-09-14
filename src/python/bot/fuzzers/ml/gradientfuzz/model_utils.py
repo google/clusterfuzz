ef761365fb1e9013f48362b1f5eea4349c9c79cb
mbarbella-chromium <41697236+mbarbella-chromium@users.noreply.github.com>
2020-09-14 15:55:43 -0700

    Revert "Revert "Revert "Gradientfuzz Integration [2] (#2002)" (#2020)" (#2023)"
    This reverts commit 8a4cbdbcabb91947f0ac9843a5520a3687302376.

--- src/python/bot/fuzzers/ml/gradientfuzz/model_utils.py
+++ src/python/bot/fuzzers/ml/gradientfuzz/model_utils.py
@@ -51,6 +51,7 @@ class BitmapAcc(keras.metrics.Metric):
       Computes (total matches) / (total branches).
       """
     y_true, y_pred = args
+    # y_true = tf.cast(tf.round(y_true), tf.bool)
     y_pred = tf.cast(tf.round(y_pred), tf.bool)
     total_correct_branches = tf.reduce_sum(
         tf.cast(tf.equal(y_true, y_pred), tf.float32))
@@ -95,6 +96,7 @@ class NeuzzJaccardAcc(keras.metrics.Metric):
       Computes (true positives) / (true positives + errors).
       """
     y_true, y_pred = args
+    # y_true = tf.cast(tf.round(y_true), tf.bool)
     y_pred = tf.cast(tf.round(y_pred), tf.bool)
     self.true_positives.assign_add(
         tf.reduce_sum(tf.cast(tf.logical_and(y_true, y_pred), tf.float32)))
