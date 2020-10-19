20a4de8c85595566e4f3ebdfbd49bde7fb090bfd
Oliver Chang <oliverchang@users.noreply.github.com>
2020-10-20 08:41:38 +1100

    Remove some dependencies for libClusterFuzz. (#2095)
    * Remove some dependencies for libClusterFuzz.
    
    - Move selenium to Pipfile.
    - Move python-dateutil to appengine requirements.
    - Remove hyperlink pin.
    - Remove pytz.
    - Remove configparser. This was a Python 2 backport of a Python 3
      module.
    - Remove peach, antlr dependencies for Pip package.
    - Also some import fixes for the pip package.
    
    * remove debug print

--- src/python/lib/build.sh
+++ src/python/lib/build.sh
@@ -19,14 +19,13 @@ find clusterfuzz/ -type l | xargs rm
 # Symlink dependencies.
 ln -s $(pwd)/../base clusterfuzz/
 ln -s $(pwd)/../bot clusterfuzz/
-ln -s $(pwd)/../build_managerment clusterfuzz/
+ln -s $(pwd)/../build_management clusterfuzz/
 ln -s $(pwd)/../config clusterfuzz/
 ln -s $(pwd)/../crash_analysis clusterfuzz/
 ln -s $(pwd)/../datastore clusterfuzz/
 ln -s $(pwd)/../fuzzer_utils clusterfuzz/
 ln -s $(pwd)/../fuzzing clusterfuzz/
 ln -s $(pwd)/../google_cloud_utils clusterfuzz/
-ln -s $(pwd)/../lib clusterfuzz/
 ln -s $(pwd)/../metrics clusterfuzz/
 ln -s $(pwd)/../platforms clusterfuzz/
 ln -s $(pwd)/../system clusterfuzz/
