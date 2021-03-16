7efb018c89b29abcc7ad030e6b79f5a530f4040a
Cameron Finucane <eep@google.com>
2021-03-15 17:26:28 -0700

    add clarifying comments about undercoat stdout vs stderr

--- src/python/platforms/fuchsia/undercoat.py
+++ src/python/platforms/fuchsia/undercoat.py
@@ -57,13 +57,17 @@ def undercoat_api_command(*args):
   bundle_dir = environment.get_value('FUCHSIA_RESOURCES_DIR')
   undercoat_path = os.path.join(bundle_dir, 'undercoat', 'undercoat')
   undercoat = new_process.ProcessRunner(undercoat_path, args)
-  error_log = tempfile.TemporaryFile()
-  result = undercoat.run_and_wait(stderr=error_log)
+  # The undercoat log is sent to stderr, which we capture to a tempfile
+  undercoat_log = tempfile.TemporaryFile()
+  result = undercoat.run_and_wait(stderr=undercoat_log)
   result.output = result.output.decode('utf-8')
 
   if result.return_code != 0:
-    log_data = utils.read_from_handle_truncated(error_log, 1024 * 1024)
+    # Dump the undercoat log to assist in debugging
+    log_data = utils.read_from_handle_truncated(undercoat_log, 1024 * 1024)
     logs.log_warn('Log output from undercoat: ' + log_data)
+
+    # The API error message is returned on stdout
     raise UndercoatError(
         'Error running undercoat command %s: %s' % (args, result.output))
 
