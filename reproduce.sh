13bb8178945b7e0bd99c87e472630c8df294a3ce
Oliver Chang <ochang@google.com>
2021-05-07 11:01:02 +1000

    Fix reproduce script again.

--- reproduce.sh
+++ reproduce.sh
@@ -62,7 +62,9 @@ fi
 
 # Need to get python to use pipenv.
 if [ -z "$PYTHON" ]; then
-  if which python3.8 > /dev/null; then
+  if which python3.9 > /dev/null; then
+    PYTHON='python3.9'
+  elif which python3.8 > /dev/null; then
     PYTHON='python3.8'
   elif which python3.7 > /dev/null; then
     PYTHON='python3.7'
@@ -77,8 +79,8 @@ if ! which "$PYTHON" > /dev/null; then
 fi
 
 version=$($PYTHON --version 2>&1 | cut -f2 -d' ')
-if [[ "$version" < "3.7" || ! "$version" < "3.9" ]]; then
-  echo "You need Python 3.8 or 3.7. Try \`export PYTHON=python3.8\` (or 3.7)."
+if [[ "$version" < "3.7" || ! "$version" < "3.a" ]]; then
+  echo "You need Python 3.9, 3.8 or 3.7. Try \`export PYTHON=python3.7\` (or 3.8, 3.9)."
   exit 1
 fi
 
