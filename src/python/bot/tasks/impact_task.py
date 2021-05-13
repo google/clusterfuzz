e44231b5b2c671a9a76ee11b5da0b2ade8bafc83
yuanjunh-git <82986944+yuanjunh-git@users.noreply.github.com>
2021-05-12 20:17:00 -0700

    Add tmp code for testing extended stable flow in Prod (#2333)

--- src/python/bot/tasks/impact_task.py
+++ src/python/bot/tasks/impact_task.py
@@ -242,6 +242,10 @@ def get_impacts_on_prod_builds(testcase, testcase_file_path):
 def get_impact_on_build(build_type, current_version, testcase,
                         testcase_file_path):
   """Return impact and additional trace on a prod build given build_type."""
+  # TODO(yuanjunh): remove es_enabled var after testing is done.
+  es_enabled = testcase.get_metadata('es_enabled', False)
+  if build_type == 'extended_stable' and not es_enabled:
+    return Impact()
   build = build_manager.setup_production_build(build_type)
   if not build:
     raise BuildFailedException(
@@ -257,6 +261,11 @@ def get_impact_on_build(build_type, current_version, testcase,
   app_path = environment.get_value('APP_PATH')
   command = testcase_manager.get_command_line_for_application(
       testcase_file_path, app_path=app_path, needs_http=testcase.http_flag)
+
+  if es_enabled:
+    logs.log(
+        "ES build for testcase %d, command: %s" % (testcase.key.id(), command))
+
   result = testcase_manager.test_for_crash_with_retries(
       testcase,
       testcase_file_path,
