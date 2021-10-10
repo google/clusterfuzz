59bcd8ea15077904ca26c1a0d2473690bcfc776f
Oliver Chang <oliverchang@users.noreply.github.com>
2021-10-11 10:27:13 +1100

    Bump setup.py version for new release. (#2473)

--- src/setup.py
+++ src/setup.py
@@ -19,7 +19,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='clusterfuzz',
-    version='2.5.5.post1',
+    version='2.5.6',
     author='ClusterFuzz authors',
     author_email='clusterfuzz-dev@googlegroups.com',
     description='ClusterFuzz',
