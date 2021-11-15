788137e4cc994a0f7a2f21ebb1e184569c9d21e0
Huizi Yang <yanghuiz@google.com>
2021-11-14 20:01:32 -0800

    Add heartbeat script for Android device. (#2487)
    * Add heartbeat script for Android device.
    
    * fix lint
    
    * fix unittest
    
    * Address comments

--- src/python/bot/startup/run.py
+++ src/python/bot/startup/run.py
@@ -36,11 +36,13 @@ from clusterfuzz._internal.system import shell
 
 BOT_SCRIPT = 'run_bot.py'
 HEARTBEAT_SCRIPT = 'run_heartbeat.py'
+ANDROID_HEARTBEAT_SCRIPT = 'android_heartbeat.py'
 HEARTBEAT_START_WAIT_TIME = 60
 LOOP_SLEEP_INTERVAL = 3
 MAX_SUBPROCESS_TIMEOUT = 2**31 // 1000  # https://bugs.python.org/issue20493
 
 _heartbeat_handle = None
+_android_heartbeat_handle = None
 
 
 def start_bot(bot_command):
@@ -129,6 +131,46 @@ def stop_heartbeat():
   _heartbeat_handle = None
 
 
+def start_android_heartbeat():
+  """Start the android heartbeat (in another process)."""
+  global _android_heartbeat_handle
+  if _android_heartbeat_handle:
+    # If heartbeat is already started, no work to do. Bail out.
+    return
+
+  base_directory = environment.get_startup_scripts_directory()
+  android_beat_script_path = os.path.join(base_directory,
+                                          ANDROID_HEARTBEAT_SCRIPT)
+  android_beat_interpreter = shell.get_interpreter(android_beat_script_path)
+  assert android_beat_interpreter
+  android_beat_command = [android_beat_interpreter, android_beat_script_path]
+
+  try:
+    process_handle = subprocess.Popen(android_beat_command)
+  except Exception:
+    logs.log_error('Unable to start android heartbeat process (%s).' %
+                   android_beat_command)
+    return
+
+  # If heartbeat is successfully started, set its handle now.
+  _android_heartbeat_handle = process_handle
+
+
+def stop_android_heartbeat():
+  """Stop the android heartbeat process."""
+  global _android_heartbeat_handle
+  if not _android_heartbeat_handle:
+    # If there is no heartbeat started yet, no work to do. Bail out.
+    return
+
+  try:
+    _android_heartbeat_handle.kill()
+  except Exception as e:
+    logs.log_error('Unable to stop android heartbeat process: %s' % str(e))
+
+  _android_heartbeat_handle = None
+
+
 def update_source_code_if_needed():
   """Update source code if needed."""
   try:
@@ -148,9 +190,13 @@ def update_source_code_if_needed():
 def run_loop(bot_command, heartbeat_command):
   """Run infinite loop with bot's command."""
   atexit.register(stop_heartbeat)
+  if environment.is_android():
+    atexit.register(stop_android_heartbeat)
 
   while True:
     update_source_code_if_needed()
+    if environment.is_android():
+      start_android_heartbeat()
     start_heartbeat(heartbeat_command)
     start_bot(bot_command)
 
