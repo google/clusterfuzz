01548d842ec698c21f502355873576c720b04639
Al Muthanna Athamina <almuthanna@chromium.org>
2022-01-13 16:20:28 +0100

    Add the ability to configure trials using a JSON file

--- src/clusterfuzz/_internal/tests/core/bot/tasks/trials_test.py
+++ src/clusterfuzz/_internal/tests/core/bot/tasks/trials_test.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 """Tests for app specific trials and experiments."""
 
+import json
+
 from pyfakefs import fake_filesystem_unittest
 
 from clusterfuzz._internal.bot.tasks import trials
@@ -28,6 +30,7 @@ class TrialsTest(fake_filesystem_unittest.TestCase):
 
   def setUp(self):
     environment.set_value('APP_DIR', '/src')
+    test_utils.set_up_pyfakefs(self)
 
     test_helpers.patch_environ(self, env={'APP_ARGS': '-x'})
 
@@ -44,12 +47,18 @@ class TrialsTest(fake_filesystem_unittest.TestCase):
 
   def source_side_test(self, config_file_content, probability, app_name,
                        app_args, trial_app_args):
-    """source side trials test template"""
-    test_utils.set_up_pyfakefs(self)
+    """Source side trials test template."""
     self.fs.create_file('/src/clusterfuzz_trials_config.json')
 
     with open('/src/clusterfuzz_trials_config.json', 'w') as f:
-      f.write(config_file_content)
+      f.write(json.dumps(config_file_content))
+
+    self.mock.random.return_value = probability
+    environment.set_value('APP_NAME', app_name)
+    trial_selector = trials.Trials()
+    trial_selector.setup_additional_args_for_app()
+    self.assertEqual(environment.get_value('APP_ARGS'), app_args)
+    self.assertEqual(environment.get_value('TRIAL_APP_ARGS'), trial_app_args)
 
     self.mock.random.return_value = probability
     environment.set_value('APP_NAME', app_name)
@@ -114,84 +123,93 @@ class TrialsTest(fake_filesystem_unittest.TestCase):
 
   def test_no_effect_on_no_match_source_side(self):
     """Ensure that no additional flags are added if a binary has no trials on source side."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--c4", "app_name": "app_4", "probability": 0.2}
-      ]
-      """, 0.0, 'app_0', '-x', None)
+    config_file_content = [{
+        "app_args": "--c4",
+        "app_name": "app_4",
+        "probability": 0.2
+    }]
+    self.source_side_test(config_file_content, 0.0, 'app_0', '-x', None)
 
   def test_trial_selected_one_option_source_side(self):
     """Ensure that the expected flags are added if a trial is selected on source side."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--c4", "app_name": "app_4", "probability": 0.5}
-      ]
-      """, 0.3, 'app_4', '-x --c4', '--c4')
+    config_file_content = [{
+        "app_args": "--c4",
+        "app_name": "app_4",
+        "probability": 0.5
+    }]
+    self.source_side_test(config_file_content, 0.3, 'app_4', '-x --c4', '--c4')
 
   def test_trial_not_selected_source_side(self):
     """Ensure no additional flags if a trial was not selected on source side."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--c4", "app_name": "app_4", "probability": 0.4},
-        {"app_args": "--c5", "app_name": "app_4", "probability": 0.2}
-      ]
-      """, 0.5, 'app_4', '-x', None)
+    config_file_content = [{
+        "app_args": "--c4",
+        "app_name": "app_4",
+        "probability": 0.4
+    }, {
+        "app_args": "--c5",
+        "app_name": "app_4",
+        "probability": 0.2
+    }]
+    self.source_side_test(config_file_content, 0.5, 'app_4', '-x', None)
 
   def test_multiple_trial_selection_source_side(self):
     """Ensure that we can suggest the second trial in a batch of multiple on source side."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--c4", "app_name": "app_4", "probability": 1.0},
-        {"app_args": "--c5", "app_name": "app_4", "probability": 0.2},
-        {"app_args": "--c6", "app_name": "app_4", "probability": 0.2}
-      ]
-      """, 0.1, 'app_4', '-x --c4 --c5 --c6', '--c4 --c5 --c6')
+    config_file_content = [{
+        "app_args": "--c4",
+        "app_name": "app_4",
+        "probability": 1.0
+    }, {
+        "app_args": "--c5",
+        "app_name": "app_4",
+        "probability": 0.2
+    }, {
+        "app_args": "--c6",
+        "app_name": "app_4",
+        "probability": 0.2
+    }]
+    self.source_side_test(config_file_content, 0.1, 'app_4',
+                          '-x --c4 --c5 --c6', '--c4 --c5 --c6')
 
   def test_selection_for_windows_executable_source_side(self):
     """Ensure that flags are added when the app name ends in ".exe" on source side."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--c4", "app_name": "app_4", "probability": 1.0}
-      ]
-      """, 0.3, 'app_4.exe', '-x --c4', '--c4')
+    config_file_content = [{
+        "app_args": "--c4",
+        "app_name": "app_4",
+        "probability": 1.0
+    }]
+    self.source_side_test(config_file_content, 0.3, 'app_4.exe', '-x --c4',
+                          '--c4')
 
   def test_selection_for_android_apk_source_side(self):
     """Ensure that flags are added for the Android APK format on source side."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--c4", "app_name": "app_4", "probability": 1.0}
-      ]
-      """, 0.3, 'App_4.apk', '-x --c4', '--c4')
+    config_file_content = [{
+        "app_args": "--c4",
+        "app_name": "app_4",
+        "probability": 1.0
+    }]
+    self.source_side_test(config_file_content, 0.3, 'App_4.apk', '-x --c4',
+                          '--c4')
 
   def test_trial_args_adding_using_config_file(self):
     """Ensure that a trial can add args using the config file."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--c4", "app_name": "app_1", "probability": 0.5}
-      ]
-      """, 0.3, 'app_1', '-x --a1 --c4', '--a1 --c4')
+    config_file_content = [{
+        "app_args": "--c4",
+        "app_name": "app_1",
+        "probability": 0.5
+    }]
+    self.source_side_test(config_file_content, 0.3, 'app_1', '-x --a1 --c4',
+                          '--a1 --c4')
 
   def test_trial_probability_override_using_config_file(self):
     """Ensure that a trial probability can be overriden using the config file."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--a1", "app_name": "app_1", "probability": 0.8}
-      ]
-      """, 0.7, 'app_1', '-x --a1', '--a1')
+    config_file_content = [{
+        "app_args": "--a1",
+        "app_name": "app_1",
+        "probability": 0.8
+    }]
+    self.source_side_test(config_file_content, 0.7, 'app_1', '-x --a1', '--a1')
 
   def test_corrupted_config_file_is_ignored(self):
     """Ensure that a trial probability will not be overriden using a corrupted config file."""
-    self.source_side_test(
-        """
-      [
-        {"app_args": "--a1", "app_name": "app_1", "probability": 0.8
-      ]
-      """, 0.7, 'app_1', '-x', None)
+    config_file_content = '[{"app_args": "--a1", "app_name": "app_1", "probability": 0.8]'
+    self.source_side_test(config_file_content, 0.7, 'app_1', '-x', None)
