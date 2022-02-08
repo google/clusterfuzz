3418b7c5d781188d1daa0ce6aa8fee84c8411c2f
Alan32Liu <donggeliu@google.com>
2022-02-08 14:02:58 +1100

    Three new testcases for updates with multiple trials

--- src/clusterfuzz/_internal/tests/appengine/handlers/external_update_test.py
+++ src/clusterfuzz/_internal/tests/appengine/handlers/external_update_test.py
@@ -118,6 +118,7 @@ class ExternalUpdatesTest(unittest.TestCase):
     self.testcase.put()
 
     stacktrace = self._read_test_data('asan_uaf.txt')
+    print(DATA_DIRECTORY)
     self.app.post(
         '/external-update',
         params=self._make_message(stacktrace, {
@@ -233,3 +234,75 @@ class ExternalUpdatesTest(unittest.TestCase):
         'libFuzzer_fuzzer/libfuzzer_external_job')
     self.assertIsNotNone(fuzz_target_job)
     self.assertEqual(datetime.datetime(2021, 1, 1), fuzz_target_job.last_run)
+
+  def test_update_multiple_fixed(self):
+    """Test an update that has multiple trials with no crash."""
+    self.app.post(
+        '/external-update',
+        params=self._make_message(
+            data=b'["", "", ""]',
+            attributes={
+                'testcaseId': self.testcase.key.id(),
+                'revision': '1337'
+            }),
+        headers={'Authorization': 'Bearer fake'},
+        content_type='application/octet-stream')
+
+    updated_testcase = self.testcase.key.get()
+    self.assertFalse(updated_testcase.open)
+    self.assertEqual('1336:1337', updated_testcase.fixed)
+    self.assertEqual('last_tested',
+                     updated_testcase.last_tested_crash_stacktrace)
+    self.assertEqual(1337,
+                     updated_testcase.get_metadata('last_tested_revision'))
+    self.assertIsNone(
+        updated_testcase.get_metadata('last_tested_crash_revision'))
+
+  def test_update_multiple_inconsistent(self):
+    """Test an update that that has multiple trials with inconsistent results
+    (some crashed, some did not)."""
+    stacktrace = self._read_test_data('asan_uaf.txt')
+    stacktraces = ["", stacktrace.decode(), ""]
+    stacktraces_bytes = json.dumps(stacktraces).encode()
+    self.app.post(
+        '/external-update',
+        params=self._make_message(stacktraces_bytes, {
+            'testcaseId': self.testcase.key.id(),
+            'revision': '1337'
+        }),
+        headers={'Authorization': 'Bearer fake'},
+        content_type='application/octet-stream')
+
+    updated_testcase = self.testcase.key.get()
+    self.assertTrue(updated_testcase.open)
+    self.assertEqual('', updated_testcase.fixed)
+    self.assertEqual(stacktrace.decode(),
+                     updated_testcase.last_tested_crash_stacktrace)
+    self.assertEqual(1337,
+                     updated_testcase.get_metadata('last_tested_revision'))
+    self.assertEqual(
+        1337, updated_testcase.get_metadata('last_tested_crash_revision'))
+
+  def test_update_multiple_crashed(self):
+    """Test an update that has multiple trials and all crashed."""
+    stacktrace = self._read_test_data('asan_uaf.txt')
+    stacktraces = [stacktrace.decode()]*3
+    stacktraces_bytes = json.dumps(stacktraces).encode()
+    self.app.post(
+        '/external-update',
+        params=self._make_message(stacktraces_bytes, {
+            'testcaseId': self.testcase.key.id(),
+            'revision': '1337'
+        }),
+        headers={'Authorization': 'Bearer fake'},
+        content_type='application/octet-stream')
+
+    updated_testcase = self.testcase.key.get()
+    self.assertTrue(updated_testcase.open)
+    self.assertEqual('', updated_testcase.fixed)
+    self.assertEqual(stacktrace.decode(),
+                     updated_testcase.last_tested_crash_stacktrace)
+    self.assertEqual(1337,
+                     updated_testcase.get_metadata('last_tested_revision'))
+    self.assertEqual(
+        1337, updated_testcase.get_metadata('last_tested_crash_revision'))
