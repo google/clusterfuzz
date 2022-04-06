bdbfbb057e918ae578492dc291c8e1d7e6e685d1
Alan32Liu <donggeliu@google.com>
2022-04-06 17:49:32 +1000

    Remove old tests as they have been replaced by new ones

--- src/clusterfuzz/_internal/tests/appengine/libs/issue_filer_test.py
+++ src/clusterfuzz/_internal/tests/appengine/libs/issue_filer_test.py
@@ -23,7 +23,6 @@ import mock
 import parameterized
 import six
 
-from clusterfuzz._internal.datastore import data_handler
 from clusterfuzz._internal.datastore import data_types
 from clusterfuzz._internal.google_cloud_utils import pubsub
 from clusterfuzz._internal.tests.test_libs import helpers
@@ -32,7 +31,6 @@ from clusterfuzz._internal.tests.test_libs import test_utils
 from libs.issue_management import issue_filer
 from libs.issue_management import issue_tracker_policy
 from libs.issue_management import monorail
-from libs.issue_management import oss_fuzz_github
 from libs.issue_management.issue_tracker import LabelStore
 from libs.issue_management.monorail.issue import Issue as MonorailIssue
 
@@ -195,32 +193,6 @@ QUESTIONS_NOTE = (
     'other feedback, please file an issue at '
     'https://github.com/google/oss-fuzz/issues.')
 
-TESTCASE_REPORT_URL = 'https://{domain}/testcase?key={testcase_id}'
-
-MONORAIL_URL = (
-    'https://bugs.chromium.org/p/oss-fuzz/issues/detail?id={bug_information}')
-
-OSS_FUZZ_ISSUE_URL = 'https://github.com/google/oss-fuzz/issues/new'
-
-GITHUB_ISSUE_TITTLE_TEXT = 'OSS-Fuzz issue {bug_information}'
-
-GITHUB_ISSUE_CONTENT_TEXT = (
-    'OSS-Fuzz has found a bug in this project. Please see '
-    f'{TESTCASE_REPORT_URL} '
-    'for details and reproducers.'
-    '\n\n'
-    'This issue is mirrored from '
-    f'{MONORAIL_URL} '
-    'and will auto-close if the status changes there.'
-    '\n\n'
-    'If you have trouble accessing this report, '
-    f'please file an issue at {OSS_FUZZ_ISSUE_URL}.'
-    '\n')
-
-GITHUB_ISSUE_CLOSE_COMMENT_TEXT = ('OSS-Fuzz has closed this bug. Please see '
-                                   f'{MONORAIL_URL} '
-                                   'for details.')
-
 
 class IssueTrackerManager(object):
   """Mock issue tracker manager."""
@@ -304,16 +276,6 @@ class IssueFilerTests(unittest.TestCase):
 
     self.testcase7 = data_types.Testcase(job_type='ios_job4', **testcase_args)
     self.testcase7.put()
-
-    self.testcases = [
-        self.testcase1,
-        self.testcase2,
-        self.testcase3,
-        self.testcase4,
-        self.testcase5,
-        self.testcase6,
-        self.testcase7,
-    ]
 
     data_types.ExternalUserPermission(
         email='user@example.com',
@@ -681,32 +643,6 @@ class IssueFilerTests(unittest.TestCase):
     issue_filer.file_issue(self.testcase1, issue_tracker)
     self.assertIn('Target: target, Project: proj',
                   issue_tracker._itm.last_issue.body)
-
-  def test_github_issue_title(self):
-    """Test the title format of new GitHub issues."""
-    for testcase in self.testcases:
-      actual_title = oss_fuzz_github.get_issue_title(testcase)
-      expected_title = GITHUB_ISSUE_TITTLE_TEXT.format(
-          bug_information=testcase.bug_information)
-      self.assertEqual(actual_title, expected_title)
-
-  def test_github_issue_body(self):
-    """Test the body format of new GitHub issues."""
-    for testcase in self.testcases:
-      actual_body = oss_fuzz_github.get_issue_body(testcase)
-      expected_body = GITHUB_ISSUE_CONTENT_TEXT.format(
-          domain=data_handler.get_domain(),
-          testcase_id=testcase.key.id(),
-          bug_information=testcase.bug_information)
-      self.assertEqual(actual_body, expected_body)
-
-  def test_github_issue_close(self):
-    """Test the closing message format of GitHub issues filed."""
-    for testcase in self.testcases:
-      actual_comment = oss_fuzz_github.get_issue_close_comment(testcase)
-      expected_comment = GITHUB_ISSUE_CLOSE_COMMENT_TEXT.format(
-          bug_information=testcase.bug_information)
-      self.assertEqual(actual_comment, expected_comment)
 
 
 class MemoryToolLabelsTest(unittest.TestCase):
