895df4ae2b1fc37dae1b82248941a5a145c68978
Alan32Liu <donggeliu@google.com>
2022-06-17 15:26:31 +1000

    Capture shell bugs detected by extra sanitisers.

--- src/clusterfuzz/stacktraces/__init__.py
+++ src/clusterfuzz/stacktraces/__init__.py
@@ -823,6 +823,10 @@ class StackParser:
           new_state='',
           reset=True)
 
+      # Shell bugs detected by extra sanitisers.
+      self.update_state_on_match(
+          EXTRA_SANITIZERS_SHELL_BUG_REGEX, line, state, new_type='Shell bug')
+
       # For KASan crashes, additional information about a bad access may come
       # from a later line. Update the type and address if this happens.
       update_kasan_crash_details(state, line)
