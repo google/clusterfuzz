9661be526824997c365b96ca2f8190f86cef58f2
Dongge Liu <donggeliu@google.com>
2022-08-11 14:51:53 +1000

    Support parsing arbitrary file open (#2759)
    * Capture arbitrary file open
    
    * Test arbitrary file open
    
    * Label bug as security and experimental
    
    * Test no filing experimental bug

--- src/clusterfuzz/stacktraces/__init__.py
+++ src/clusterfuzz/stacktraces/__init__.py
@@ -830,6 +830,13 @@ class StackParser:
           state,
           new_type='Command injection')
 
+      # Arbitrary file open detected by extra sanitizers.
+      self.update_state_on_match(
+          EXTRA_SANITIZERS_ARBITRARY_FILE_OPEN_REGEX,
+          line,
+          state,
+          new_type='Arbitrary file open')
+
       # For KASan crashes, additional information about a bad access may come
       # from a later line. Update the type and address if this happens.
       update_kasan_crash_details(state, line)
