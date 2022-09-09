30f96a9f68e4755e9a27f451256046b9a2d5e277
Oliver Chang <oliverchang@users.noreply.github.com>
2022-09-09 13:33:47 +1000

    Work around IAM policy issue. (#2776)
    Context:
    https://github.com/google/clusterfuzz/issues/2775#issuecomment-1241442478

--- src/appengine/handlers/cron/project_setup.py
+++ src/appengine/handlers/cron/project_setup.py
@@ -652,8 +652,12 @@ class ProjectSetup(object):
 
   def _create_service_accounts_and_buckets(self, project, info):
     """Create per-project service account and buckets."""
-    service_account = service_accounts.get_or_create_service_account(project)
-    service_accounts.set_service_account_roles(service_account)
+    service_account, exists = service_accounts.get_or_create_service_account(
+        project)
+    if not exists:
+      # TODO(ochang): Temporary hack to get around
+      # https://github.com/google/clusterfuzz/issues/2775.
+      service_accounts.set_service_account_roles(service_account)
 
     # Create GCS buckets.
     backup_bucket_name = self._backup_bucket_name(project)
