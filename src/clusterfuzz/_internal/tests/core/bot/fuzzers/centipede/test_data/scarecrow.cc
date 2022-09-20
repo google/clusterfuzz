a15f8694a2a5ac8d5b4dec74735bcc42af48b917
Alan32Liu <donggeliu@google.com>
2022-09-20 15:10:25 +1000

    Remove redundant testcases

--- src/clusterfuzz/_internal/tests/core/bot/fuzzers/centipede/test_data/scarecrow.cc
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <stddef.h>
-#include <stdlib.h>
-
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  char *s = (char *) malloc(size);
-  return 0;
-}
-
