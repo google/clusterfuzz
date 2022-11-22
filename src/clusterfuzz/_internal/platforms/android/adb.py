ed25b7957850fea1d02d47b1939763a88d5678fd
David Fu <davfu@google.com>
2022-11-21 16:44:41 -0800

    Added additional spaces to fix lint issue in adb.py.

--- src/clusterfuzz/_internal/platforms/android/adb.py
+++ src/clusterfuzz/_internal/platforms/android/adb.py
@@ -733,7 +733,7 @@ def stop_shell():
 def time_since_last_reboot():
   """Return time in seconds since last reboot."""
   uptime_string = run_shell_command(['cat', '/proc/uptime']).split(
-    ' ', maxsplit=1)[0]
+      ' ', maxsplit=1)[0]
   try:
     return float(uptime_string)
   except ValueError:
