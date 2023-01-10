a507bd352519af5197be0ccc2d76497700359d8f
Jonathan Metzman <metzman@chromium.org>
2023-01-10 12:38:45 -0500

    Update package version

--- src/setup.py
+++ src/setup.py
@@ -19,7 +19,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='clusterfuzz',
-    version='2.5.8',
+    version='2.5.9',
     author='ClusterFuzz authors',
     author_email='clusterfuzz-dev@googlegroups.com',
     description='ClusterFuzz',
