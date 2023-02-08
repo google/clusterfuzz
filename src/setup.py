5b2e3493abe0a125785e31ff0fd026d5db9c6f44
Dongge Liu <donggeliu@google.com>
2023-02-08 11:31:24 +1100

    Pump CF version

--- src/setup.py
+++ src/setup.py
@@ -19,7 +19,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='clusterfuzz',
-    version='2.5.9',
+    version='2.6.0',
     author='ClusterFuzz authors',
     author_email='clusterfuzz-dev@googlegroups.com',
     description='ClusterFuzz',
