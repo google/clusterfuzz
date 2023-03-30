94e017a50be468ae6acab7d05372a69a0d8364d4
davfu1 <118572048+davfu1@users.noreply.github.com>
2023-03-30 09:57:16 -0700

    Update adb.py

--- src/clusterfuzz/_internal/platforms/android/adb.py
+++ src/clusterfuzz/_internal/platforms/android/adb.py
@@ -225,8 +225,8 @@ def get_adb_path():
 def get_device_state():
   """Return the device status."""
   if 'is-ramdump-mode: yes' in run_fastboot_command(
-    ['getvar', 'is-ramdump-mode']):
-  return 'is-ramdump-mode: yes'
+      ['getvar', 'is-ramdump-mode']):
+    return 'is-ramdump-mode: yes'
   state_cmd = get_adb_command_line('get-state')
   return execute_command(state_cmd, timeout=RECOVERY_CMD_TIMEOUT)
 
