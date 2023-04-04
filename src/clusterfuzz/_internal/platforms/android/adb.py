8e13ed9b5f1bab1d35ff168473c51617a36b4b91
davfu1 <118572048+davfu1@users.noreply.github.com>
2023-04-03 21:56:11 -0700

    Added Trusty ramdump logcat extraction (#2987)
    Trusty fuzzing sometimes causes devices to crash into ramdump state.
    This PR enables Clusterfuzz to download `kernel.log` from ramdumps and
    to desymbolize it into a logcat using the `ramoops_reader.py` script.
    The desymbolized logcat will contain a Kernel Panic stacktrace. This PR
    adds the KP stacktrace and logcat to the crash output, and resets the
    device.

--- src/clusterfuzz/_internal/platforms/android/adb.py
+++ src/clusterfuzz/_internal/platforms/android/adb.py
@@ -25,6 +25,7 @@ import time
 
 from clusterfuzz._internal.base import persistent_cache
 from clusterfuzz._internal.base import utils
+from clusterfuzz._internal.google_cloud_utils import storage
 from clusterfuzz._internal.metrics import logs
 from clusterfuzz._internal.system import environment
 from clusterfuzz._internal.system import shell
@@ -57,6 +58,8 @@ LSUSB_SERIAL_RE = re.compile(r'\s+iSerial\s+\d\s+(.*)')
 
 # This is a constant value defined in usbdevice_fs.h in Linux system.
 USBDEVFS_RESET = ord('U') << 8 | 20
+
+RAMOOPS_READER_GCS_PATH = 'gs://haiku-storage/trusty/ramoops_reader.py'
 
 
 def bad_state_reached():
@@ -224,6 +227,11 @@ def get_adb_path():
 
 def get_device_state():
   """Return the device status."""
+  fastboot_state = run_fastboot_command(
+      ['getvar', 'is-ramdump-mode'], timeout=RECOVERY_CMD_TIMEOUT)
+  if fastboot_state and 'is-ramdump-mode: yes' in fastboot_state:
+    return 'is-ramdump-mode:yes'
+
   state_cmd = get_adb_command_line('get-state')
   return execute_command(state_cmd, timeout=RECOVERY_CMD_TIMEOUT)
 
@@ -263,6 +271,29 @@ def get_kernel_log_content():
     kernel_log_content += read_data_from_file(kernel_log_file) or ''
 
   return kernel_log_content
+
+
+def extract_logcat_from_ramdump_and_reboot():
+  """Extracts logcat from ramdump kernel log and reboots."""
+  run_fastboot_command(['oem', 'ramdump', 'stage_file', 'kernel.log'])
+  run_fastboot_command(['get_staged', 'kernel.log'])
+
+  storage.copy_file_from(RAMOOPS_READER_GCS_PATH, 'ramoops_reader.py')
+  subprocess.run(
+      'python ramoops_reader.py kernel.log > logcat.log',
+      shell=True,
+      check=False)
+  with open('logcat.log', 'r') as file:
+    logcat = file.read()
+
+  files_to_delete = ['ramoops_reader.py', 'kernel.log', 'logcat.log']
+  for filepath in files_to_delete:
+    os.remove(filepath)
+
+  run_fastboot_command('reboot')
+  wait_until_fully_booted()
+
+  return logcat
 
 
 def get_ps_output():
@@ -818,6 +849,9 @@ def wait_until_fully_booted():
     time.sleep(BOOT_WAIT_INTERVAL)
 
   factory_reset()
+  if environment.is_android_emulator():
+    #Device may be in recovery mode
+    hard_reset()
   logs.log_fatal_and_exit(
       'Device failed to finish boot. Reset to factory settings and exited.')
 
