394616cc52afe152e7d26f3b6256a4ccc6dd4698
David Fu <davfu@google.com>
2023-04-06 13:38:28 -0700

    Added stacktrace regex for Trusty crashes

--- src/clusterfuzz/stacktraces/__init__.py
+++ src/clusterfuzz/stacktraces/__init__.py
@@ -67,6 +67,7 @@ class CrashInfo:
     self.found_python_crash = False
     self.found_golang_crash = False
     self.found_android_kernel_crash = False
+    self.is_trusty = False
 
 
 def _filter_stack_frame(stack_frame):
@@ -403,6 +404,7 @@ class StackParser:
     state.is_golang = '.go:' in stacktrace
     state.is_python = '.py", line' in stacktrace
     state.is_js = 'Uncaught Exception: Jazzer.js' in stacktrace
+    state.is_trusty = 'Trusted App crash stacktrace' in stacktrace
 
     # For Android LKL (and potentially kernel output), the KASAN crash may start
     # with the time since boot.  We need to remove this so that our regexes
@@ -645,6 +647,10 @@ class StackParser:
           state.crash_state = ''
           state.frame_count = 0
           continue
+
+      #Trusty stacktraces
+      if state.is_trusty:
+        state.crash_type = 'Trusted app crash'
 
       # Sanitizer SEGV crashes.
       segv_match = SAN_SEGV_REGEX.match(line)
@@ -1280,6 +1286,11 @@ class StackParser:
 
       if state.is_js and self.add_frame_on_match(
           JAZZER_JS_STACK_FRAME_FUNCTION_REGEX, line, state, group=1):
+        continue
+
+      # Trusty logcat stack frames
+      if state.is_trusty and self.add_frame_on_match(
+          TRUSTY_STACK_FRAME_REGEX, line, state, group=4):
         continue
 
     # Detect cycles in stack overflow bugs and update crash state.
