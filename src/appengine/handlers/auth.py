8deaa2c9e9f2db597a8ce158be59815da01efcee
Holly Gong <gongh@google.com>
2023-04-17 13:18:54 +1000

    fix bug and improve style.

--- src/appengine/handlers/auth.py
+++ src/appengine/handlers/auth.py
@@ -28,10 +28,11 @@ from libs import handler
 class Handler(base_handler.Handler):
   """Auth page."""
 
-  # pylint: disable=unused-argument
   @handler.get(handler.HTML)
   def get(self, extra=None):
     """Handle a get request."""
+    # We iuse `request.url` which is already the full URL.
+    del extra
     target_url = request.url.replace(auth.auth_domain(),
                                      auth.real_auth_domain(), 1)
     logging.info('Forwarding auth request to: %s', target_url)
