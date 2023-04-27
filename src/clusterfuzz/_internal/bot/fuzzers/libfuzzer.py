7f94ec184870762c04046b23c24ff1a0424133b4
Mark Teffeteller <markteffeteller@hotmail.com>
2023-04-27 17:31:34 -0500

    Fix issue searching for android dependencies  (#3020)
    Currently we are searching in /system/lib64 for dependencies but fuzzer
    deps get pushed to the /data/fuzz directory. Also clearing out
    downloaded deps before fuzzing

--- src/clusterfuzz/_internal/bot/fuzzers/libfuzzer.py
+++ src/clusterfuzz/_internal/bot/fuzzers/libfuzzer.py
@@ -808,13 +808,17 @@ class AndroidLibFuzzerRunner(new_process.UnicodeProcessRunner, LibFuzzerCommon):
     """Return a set of default arguments to pass to adb binary."""
     default_args = ['shell']
 
-    # Add directory containing libclang_rt.ubsan_standalone-aarch64-android.so
-    # to LD_LIBRARY_PATH.
+    # LD_LIBRARY_PATH set to search for fuzzer deps first, and then
+    # sanitizers if any are found.
     ld_library_path = ''
     if not android.settings.is_automotive():
       # TODO(MHA3): Remove this auto check.
-      ld_library_path = android.sanitizer.get_ld_library_path_for_sanitizers()
-    if ld_library_path:
+      executable_dir = os.path.dirname(executable_path)
+      deps_path = os.path.join(self._get_device_path(executable_dir), 'lib')
+      ld_library_path += deps_path
+      sanitizer_path = android.sanitizer.get_ld_library_path_for_sanitizers()
+      if sanitizer_path:
+        ld_library_path += ':' + sanitizer_path
       default_args.append('LD_LIBRARY_PATH=' + ld_library_path)
 
     # Add sanitizer options.
