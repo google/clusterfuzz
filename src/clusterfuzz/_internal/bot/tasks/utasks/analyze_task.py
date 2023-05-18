e299d2b71e544fa3f79ad3b8ebac46ba01bb7183
Jonathan Metzman <metzman@chromium.org>
2023-05-18 12:05:27 -0400

    fix typo

--- src/clusterfuzz/_internal/bot/tasks/utasks/analyze_task.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/analyze_task.py
@@ -343,7 +343,7 @@ def utask_main(testcase, testcase_id, testcase_download_url, job_type,
   # See if we have to ignore this crash.
   if crash_analyzer.ignore_stacktrace(state.crash_stacktrace):
     data_handler.close_invalid_uploaded_testcase(output.testcase,
-                                                 output.metadata, 'Irrelavant')
+                                                 output.metadata, 'Irrelevant')
 
   test_for_reproducibility(testcase, testcase_file_path, state, test_timeout)
   return uworker_io.UworkerOutput(
