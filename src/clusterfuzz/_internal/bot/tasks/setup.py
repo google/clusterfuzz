40b77a00fd9091838d2da50d73ac9e22b71fe79f
Jonathan Metzman <metzman@chromium.org>
2023-07-09 08:30:15 -0400

    fix

--- src/clusterfuzz/_internal/bot/tasks/setup.py
+++ src/clusterfuzz/_internal/bot/tasks/setup.py
@@ -264,7 +264,7 @@ def setup_testcase(testcase,
       return None, None, uworker_io.UworkerOutput(
           uworker_input=uworker_error_input,
           error_message=error_message,
-          uworker_msg_pb2.ErrorType.TESTCASE_SETUP_INVALID_FUZZER)
+          error=uworker_msg_pb2.ErrorType.TESTCASE_SETUP_INVALID_FUZZER)
 
     if not update_successful:
       error_message = f'Unable to setup fuzzer {fuzzer_name}'
