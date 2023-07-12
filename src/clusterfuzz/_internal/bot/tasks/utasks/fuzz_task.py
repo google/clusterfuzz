60b49f21ff3f826744d9ebd7a1dd4b993c5419f3
Jonathan Metzman <metzman@chromium.org>
2023-07-12 11:37:40 -0400

    fix

--- src/clusterfuzz/_internal/bot/tasks/utasks/fuzz_task.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/fuzz_task.py
@@ -1885,7 +1885,7 @@ class FuzzingSession:
     utils.python_gc()
 
     return uworker_io.UworkerOutput(
-        fuzz_task_output=uworker_io.UworkerOutput(
+        fuzz_task_output=uworker_io.FuzzTaskOutput(
             fully_qualified_fuzzer_name=self.fully_qualified_fuzzer_name,
             crash_revision=crash_revision,
             job_run_timestamp=time.time(),
