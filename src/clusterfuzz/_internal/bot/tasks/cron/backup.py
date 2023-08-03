bdbf0cef0784201cf9bacbe0c1037ce096a02a34
Holly Gong <39108850+hogo6002@users.noreply.github.com>
2023-08-03 11:35:47 +1000

    Add new entry point of k8s cron jobs. (#3262)
    1. Add run_cron.py to execute cron jobs.
    2. Move cron job[ backup.py
    ](https://github.com/google/clusterfuzz/blob/master/src/appengine/handlers/cron/backup.py)to
    bot folder.

---
+++ src/clusterfuzz/_internal/bot/tasks/cron/backup.py
@@ -0,0 +1,74 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Task that triggers the backup URL. We need this task because
+  https://cloud.google.com/appengine/articles/scheduled_backups doesn't support
+  backing up all entities. Therefore, we have to get all entities here and
+  send them to the backup URL."""
+
+import datetime
+
+from google.cloud import ndb
+from googleapiclient import discovery
+from googleapiclient import errors
+
+from clusterfuzz._internal.base import utils
+from clusterfuzz._internal.config import local_config
+from clusterfuzz._internal.metrics import logs
+
+# CrashStatistic is excluded because the number of records is too high and
+# can be rebuilt from BigQuery dataset.
+EXCLUDED_MODELS = {'CrashStatistic', 'CrashStatisticJobHistory'}
+
+
+def _datastore_client():
+  """Returns an api client for datastore."""
+  return discovery.build('datastore', 'v1')
+
+
+def main():
+  """Backups all entities in a datastore bucket."""
+  backup_bucket = local_config.Config(
+      local_config.PROJECT_PATH).get('backup.bucket')
+  if not backup_bucket:
+    logs.log_error('No backup bucket is set, skipping.')
+    return False
+
+  kinds = [
+      kind for kind in ndb.Model._kind_map  # pylint: disable=protected-access
+      if (not kind.startswith('_') and kind not in EXCLUDED_MODELS)
+  ]
+
+  app_id = utils.get_application_id()
+  timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%d-%H:%M:%S')
+  output_url_prefix = (
+      f'gs://testing-{backup_bucket}/datastore-backups/{timestamp}')
+
+  body = {
+      'output_url_prefix': output_url_prefix,
+      'entity_filter': {
+          'kinds': kinds
+      }
+  }
+
+  try:
+    request = _datastore_client().projects().export(projectId=app_id, body=body)
+    response = request.execute()
+    message = 'Datastore export succeeded.'
+    logs.log(message, response=response)
+    return True
+  except errors.HttpError as e:
+    status_code = e.resp.status
+    message = f'Datastore export failed. Status code: {status_code}'
+    logs.log_error(message)
+    return False
---
+++ src/python/bot/startup/run_cron.py
@@ -0,0 +1,57 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Starts the cron scripts."""
+
+# Before any other imports, we must fix the path. Some libraries might expect
+# to be able to import dependencies directly, but we must store these in
+# subdirectories of common so that they are shared with App Engine.
+from clusterfuzz._internal.base import modules
+
+modules.fix_module_search_paths()
+
+import importlib
+import sys
+
+from clusterfuzz._internal.config import local_config
+from clusterfuzz._internal.datastore import ndb_init
+from clusterfuzz._internal.system import environment
+
+CRON_TASKS = ['backup']
+
+
+def main():
+  """Runs the cron jobs"""
+  root_directory = environment.get_value('ROOT_DIR')
+  local_config.ProjectConfig().set_environment()
+
+  if not root_directory:
+    print('Please set ROOT_DIR environment variable to the root of the source '
+          'checkout before running. Exiting.')
+    print('For an example, check init.bash in the local directory.')
+    return 1
+
+  task = sys.argv[1]
+  if task not in CRON_TASKS:
+    print('Invalid task name. '
+          f'Please enter one of the following tasks: {CRON_TASKS}')
+    return 1
+
+  task_module_name = f'clusterfuzz._internal.bot.tasks.cron.{task}'
+  task_module = importlib.import_module(task_module_name)
+  return 0 if task_module.main() else 1
+
+
+if __name__ == '__main__':
+  with ndb_init.context():
+    sys.exit(main())
