42a3bac28e3beffed1df7d6bf2582780853cd510
Holly Gong <gongh@google.com>
2023-08-28 16:09:20 +1000

    disable backlash

--- src/local/butler/deploy.py
+++ src/local/butler/deploy.py
@@ -445,6 +445,7 @@ def _deploy_k8s():
   redis_host = _get_redis_ip(k8s_project)
   common.execute(f'export REDIS_HOST={redis_host}')
   for workload in workloads:
+    # pylint:disable=anomalous-backslash-in-string
     common.execute(f'envsubst \$REDIS_HOST < {workload} | kubectl apply -f -')
 
 
