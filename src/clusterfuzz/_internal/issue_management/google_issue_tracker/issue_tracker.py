ca5b9a768f119b0c822e7b25979c35e04a350411
Micah Bales <micahbales@google.com>
2023-10-16 21:32:01 +0000

    format

--- src/clusterfuzz/_internal/issue_management/google_issue_tracker/issue_tracker.py
+++ src/clusterfuzz/_internal/issue_management/google_issue_tracker/issue_tracker.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 # pylint: disable=protected-access
-
 """Google issue tracker implementation."""
 
 import datetime
