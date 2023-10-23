a2d0612d3c56f26476b12494d8fbaea5b5a70de8
Jonathan Metzman <metzman@chromium.org>
2023-10-23 16:59:56 -0400

    Fix docker build

--- docker/build.sh
+++ docker/build.sh
@@ -38,7 +38,7 @@ stamp=$GIT_HASH-$(date -u +%Y%m%d%H%M)
 for image in "${IMAGES[@]}"; do
   docker build -t $image ${image#gcr.io/clusterfuzz-images/}
   docker tag $image $image:$stamp
-  wait -n
+  wait
   docker_push &
 done
 
