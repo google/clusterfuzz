4c9ca5e17c5d95f58bd761a1cf2204f8ceb8d3be
Micah Bales <micahbales@google.com>
2023-10-26 08:48:12 -0700

    Issue Update Security Flow (Google Issue Tracker) (#3460)
    In https://github.com/google/clusterfuzz/pull/3442 we added a save-issue
    security flow. This PR tidies up and ensures that this flow also works
    for updating an issue.
    
    ---------
    
    Co-authored-by: Ravi Mistry <rmistry@google.com>

--- src/clusterfuzz/_internal/issue_management/google_issue_tracker/issue_tracker.py
+++ src/clusterfuzz/_internal/issue_management/google_issue_tracker/issue_tracker.py
@@ -108,10 +108,12 @@ class Issue(issue_tracker.Issue):
   def apply_extension_fields(self, extension_fields):
     """Applies _ext_ prefixed extension fields."""
     if extension_fields.get('_ext_collaborators'):
+      self._changed.add('_ext_collaborators')
       for collaborator in extension_fields['_ext_collaborators']:
         self._collaborators.add(collaborator)
-    self._issue_access_limit = extension_fields.get(
-        '_ext_issue_access_limit') or IssueAccessLevel.LIMIT_NONE
+    if extension_fields.get('_ext_issue_access_limit'):
+      self._changed.add('_issue_access_limit')
+      self._issue_access_limit = extension_fields.get('_ext_issue_access_limit')
 
   @property
   def issue_tracker(self):
--- src/clusterfuzz/_internal/tests/appengine/libs/issue_management/google_issue_tracker/google_issue_tracker_test.py
+++ src/clusterfuzz/_internal/tests/appengine/libs/issue_management/google_issue_tracker/google_issue_tracker_test.py
@@ -430,6 +430,110 @@ class GoogleIssueTrackerTest(unittest.TestCase):
         mock.call().execute(http=None, num_retries=3),
     ])
 
+  def test_update_issue_to_security(self):
+    """Test updating an existing issue."""
+    self.client.issues().get().execute.return_value = BASIC_ISSUE
+    self.client.issues().modify().execute.return_value = {
+        'issueId': '68828938',
+        'issueState': {
+            'componentId':
+                '9001',
+            'type':
+                'ASSIGNED',
+            'status':
+                'NEW',
+            'priority':
+                'P2',
+            'severity':
+                'S2',
+            'title':
+                'issue title2',
+            'access_limit': {
+                'access_level': issue_tracker.IssueAccessLevel.LIMIT_NONE
+            },
+            'reporter': {
+                'emailAddress': 'reporter@google.com',
+                'userGaiaStatus': 'ACTIVE',
+            },
+            'assignee': {
+                'emailAddress': 'assignee2@google.com',
+                'userGaiaStatus': 'ACTIVE',
+            },
+            'retention':
+                'COMPONENT_DEFAULT',
+            'ccs': [{
+                'emailAddress': 'cc@google.com',
+                'userGaiaStatus': 'ACTIVE'
+            },],
+            'hotlistIds': ['12345',],
+        },
+        'createdTime': '2019-06-25T01:29:30.021Z',
+        'modifiedTime': '2019-06-25T01:29:30.021Z',
+        'userData': {},
+        'accessLimit': {
+            'accessLevel': 'INTERNAL'
+        },
+        'lastModifier': {
+            'emailAddress': 'user1@google.com',
+            'userGaiaStatus': 'ACTIVE',
+        },
+    }
+    issue = self.issue_tracker.get_issue(68828938)
+
+    # Mimic issue_filer's action in setting up the issue
+    issue.apply_extension_fields(EXTENSION_FIELDS)
+
+    issue.reporter = 'reporter@google.com'
+    issue.assignee = 'assignee2@google.com'
+    issue.ccs.add('cc@google.com')
+    issue.components.add('9001')
+    issue.labels.add('12345')
+    issue.status = 'ASSIGNED'
+    issue.title = 'issue title2'
+    issue.save()
+    self.assertEqual(68828938, issue.id)
+    self.client.issues().modify.assert_has_calls([
+        mock.call(
+            issueId='68828938',
+            body={
+                'add': {
+                    'status':
+                        'ASSIGNED',
+                    'assignee': {
+                        'emailAddress': 'assignee2@google.com'
+                    },
+                    'reporter': {
+                        'emailAddress': 'reporter@google.com'
+                    },
+                    'title':
+                        'issue title2',
+                    'ccs': [{
+                        'emailAddress': 'cc@google.com'
+                    }],
+                    'collaborators': [
+                        {
+                            'emailAddress': 'superman@krypton.com'
+                        },
+                        {
+                            'emailAddress': 'batman@gotham.com'
+                        },
+                    ],
+                    'access_limit':
+                        issue_tracker.IssueAccessLevel.LIMIT_VIEW,
+                },
+                'addMask':
+                    'status,assignee,reporter,title,' +
+                    'ccs,collaborators,access_limit',
+                'remove': {},
+                'removeMask':
+                    '',
+                'significanceOverride':
+                    'MAJOR',
+            },
+        ),
+        mock.call().execute(num_retries=3, http=None),
+    ])
+
   def test_find_issues_url(self):
     """Test find_issues_url."""
     url = self.issue_tracker.find_issues_url(
