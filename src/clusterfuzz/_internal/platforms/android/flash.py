f0e060ed626f3f2f45eed0d12e1b42875df377e3
Mark <mteffeteller@google.com>
2023-11-01 20:12:23 +0000

    fix formatting

--- src/clusterfuzz/_internal/platforms/android/flash.py
+++ src/clusterfuzz/_internal/platforms/android/flash.py
@@ -66,7 +66,8 @@ def download_latest_build(build_info, image_regexes, image_directory):
   target = build_info['target']
   logs.log_warn('target stored in current build_info: %s.' % target)
   last_build_info = persistent_cache.get_value(constants.LAST_FLASH_BUILD_KEY)
-  logs.log_warn('last_build_info take from persisten cache: %s.' % last_build_info)
+  logs.log_warn(
+      'last_build_info take from persisten cache: %s.' % last_build_info)
   if last_build_info and last_build_info['bid'] == build_id:
     return
 
@@ -130,10 +131,12 @@ def flash_to_latest_build_if_needed():
       logs.log_warn('build_params found on device: %s.' % build_params)
       if environment.is_android_cuttlefish():
         target = build_params.get('target') + FLASH_DEFAULT_BUILD_TARGET
-        logs.log_warn('is_android_cuttlefish() returned True. Target: %s.' % target)
+        logs.log_warn(
+            'is_android_cuttlefish() returned True. Target: %s.' % target)
       else:
         target = build_params.get('target') + '-userdebug'
-        logs.log_warn('is_android_cuttlefish() returned False. Target: %s.' % target)
+        logs.log_warn(
+            'is_android_cuttlefish() returned False. Target: %s.' % target)
 
       # Cache target in environment. This is also useful for cases when
       # device is bricked and we don't have this information available.
