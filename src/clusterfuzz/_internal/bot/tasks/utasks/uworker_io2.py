ed59efd653b3599deaff4c7fb9fa1d99b456e7cf
Titouan Rigoudy <titouan@google.com>
2023-11-04 12:46:18 +0100

    Fix lint errors.

--- src/clusterfuzz/_internal/bot/tasks/utasks/uworker_io2.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/uworker_io2.py
@@ -1,3 +1,16 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """TODO(titouan)
 
 ...
@@ -7,38 +20,41 @@ import abc
 import dataclasses
 import json
 from typing import Any
-from typing import Dict
 from typing import Generic
 from typing import List
 from typing import Tuple
-from typing import Type
 from typing import TypeVar
-from typing import Union
 
 from google.cloud import ndb
 from google.cloud.datastore_v1.proto import entity_pb2
 from google.cloud.ndb import model  # pyright: ignore[reportMissingModuleSource]
 from google.protobuf import message
 
-from clusterfuzz._internal.datastore import data_types
+#from clusterfuzz._internal.datastore import data_types
 #from clusterfuzz._internal.metrics import logs
 from clusterfuzz._internal.protos import uworker_msg_pb2
 
 T = TypeVar('T', bound=message.Message)
 
 
-# WIP, seems too complicated
+# WIP, seems too complicated, abandoned for now.
 class ComplexProtoConvertible(Generic[T], abc.ABC):
+  """WIP attempt to define a superclass that would generate proto conversion
+  automatically based on proto and dataclass field descriptors.
+
+  Seems too complicated, abandoned for now.
+  """
 
   @abc.abstractmethod
   def _make_proto(self) -> T:
-    pass
+    """TODO."""
 
   @abc.abstractmethod
   def _fields(self) -> Tuple[dataclasses.Field, ...]:
-    pass
+    """TODO."""
 
   def copy_to_proto(self) -> T:
+    """TODO."""
     proto = self._make_proto()
 
     fields = self._fields()
@@ -58,9 +74,11 @@ class ComplexProtoConvertible(Generic[T], abc.ABC):
     return proto
 
   def copy_from_proto(self, proto: T):
-    return
+    """TODO."""
+    del proto
 
 
+# TODO: Is this useful at all?
 class ProtoConvertible(Generic[T], abc.ABC):
 
   @abc.abstractmethod
@@ -69,42 +87,55 @@ class ProtoConvertible(Generic[T], abc.ABC):
 
   @classmethod
   @abc.abstractmethod
-  def from_proto(cls, T) -> "ProtoConvertible[T]":
+  def from_proto(cls, proto: T) -> "ProtoConvertible[T]":
     pass
 
 
 def json_to_proto(value: Any) -> uworker_msg_pb2.Json:
+  """TODO
+  """
   serialized = json.dumps(value)
   return uworker_msg_pb2.Json(serialized=serialized)
 
 
 def json_from_proto(proto: uworker_msg_pb2.Json) -> Any:
+  """TODO
+  """
   return json.loads(proto.serialized)
 
 
 def model_to_proto(mdl: ndb.Model) -> entity_pb2.Entity:
-  return model._entity_to_protobuf(mdl)  # pyright: ignore
+  """TODO
+  """
+  return model._entity_to_protobuf(mdl)  # pyright: ignore # pylint: disable=protected-access
 
 
 def model_from_proto(entity: entity_pb2.Entity) -> ndb.Model:
-  return model._entity_from_protobuf(entity)  # pyright: ignore
+  """TODO
+  """
+  return model._entity_from_protobuf(entity)  # pyright: ignore # pylint: disable=protected-access
 
 
 @dataclasses.dataclass
 class AnalyzeTaskInput(ProtoConvertible[uworker_msg_pb2.AnalyzeTaskInput]):
-  """Input for analyze_task.uworker_main."""
+  """Input for `analyze_task.uworker_main()`.
+
+  See `uworker_msg_pb2.AnalyzeTaskInput` for field documentation.
+  """
+
   bad_revisions: List[int]
 
   def to_proto(self) -> uworker_msg_pb2.AnalyzeTaskInput:
+    """TODO.
+    """
     return uworker_msg_pb2.AnalyzeTaskInput(bad_revisions=self.bad_revisions)
 
   @classmethod
   def from_proto(cls,
                  proto: uworker_msg_pb2.AnalyzeTaskInput) -> "AnalyzeTaskInput":
+    """TODO.
+    """
     return cls(bad_revisions=list(proto.bad_revisions))
-
-
-_input = AnalyzeTaskInput([])
 
 
 @dataclasses.dataclass
@@ -114,5 +145,6 @@ class AnalyzeTaskOutput(ProtoConvertible[uworker_msg_pb2.AnalyzeTaskOutput]):
     return uworker_msg_pb2.AnalyzeTaskOutput()
 
   @classmethod
-  def from_proto(cls, _proto: uworker_msg_pb2.AnalyzeTaskOutput):
+  def from_proto(cls, proto: uworker_msg_pb2.AnalyzeTaskOutput):
+    del proto
     return cls()
