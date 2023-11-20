9f70318177b7b76b73c8da6cceed8039e6ee3277
Jonathan Metzman <metzman@chromium.org>
2023-11-20 12:19:42 -0500

    Fix tests

--- src/clusterfuzz/_internal/tests/core/base/tasks_test.py
+++ src/clusterfuzz/_internal/tests/core/base/tasks_test.py
@@ -88,7 +88,7 @@ class GetUtaskFiltersTest(unittest.TestCase):
     # TOOD(metzman): Delete this test when it is no longer needed.
     filters = tasks.get_utask_filters(is_chromium=True, is_linux=True)
     self.assertEqual(
-        filters, 'attribute.name = analyze AND attribute.name = postprocess')
+        filters, 'attribute.name = analyze AND attribute.name = postprocess AND attribute.name = variant')
 
   def test_chromium_nonlinux(self):
     """Tests that the get_utask_filters only has linux bots in chrome
@@ -98,7 +98,7 @@ class GetUtaskFiltersTest(unittest.TestCase):
     filters = tasks.get_utask_filters(is_chromium=True, is_linux=False)
     # Analyze will be begun only on Linux bots.
     self.assertEqual(
-        filters, '-attribute.name = analyze AND -attribute.name = postprocess')
+        filters, '-attribute.name = analyze AND -attribute.name = postprocess AND -attribute.name = variant')
 
   def test_external_linux(self):
     """Tests that the get_utask_filters only has linux bots in chrome
