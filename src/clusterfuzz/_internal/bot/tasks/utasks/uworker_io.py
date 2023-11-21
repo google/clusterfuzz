dcd9de24aff891ec1487419b9d3cd38ec54fc021
Ali HIJAZI <ahijazi@google.com>
2023-11-21 15:07:22 +0100

    [Variant] Don't pass the testcase and variant back thru UworkerOutput (#3513)

--- src/clusterfuzz/_internal/bot/tasks/utasks/uworker_io.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/uworker_io.py
@@ -533,6 +533,11 @@ class RegressionTaskOutput(UworkerMsg):  # pylint: disable=abstract-method
   PROTO_CLS = uworker_msg_pb2.RegressionTaskOutput
 
 
+class VariantTaskOutput(UworkerMsg):  # pylint: disable=abstract-method
+  """Output from variant_task.uworker_main."""
+  PROTO_CLS = uworker_msg_pb2.VariantTaskOutput
+
+
 class DeserializedUworkerMsg:
 
   def __init__(self, testcase=None, error_type=None, **kwargs):
