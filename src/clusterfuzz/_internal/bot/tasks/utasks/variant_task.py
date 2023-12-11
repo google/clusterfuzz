c315c46b7ebd50e6a095f8e07c7e4d9a38fc0a6f
Jonathan Metzman <metzman@chromium.org>
2023-12-11 02:33:47 -0500

    Fix last PR

--- src/clusterfuzz/_internal/bot/tasks/utasks/variant_task.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/variant_task.py
@@ -76,7 +76,6 @@ def utask_preprocess(testcase_id, job_type, uworker_env):
       job_type=job_type,
       original_job_type=original_job_type,
       testcase=uworker_io.entity_to_protobuf(testcase),
-      testcase_upload_metadata=),
       uworker_env=uworker_env,
       testcase_id=testcase_id,
       setup_input=setup_input,
@@ -85,7 +84,7 @@ def utask_preprocess(testcase_id, job_type, uworker_env):
       data_types.TestcaseUploadMetadata.testcase_id == int(testcase_id)).get()
   if testcase_upload_metadata:
     uworker_input.testcase_upload_metadata = uworker_io.entity_to_protobuf(
-      testcase_upload_metadata)
+        testcase_upload_metadata)
   return uworker_input
 
 
