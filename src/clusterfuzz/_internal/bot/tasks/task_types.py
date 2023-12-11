696fa53a211b31c8ac26aa340cf4512174968099
Jonathan Metzman <metzman@chromium.org>
2023-12-11 15:24:43 -0500

    Revert "Reenable batch execution of variant task (#3561)"
    This reverts commit 1ee7fbc87b0e2b1325e90d209fb57772feae0d37.

--- src/clusterfuzz/_internal/bot/tasks/task_types.py
+++ src/clusterfuzz/_internal/bot/tasks/task_types.py
@@ -168,7 +168,7 @@ COMMAND_TYPES = {
     'unpack': TrustedTask,
     'postprocess': PostprocessTask,
     'uworker_main': UworkerMainTask,
-    'variant': UTask,
+    'variant': UTaskLocalExecutor,
 }
 
 
