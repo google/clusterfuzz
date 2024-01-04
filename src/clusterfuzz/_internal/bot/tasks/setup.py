77631ea8c2db4e891336461a269fd3278acb0b05
Jonathan Metzman <metzman@chromium.org>
2024-01-04 09:26:58 -0500

    Improve batch

--- src/clusterfuzz/_internal/bot/tasks/setup.py
+++ src/clusterfuzz/_internal/bot/tasks/setup.py
@@ -207,7 +207,9 @@ def preprocess_setup_testcase(testcase, fuzzer_override=None):
   """Preprocessing for setup_testcase function."""
   fuzzer_name = fuzzer_override or testcase.fuzzer_name
   testcase_id = testcase.key.id()
-  if fuzzer_name:
+  if fuzzer_name and not with_deps:
+    logs.log('Skipping fuzzer preprocess.')
+  if fuzzer_name and with_deps:
     # This branch is taken when we assume fuzzer needs to be set up for a
     # testcase to be executed (i.e. when a testcase was found by a fuzzer).
     # It's not the case for testcases uploaded by users.
