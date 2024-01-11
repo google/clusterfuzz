8ca18b18db378797ea8d883dd1333ebbb71d5314
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2024-01-11 11:34:54 -0500

    Fix error scheduling variant for mac (#3628)
    Fixes: https://github.com/google/clusterfuzz/issues/3576

--- src/clusterfuzz/_internal/tests/core/bot/tasks/task_types_test.py
+++ src/clusterfuzz/_internal/tests/core/bot/tasks/task_types_test.py
@@ -16,7 +16,9 @@ import unittest
 from unittest import mock
 
 from clusterfuzz._internal.bot.tasks import task_types
+from clusterfuzz._internal.datastore import data_types
 from clusterfuzz._internal.tests.test_libs import helpers
+from clusterfuzz._internal.tests.test_libs import test_utils
 
 
 class UTaskCombinedTest(unittest.TestCase):
@@ -29,3 +31,38 @@ class UTaskCombinedTest(unittest.TestCase):
         'clusterfuzz._internal.bot.tasks.task_types.is_remotely_executing_utasks',
         return_value=True):
       self.assertTrue(task_types.UTaskCombined.is_execution_remote())
+
+
+@test_utils.with_cloud_emulators('datastore')
+class IsRemoteUtaskTest(unittest.TestCase):
+  """Tests for is_remote_utask."""
+
+  def setUp(self):
+    helpers.patch_environ(self)
+
+  def test_mac(self):
+    job_name = 'libfuzzer_mac_asan'
+
+    with mock.patch(
+        'clusterfuzz._internal.bot.tasks.task_types.is_remotely_executing_utasks',
+        return_value=True):
+      data_types.Job(name=job_name, platform='MAC').put()
+      self.assertFalse(task_types.is_remote_utask('variant', job_name))
+
+  def test_linux(self):
+    job_name = 'libfuzzer_linux_asan'
+
+    with mock.patch(
+        'clusterfuzz._internal.bot.tasks.task_types.is_remotely_executing_utasks',
+        return_value=True):
+      data_types.Job(name=job_name, platform='LINUX').put()
+      self.assertTrue(task_types.is_remote_utask('variant', job_name))
+
+  def test_trusted(self):
+    job_name = 'libfuzzer_linux_asan'
+
+    with mock.patch(
+        'clusterfuzz._internal.bot.tasks.task_types.is_remotely_executing_utasks',
+        return_value=True):
+      data_types.Job(name=job_name, platform='LINUX').put()
+      self.assertFalse(task_types.is_remote_utask('impact', job_name))
