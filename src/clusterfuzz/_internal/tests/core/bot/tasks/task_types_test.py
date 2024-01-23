325db6e486312f9d9f2e32ad7557513a2063fa2a
Jonathan Metzman <metzman@chromium.org>
2024-01-22 21:19:18 -0500

    Undo batched execution

--- src/clusterfuzz/_internal/tests/core/bot/tasks/task_types_test.py
+++ src/clusterfuzz/_internal/tests/core/bot/tasks/task_types_test.py
@@ -21,18 +21,6 @@ from clusterfuzz._internal.tests.test_libs import helpers
 from clusterfuzz._internal.tests.test_libs import test_utils
 
 
-class UTaskCombinedTest(unittest.TestCase):
-
-  def setUp(self):
-    helpers.patch_environ(self)
-
-  def test_is_remote(self):
-    with mock.patch(
-        'clusterfuzz._internal.bot.tasks.task_types.is_remotely_executing_utasks',
-        return_value=True):
-      self.assertTrue(task_types.UTaskCombined.is_execution_remote())
-
-
 @test_utils.with_cloud_emulators('datastore')
 class IsRemoteUtaskTest(unittest.TestCase):
   """Tests for is_remote_utask."""
