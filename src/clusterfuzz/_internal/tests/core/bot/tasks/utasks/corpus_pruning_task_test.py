df0dc092d0f2568a6f5e42b5e19db1c1f149c597
Jonathan Metzman <metzman@chromium.org>
2024-01-24 15:05:53 -0500

    Fix

--- src/clusterfuzz/_internal/tests/core/bot/tasks/utasks/corpus_pruning_task_test.py
+++ src/clusterfuzz/_internal/tests/core/bot/tasks/utasks/corpus_pruning_task_test.py
@@ -513,8 +513,7 @@ class CorpusPruningTestUntrusted(
                      testcases[0].get_metadata('fuzzer_binary_name'))
 
     self.mock.add_task.assert_has_calls([
-        mock.call('minimize', testcases[0].key.id(), 'libfuzzer_asan_job',
-                  None),
+        mock.call('minimize', testcases[0].key.id(), 'libfuzzer_asan_job'),
     ])
 
     today = datetime.datetime.utcnow().date()
