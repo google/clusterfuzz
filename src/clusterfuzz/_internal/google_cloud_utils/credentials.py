fecb9382374f79f0ab91740e837c5666526296f4
Jonathan Metzman <metzman@chromium.org>
2024-02-12 21:25:38 -0500

    Fix signing creds

--- src/clusterfuzz/_internal/google_cloud_utils/credentials.py
+++ src/clusterfuzz/_internal/google_cloud_utils/credentials.py
@@ -16,8 +16,10 @@ import os
 
 from google.auth import compute_engine
 from google.auth import credentials
+from google.auth import impersonated_credentials
 from google.auth.transport import requests
 from google.oauth2 import service_account
+
 
 from clusterfuzz._internal.base import retry
 from clusterfuzz._internal.system import environment
@@ -57,10 +59,10 @@ def get_default(scopes=None):
   return google.auth.default(scopes=scopes)
 
 
-@retry.wrap(
-    retries=FAIL_RETRIES,
-    delay=FAIL_WAIT,
-    function='google_cloud_utils.credentials.get_signing_credentials')
+# @retry.wrap(
+#     retries=FAIL_RETRIES,
+#     delay=FAIL_WAIT,
+#     function='google_cloud_utils.credentials.get_signing_credentials')
 def get_signing_credentials():
   """Returns signing credentials for signing URLs."""
   if _use_anonymous_credentials():
@@ -68,16 +70,18 @@ def get_signing_credentials():
 
   google_application_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS',
                                              None)
-  if google_application_credentials is None:
-    # The normal case, when we are on GCE.
-    creds, _ = get_default()
-    request = requests.Request()
-    creds.refresh(request)
-    signing_creds = compute_engine.IDTokenCredentials(
-        request, '', service_account_email=creds.service_account_email)
-  else:
+  if google_application_credentials is not None:
     # Handle cases like android and Mac where bots are run outside of Google
     # Cloud Platform and don't have access to metadata server.
-    signing_creds = service_account.Credentials.from_service_account_file(
-        google_application_credentials)
-  return signing_creds
+    creds = service_account.Credentials.from_service_account_file(
+        google_application_credentials, scopes=['https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/prodxmon'])
+  else:
+    # The normal case, when we are on GCE.
+    creds, _ = get_default()
+
+  request = requests.Request()
+  creds.refresh(request)
+
+  signing_creds = compute_engine.IDTokenCredentials(
+    request, '', service_account_email=creds.service_account_email)
+  return signing_creds, creds.token
