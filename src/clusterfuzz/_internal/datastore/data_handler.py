1d84ab5754ee92a21bdb335a1ab29c4b6e37a3f9
Jonathan Metzman <metzman@chromium.org>
2024-02-14 16:30:58 -0500

    fix

--- src/clusterfuzz/_internal/datastore/data_handler.py
+++ src/clusterfuzz/_internal/datastore/data_handler.py
@@ -1595,8 +1595,7 @@ FUZZ_TARGET_UPDATE_FAIL_DELAY = 2
 
 def record_fuzz_target(engine_name, binary_name, job_type):
   """Records exsistence of fuzz target to the DB."""
-  result = record_fuzz_targets(
-      engine_name, [binary_name], job_type)[0]
+  result = record_fuzz_targets(engine_name, [binary_name], job_type)[0]
 
   project = get_project_name(job_type)
   key_name = data_types.fuzz_target_fully_qualified_name(
