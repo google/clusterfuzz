bb1c43f2ea45c804d04577ffbcd2b4ece9e6e313
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2024-02-15 16:29:23 -0500

    Make ProtoFuzzTargetCorpus inherit from FuzzTargetCorpus. (#3761)
    Do this so that it inherits properties for use during backup.

--- src/clusterfuzz/_internal/fuzzing/corpus_manager.py
+++ src/clusterfuzz/_internal/fuzzing/corpus_manager.py
@@ -357,7 +357,7 @@ class FuzzTargetCorpus(GcsCorpus):
     return result
 
 
-class ProtoFuzzTargetCorpus(GcsCorpus):
+class ProtoFuzzTargetCorpus(FuzzTargetCorpus):
   """Implementation of GCS corpus that uses protos (uworker-compatible) for fuzz
   targets."""
 
