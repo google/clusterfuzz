2c0364094bc470a31538a07cba993bbd0b6052b8
Jonathan Metzman <metzman@chromium.org>
2024-02-22 12:29:49 -0500

    fix typo

--- src/clusterfuzz/_internal/base/tasks.py
+++ src/clusterfuzz/_internal/base/tasks.py
@@ -394,10 +394,9 @@ class PubSubTask(Task):
         self.attribute('command'), self.attribute('argument'),
         self.attribute('job'))
 
-    items = self._pubsub_message.items() or []
     self.extra_info = {
         key: value
-        for key, value in items
+        for key, value in self._pubsub_message.attributes.items()
         if key not in {'command', 'argument', 'job'}
     }
 
