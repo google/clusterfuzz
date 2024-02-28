4de1e736b0ea89c492c5c3b58a6f998de2813d7a
Jonathan Metzman <metzman@chromium.org>
2024-02-28 15:47:54 -0500

    Dont repeat deserialization

--- src/clusterfuzz/_internal/bot/tasks/utasks/fuzz_task.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/fuzz_task.py
@@ -1775,10 +1775,9 @@ class FuzzingSession:
 
     self.testcase_directory = environment.get_value('FUZZ_INPUTS')
 
-    fuzz_target = self.uworker_input.fuzz_task_input.fuzz_target
-    if fuzz_target:
+    if self.fuzz_target:
       logs.log('Setting fuzz target {fuzz_target}.')
-      environment.set_value('FUZZ_TARGET', fuzz_target.binary)
+      environment.set_value('FUZZ_TARGET', self.fuzz_target.binary)
     build_setup_result = build_manager.setup_build(
         environment.get_value('APP_REVISION'))
 
