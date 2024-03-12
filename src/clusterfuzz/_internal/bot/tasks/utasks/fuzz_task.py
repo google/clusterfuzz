cc81ff2c298f421ad93e66bbedd529b35da5b986
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2024-03-12 15:29:03 -0400

    Make LSAN blacklist uworker safe (#3844)

--- src/clusterfuzz/_internal/bot/tasks/utasks/fuzz_task.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/fuzz_task.py
@@ -1766,9 +1766,11 @@ class FuzzingSession:
     failure_wait_interval = environment.get_value('FAIL_WAIT')
 
     # Update LSAN local blacklist with global blacklist.
-    is_lsan_enabled = environment.get_value('LSAN')
-    if is_lsan_enabled:
-      leak_blacklist.copy_global_to_local_blacklist()
+    global_blacklisted_functions = (
+        self.uworker_input.fuzz_task_input.global_blacklisted_functions)
+    if global_blacklisted_functions:
+      leak_blacklist.copy_global_to_local_blacklist(
+          global_blacklisted_functions)
 
     # Ensure that that the fuzzer still exists.
     logs.log('Setting up fuzzer and data bundles.')
@@ -2054,6 +2056,12 @@ def utask_preprocess(fuzzer_name, job_type, uworker_env):
         uworker_io.entity_to_protobuf(fuzz_target))
 
   preprocess_store_fuzzer_run_results(fuzz_task_input)
+
+  if environment.get_value('LSAN'):
+    # Copy global blacklist into local suppressions file if LSan is enabled.
+    fuzz_task_input.global_blacklisted_functions.extend(
+        leak_blacklist.get_global_blacklisted_functions())
+
   return uworker_msg_pb2.Input(
       fuzz_task_input=fuzz_task_input,
       job_type=job_type,
