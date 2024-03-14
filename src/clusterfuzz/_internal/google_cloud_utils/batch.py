378302d8300d4420028b2fd5d9b61391300db6a9
Jonathan Metzman <metzman@chromium.org>
2024-03-14 16:28:45 -0400

    Fix variable name

--- src/clusterfuzz/_internal/google_cloud_utils/batch.py
+++ src/clusterfuzz/_internal/google_cloud_utils/batch.py
@@ -273,7 +273,7 @@ def _get_spec_from_config(command, job_name):
     config_name += '-NONPREEMPTIBLE'
   # TODO(metzman): Get rid of this when we stop doing privileged operations in
   # utasks.
-  if command in UNPRIVILEGED_TASKS:
+  if command in _UNPRIVILEGED_TASKS:
     config_name += '-UNPRIVILEGED'
   batch_config = _get_batch_config()
   instance_spec = batch_config.get('mapping').get(config_name, None)
