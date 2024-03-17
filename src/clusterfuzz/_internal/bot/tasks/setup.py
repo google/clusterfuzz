9de1dbced428999ceeac3f813a99e42fb541e99a
Jonathan Metzman <metzman@chromium.org>
2024-03-16 21:40:31 -0400

    Fix

--- src/clusterfuzz/_internal/bot/tasks/setup.py
+++ src/clusterfuzz/_internal/bot/tasks/setup.py
@@ -147,7 +147,7 @@ def _setup_memory_tools_environment(testcase):
     environment.set_memory_tool_options(options_name, options_value)
 
 
-def prepare_environment_for_testcase(testcase, job_type, task_name):
+def prepare_environment_for_testcase(testcase):
   """Set various environment variables based on the test case."""
   _setup_memory_tools_environment(testcase)
 
@@ -237,7 +237,8 @@ def preprocess_setup_testcase(testcase,
   # Override APP_ARGS with minimized arguments (if available). Don't do this
   # for variant task since other job types can have its own set of required
   # arguments, so use the full set of arguments of that job.
-  app_args = _get_application_arguments(testcase, uworker_env['JOB_NAME'], task_name)
+  app_args = _get_application_arguments(testcase, uworker_env['JOB_NAME'],
+                                        uworker_env['TASK_NAME'])
   if app_args:
     environment.set_value('APP_ARGS', app_args, uworker_env)
   return setup_input
@@ -295,8 +296,7 @@ def setup_testcase(testcase: data_types.Testcase, job_type: str,
     leak_blacklist.copy_global_to_local_blacklist(
         setup_input.global_blacklisted_functions, excluded_testcase=testcase)
 
-  task_name = environment.get_value('TASK_NAME')
-  prepare_environment_for_testcase(testcase, job_type, task_name)
+  prepare_environment_for_testcase(testcase)
 
   return file_list, testcase_file_path, None
 
