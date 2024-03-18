9b5a3a467152ad6a8dd898c0cc7381381ec52a5b
Jonathan Metzman <metzman@chromium.org>
2024-03-18 17:58:41 -0400

    Run regression and minimize without privileges

--- src/clusterfuzz/_internal/google_cloud_utils/batch.py
+++ src/clusterfuzz/_internal/google_cloud_utils/batch.py
@@ -57,7 +57,7 @@ BatchWorkloadSpec = collections.namedtuple('BatchWorkloadSpec', [
     'machine_type',
 ])
 
-_UNPRIVILEGED_TASKS = {'analyze', 'variant', 'symbolize'}
+_UNPRIVILEGED_TASKS = {'analyze', 'symbolize', 'regression', 'minimize', 'variant'}
 
 
 def _create_batch_client_new():
