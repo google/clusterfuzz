4867a3b9f71068edfa808d9a9ec283a83bd18a51
Paul Semel <paulsemel@google.com>
2024-03-19 16:30:56 +0100

    Reland "[build_manager] introduce BuildArchive (#3713)" (#3891)
    This new class removes some of the burden build_manager.py was initially
    doing. Introducing this class has a few advantages:
    - Better testing of the unpacking feature. This will not be entangled in
    the build manager anymore, so this makes it easier to unittest.
    - Custom build handling. This is true for the current introduction of
    .runtime_deps, but this could also be true for e.g. listing targets
    via a file contained in the archive.
    - Reduce the amount of responsibility of the build_manager.py class.
    This also reduces what the tests are required to be testing.
    
    ---------
    
    Co-authored-by: jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>

--- src/clusterfuzz/_internal/platforms/android/flash.py
+++ src/clusterfuzz/_internal/platforms/android/flash.py
@@ -87,7 +87,7 @@ def download_latest_build(build_info, image_regexes, image_directory):
     for file_path in image_file_paths:
       if file_path.endswith('.zip') or file_path.endswith('.tar.gz'):
         with archive.open(file_path) as reader:
-          archive.unpack(reader, image_directory)
+          reader.extract_all(image_directory)
 
 
 def flash_to_latest_build_if_needed():
