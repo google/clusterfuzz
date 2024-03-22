358d2b0b5904e0bf1b979ef51858527e767ae5ea
Pâris Meuleman <pmeuleman@google.com>
2024-03-22 09:56:26 +0100

    improve log and use f-strings

--- src/clusterfuzz/_internal/platforms/android/sanitizer.py
+++ src/clusterfuzz/_internal/platforms/android/sanitizer.py
@@ -57,13 +57,13 @@ def get_options_file_path(sanitizer_tool_name):
   # Reduce the log severity for HWASAN
   # TODO(crbug.com/329346758) properly handle HWASAN options.
   if sanitizer_tool_name.lower() == "hwasan":
-    logs.log('Unsupported sanitizer: ' + sanitizer_tool_name)
+    logs.log(f'Unable to set options for sanitizer: {sanitizer_tool_name}')
     return None
 
   sanitizer_filename = SANITIZER_TOOL_TO_FILE_MAPPINGS.get(
       sanitizer_tool_name.lower())
   if sanitizer_filename is None:
-    logs.log_error('Unsupported sanitizer: ' + sanitizer_tool_name)
+    logs.log_error(f'Unable to set options for sanitizer: {sanitizer_tool_name}')
     return None
 
   return os.path.join(sanitizer_directory, sanitizer_filename)
