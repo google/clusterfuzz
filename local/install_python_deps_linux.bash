7e1f73ad656f35d6418da2df75acde49edd8eb87
Titouan Rigoudy <titouan@google.com>
2024-03-26 15:16:09 +0100

    Update pipenv.

--- local/install_python_deps_linux.bash
+++ local/install_python_deps_linux.bash
@@ -16,7 +16,7 @@
 
 # Setup pipenv and install python dependencies.
 echo If this fails, you may need to build older Python from source
-$PYTHON -m pip install --user pipenv==2022.8.5
+$PYTHON -m pip install --user pipenv==2023.12.1
 if $PYTHON -m pipenv --venv > /dev/null 2>&1; then
   # Remove existing pipenv virtual environment.
   $PYTHON -m pipenv --rm
