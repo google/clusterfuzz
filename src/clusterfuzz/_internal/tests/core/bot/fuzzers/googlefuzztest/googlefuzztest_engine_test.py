8675c59892cd8a4368db41cff5adf5ec47687f81
Vitor Guidi <vitorguidi@google.com>
2024-05-02 12:15:03 -0300

    Properly handling temporary dir creation to not break integ tests

--- src/clusterfuzz/_internal/tests/core/bot/fuzzers/googlefuzztest/googlefuzztest_engine_test.py
+++ src/clusterfuzz/_internal/tests/core/bot/fuzzers/googlefuzztest/googlefuzztest_engine_test.py
@@ -14,7 +14,6 @@
 """Tests for googlefuzztest engine."""
 # pylint: disable=unused-argument
 import os
-import shutil
 import tempfile
 import unittest
 
@@ -31,17 +30,17 @@ class GoogleFuzzTestUnitTests(unittest.TestCase):
   """Tests to make sure the fuzzing engine correctly handles logging by passing correct abseil flags"""
 
   def setUp(self):
-    self.temp_dir = tempfile.gettempdir()
+    self.temp_dir = tempfile.TemporaryDirectory()
 
   def tearDown(self):
-    shutil.rmtree(self.temp_dir, ignore_errors=True)
+    self.temp_dir.cleanup()
 
   def test_googlefuzztest_invoked_with_low_log_volume(self):
     """Test if we call fuzztest with the correct abseil flags to reduce logging volume."""
     engine_impl = engine.Engine()
     target_path = engine_common.find_fuzzer_path(DATA_DIR, NO_OP_TEST_SUFFIX)
     options = engine_impl.prepare(None, target_path, DATA_DIR)
-    results = engine_impl.fuzz(target_path, options, self.temp_dir, 10)
+    results = engine_impl.fuzz(target_path, options, self.temp_dir.name, 10)
 
     self.assertIn('--logtostderr', results.command)
     self.assertIn('--minloglevel=3', results.command)
