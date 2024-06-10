5c71fe570291a5ccabc93e06d3bad9643f9b80e2
Vitor Guidi <vitorguidi@google.com>
2024-06-10 12:57:44 +0000

    Updating local scripts to reference python 3.11

--- src/local/butler/deploy.py
+++ src/local/butler/deploy.py
@@ -476,8 +476,8 @@ def _deploy_k8s(config_dir):
 def execute(args):
   """Deploy Clusterfuzz to Appengine."""
   if sys.version_info.major != 3 or sys.version_info.minor != 7:
-    print('You can only deploy from Python 3.7. Install Python 3.7 and '
-          'run: `PYTHON=python3.7 local/install_deps.bash`')
+    print('You can only deploy from Python 3.11. Install Python 3.11 and '
+          'run: `PYTHON=python3.11 local/install_deps.bash`')
     sys.exit(1)
 
   os.environ['ROOT_DIR'] = '.'
