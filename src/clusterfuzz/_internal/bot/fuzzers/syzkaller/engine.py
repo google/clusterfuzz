5b4ab276142865cd2db903f0ad773b4a6d9bb4aa
Jonathan Metzman <metzman@chromium.org>
2024-06-20 16:57:54 -0400

    Revert "Disable --coverfile flag in Syzkaller for Cuttlefish (#2376)"
    This reverts commit 0a91a4046df1b0afc68c119fb2157a79476fdeeb.

--- src/clusterfuzz/_internal/bot/fuzzers/syzkaller/engine.py
+++ src/clusterfuzz/_internal/bot/fuzzers/syzkaller/engine.py
@@ -40,7 +40,7 @@ class SyzkallerOptions(engine.FuzzOptions):
 
   def __init__(self, corpus_dir, arguments, strategies, fuzz_corpus_dirs,
                extra_env):
-    super().__init__(corpus_dir, arguments, strategies)
+    super(SyzkallerOptions, self).__init__(corpus_dir, arguments, strategies)
     self.fuzz_corpus_dirs = fuzz_corpus_dirs
     self.extra_env = extra_env
 
@@ -151,11 +151,7 @@ class Engine(engine.Engine):
     self._create_temp_corpus_dir('new')
 
     args = options.arguments
-
-    # TODO(yanghuiz): Dump coverfile from Syzkaller HTTP endpoint and
-    # remove this.
-    if not environment.is_android_cuttlefish():
-      args += ['--coverfile', runner.get_cover_file_path()]
+    args += ['--coverfile', runner.get_cover_file_path()]
 
     self.init_corpus(options.corpus_dir, runner.get_work_dir())
     fuzz_result = syzkaller_runner.fuzz(max_time, additional_args=args)
