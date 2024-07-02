774e6769d0f074615098b6fef4e5726a8803b5d3
Vitor Guidi <vitorguidi@google.com>
2024-07-02 18:58:52 +0000

    Removing some other leftover references to py37

--- src/local/butler/deploy.py
+++ src/local/butler/deploy.py
@@ -462,7 +462,7 @@ def _deploy_k8s(config_dir):
 
 def execute(args):
   """Deploy Clusterfuzz to Appengine."""
-  if sys.version_info.major != 3 or sys.version_info.minor != 7:
+  if sys.version_info.major != 3 or sys.version_info.minor != 11:
     print('You can only deploy from Python 3.11. Install Python 3.11 and '
           'run: `PYTHON=python3.11 local/install_deps.bash`')
     sys.exit(1)
