42b60180fe7401d14512559457d7573470bf4c10
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2024-07-04 08:58:44 -0400

    Fix comment formatting (#4105)

--- src/clusterfuzz/_internal/bot/tasks/utasks/fuzz_task.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/fuzz_task.py
@@ -905,6 +905,8 @@ def create_testcase(group: uworker_msg_pb2.FuzzTaskCrashGroup,
                     fully_qualified_fuzzer_name: str):
   """Create a testcase based on crash."""
   crash = group.main_crash
+  comment = (f'Fuzzer {fully_qualified_fuzzer_name} generated testcase '
+             f'crashed in {crash.crash_time} seconds (r{crash.crash_revision})')
   testcase_id = data_handler.store_testcase(
       crash=crash,
       fuzzed_keys=crash.fuzzed_key or None,
@@ -913,9 +915,7 @@ def create_testcase(group: uworker_msg_pb2.FuzzTaskCrashGroup,
       fixed=get_fixed_or_minimized_key(group.one_time_crasher_flag),
       one_time_crasher_flag=group.one_time_crasher_flag,
       crash_revision=uworker_output.fuzz_task_output.crash_revision,
-      comment='Fuzzer %s generated testcase crashed in %d seconds (r%d)' %
-      (fully_qualified_fuzzer_name, crash.crash_time,
-       uworker_output.fuzz_task_output.crash_revision),
+      comment=comment,
       absolute_path=crash.absolute_path,
       fuzzer_name=uworker_input.fuzzer_name,
       fully_qualified_fuzzer_name=fully_qualified_fuzzer_name,
