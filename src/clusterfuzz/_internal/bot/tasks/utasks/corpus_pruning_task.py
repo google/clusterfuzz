google/clusterfuzz

commit 180453c9c24338dfcd2fb476ecb93b6738251a31

Store the backup corpus using a signed url (#3946)

src/clusterfuzz/_internal/bot/tasks/utasks/corpus_pruning_task.py
@@ -199,23 +199,16 @@ class Context:
         '%s_bad_units' % self.fuzz_target.project_qualified_name())
     self.merge_tmp_dir = self._create_temp_corpus_directory('merge_workdir')
 
-    if uworker_input is not None:
-      self.corpus = corpus_manager.ProtoFuzzTargetCorpus(
-          self.fuzz_target.engine, self.fuzz_target.project_qualified_name(),
-          uworker_input.corpus_pruning_task_input.corpus)
-      self.quarantine_corpus = corpus_manager.ProtoFuzzTargetCorpus(
-          self.fuzz_target.engine, self.fuzz_target.project_qualified_name(),
-          uworker_input.corpus_pruning_task_input.quarantine_corpus)
-    else:
-      # Delete this branch after we get rid of untrusted runnner.
-      self.corpus = corpus_manager.FuzzTargetCorpus(
-          self.fuzz_target.engine,
-          self.fuzz_target.project_qualified_name(),
-          include_regressions=True)
-      self.quarantine_corpus = corpus_manager.FuzzTargetCorpus(
-          self.fuzz_target.engine,
-          self.fuzz_target.project_qualified_name(),
-          quarantine=True)
+    self.corpus = corpus_manager.ProtoFuzzTargetCorpus(
+        self.fuzz_target.engine, self.fuzz_target.project_qualified_name(),
+        uworker_input.corpus_pruning_task_input.corpus)
+    self.quarantine_corpus = corpus_manager.ProtoFuzzTargetCorpus(
+        self.fuzz_target.engine, self.fuzz_target.project_qualified_name(),
+        uworker_input.corpus_pruning_task_input.quarantine_corpus)
+    self.dated_backup_gcs_url = (
+        uworker_input.corpus_pruning_task_input.dated_backup_gcs_url)
+    self.dated_backup_signed_url = (
+        uworker_input.corpus_pruning_task_input.dated_backup_signed_url)
 
   def restore_quarantined_units(self):
     """Restore units from the quarantine."""
@@ -645,9 +638,11 @@ def do_corpus_pruning(context, revision):
                                         'regressions')
   if shell.get_directory_file_count(regressions_input_dir):
     shutil.copytree(regressions_input_dir, regressions_output_dir)
-  backup_bucket = environment.get_value('BACKUP_BUCKET')
-  corpus_backup_url = corpus_manager.backup_corpus(
-      backup_bucket, context.corpus, context.minimized_corpus_path)
+  backup_succeeded = corpus_manager.backup_corpus(
+      context.dated_backup_signed_url, context.corpus,
+      context.minimized_corpus_path)
+  corpus_backup_location = (
+      context.dated_backup_gcs_url if backup_succeeded else None)
   shell.remove_directory(regressions_output_dir)
 
   minimized_corpus_size_units = shell.get_directory_file_count(
@@ -684,7 +679,7 @@ def do_corpus_pruning(context, revision):
   coverage_info.corpus_size_bytes = minimized_corpus_size_bytes
   coverage_info.quarantine_size_units = quarantine_corpus_size
   coverage_info.quarantine_size_bytes = quarantine_corpus_dir_size
-  coverage_info.corpus_backup_location = corpus_backup_url
+  coverage_info.corpus_backup_location = corpus_backup_location
   coverage_info.corpus_location = context.corpus.get_gcs_url()
   coverage_info.quarantine_location = context.quarantine_corpus.get_gcs_url()
 
@@ -1025,7 +1020,8 @@ def utask_main(uworker_input):
             coverage_info=_extract_coverage_information(context, result),
             fuzzer_binary_name=result.fuzzer_binary_name,
             crash_revision=result.revision,
-            crashes=_extract_corpus_crashes(result)),
+            crashes=_extract_corpus_crashes(result),
+            corpus_backup_uploaded=bool(result.coverage_info.corpus_location)),
         issue_metadata=issue_metadata)
     _fill_cross_pollination_stats(result.cross_pollination_stats,
                                   uworker_output)
@@ -1045,6 +1041,30 @@ def handle_corpus_pruning_failures(output: uworker_msg_pb2.Output):
   task_name = (f'corpus_pruning_{output.uworker_input.fuzzer_name}_'
                f'{output.uworker_input.job_type}')
   data_handler.update_task_status(task_name, data_types.TaskState.ERROR)
+
+
+def _create_backup_urls(fuzz_target: data_types.FuzzTarget,
+                        corpus_pruning_task_input):
+  """Creates the backup urls if a backup bucket is provided."""
+  backup_bucket_name = environment.get_value('BACKUP_BUCKET')
+  if not backup_bucket_name:
+    logs.log('No backup bucket provided, corpus backup will be skipped.')
+    return
+
+  timestamp = str(utils.utcnow().date())
+  dated_backup_gcs_url = corpus_manager.gcs_url_for_backup_file(
+      backup_bucket_name, fuzz_target.engine,
+      fuzz_target.project_qualified_name(), timestamp)
+  latest_backup_gcs_url = corpus_manager.gcs_url_for_backup_file(
+      backup_bucket_name, fuzz_target.engine,
+      fuzz_target.project_qualified_name(),
+      corpus_manager.LATEST_BACKUP_TIMESTAMP)
+
+  dated_backup_signed_url = storage.get_signed_upload_url(dated_backup_gcs_url)
+
+  corpus_pruning_task_input.dated_backup_gcs_url = dated_backup_gcs_url
+  corpus_pruning_task_input.latest_backup_gcs_url = latest_backup_gcs_url
+  corpus_pruning_task_input.dated_backup_signed_url = dated_backup_signed_url
 
 
 def utask_preprocess(fuzzer_name, job_type, uworker_env):
@@ -1098,6 +1118,8 @@ def utask_preprocess(fuzzer_name, job_type, uworker_env):
       corpus_crashes_blob_name=corpus_crashes_blob_name,
       corpus_crashes_upload_url=corpus_crashes_upload_url)
 
+  _create_backup_urls(fuzz_target, corpus_pruning_task_input)
+
   if environment.get_value('LSAN'):
     # Copy global blacklist into local suppressions file if LSan is enabled.
     setup_input.global_blacklisted_functions.extend(
@@ -1119,6 +1141,26 @@ _ERROR_HANDLER = uworker_handle_errors.CompositeErrorHandler({
 })
 
 
+def _update_latest_backup(output):
+  """Updates the latest_backup with the dated_backup uploaded in utask_main
+  if any."""
+  if not output.corpus_pruning_task_output.corpus_backup_uploaded:
+    return
+
+  dated_backup_gcs_url = (
+      output.uworker_input.corpus_pruning_task_input.dated_backup_gcs_url)
+  latest_backup_gcs_url = (
+      output.uworker_input.corpus_pruning_task_input.latest_backup_gcs_url)
+
+  try:
+    if not storage.copy_blob(dated_backup_gcs_url, latest_backup_gcs_url):
+      logs.log_error('backup_corpus: Failed to update latest corpus backup at '
+                     f'{latest_backup_gcs_url}.')
+  except:
+    logs.log_error('backup_corpus: Failed to update latest corpus backup at '
+                   f'{latest_backup_gcs_url}.')
+
+
 def utask_postprocess(output):
   """Trusted: Handles errors and writes anything needed to the db."""
   if output.error_type != uworker_msg_pb2.ErrorType.NO_ERROR:
@@ -1127,6 +1169,7 @@ def utask_postprocess(output):
   task_name = (f'corpus_pruning_{output.uworker_input.fuzzer_name}_'
                f'{output.uworker_input.job_type}')
 
+  _update_latest_backup(output)
   _record_cross_pollination_stats(output)
   _save_coverage_information(output)
   _process_corpus_crashes(output)

src/clusterfuzz/_internal/fuzzing/corpus_manager.py
@@ -474,50 +474,40 @@ def gcs_url_for_backup_file(backup_bucket_name, fuzzer_name,
   return f'{backup_dir.rstrip("/")}/{backup_file}'
 
 
-def backup_corpus(backup_bucket_name, corpus, directory):
+def backup_corpus(dated_backup_signed_url, corpus, directory):
   """Archive and store corpus as a backup.
 
   Args:
-    backup_bucket_name: Backup bucket.
+    dated_backup_signed_url: Signed url to upload the backup.
     corpus: uworker_msg.FuzzTargetCorpus.
     directory: Path to directory to be archived and backuped.
 
   Returns:
     The backup GCS url, or None on failure.
   """
-  logs.log(f'Backing up corpus {backup_bucket_name} {corpus} {directory}')
-  # TODO(metzman): Make this safe to run on a uworker.
-  if not backup_bucket_name:
-    logs.log('No backup bucket provided, skipping corpus backup.')
-    return None
+  logs.log(f'Backing up corpus {corpus} {directory}')
+  if not dated_backup_signed_url:
+    logs.log('No backup url provided, skipping corpus backup.')
+    return False
 
-  dated_backup_url = None
   timestamp = str(utils.utcnow().date())
 
   # The archive path for shutil.make_archive should be without an extension.
   backup_archive_path = os.path.join(
       os.path.dirname(os.path.normpath(directory)), timestamp)
+
+  backup_succeeded = True
   try:
     backup_archive_path = shutil.make_archive(backup_archive_path,
                                               BACKUP_ARCHIVE_FORMAT, directory)
-    dated_backup_url = gcs_url_for_backup_file(
-        backup_bucket_name, corpus.engine, corpus.project_qualified_target_name,
-        timestamp)
-
-    if not storage.copy_file_to(backup_archive_path, dated_backup_url):
-      return None
-
-    latest_backup_url = gcs_url_for_backup_file(
-        backup_bucket_name, corpus.engine, corpus.project_qualified_target_name,
-        LATEST_BACKUP_TIMESTAMP)
-
-    if not storage.copy_blob(dated_backup_url, latest_backup_url):
-      logs.log_error('backup_corpus: Failed to update latest corpus backup at '
-                     f'{latest_backup_url}.')
+    with open(backup_archive_path, 'rb') as fp:
+      data = fp.read()
+      if not storage.upload_signed_url(data, dated_backup_signed_url):
+        return False
   except Exception as ex:
+    backup_succeeded = False
     logs.log_error(
         f'backup_corpus failed: {ex}\n',
-        backup_bucket_name=backup_bucket_name,
         directory=directory,
         backup_archive_path=backup_archive_path)
 
@@ -525,7 +515,7 @@ def backup_corpus(backup_bucket_name, corpus, directory):
     # Remove backup archive.
     shell.remove_file(backup_archive_path)
 
-  return dated_backup_url
+  return backup_succeeded
 
 
 def gcs_url_for_backup_directory(backup_bucket_name, fuzzer_name,

src/clusterfuzz/_internal/protos/uworker_msg.proto
@@ -100,6 +100,9 @@ message CorpusPruningTaskInput {
   optional FuzzTargetCorpus quarantine_corpus = 5;
   optional string corpus_crashes_blob_name = 6;
   optional string corpus_crashes_upload_url = 7;
+  optional string dated_backup_gcs_url = 8;
+  optional string latest_backup_gcs_url = 9;
+  optional string dated_backup_signed_url = 10;
 }
 
 // TODO(metzman): Handle None in protobuf. Right now, it's hard to tell if a
@@ -282,6 +285,7 @@ message CorpusPruningTaskOutput {
   optional string fuzzer_binary_name = 3;
   optional int32 crash_revision = 4;
   repeated CrashInfo crashes = 5;
+  optional bool corpus_backup_uploaded = 6;
 }
 
 enum ErrorType {

src/clusterfuzz/_internal/tests/core/bot/tasks/utasks/corpus_pruning_task_test.py
@@ -81,7 +81,7 @@ class BaseTest:
     self.mock.proto_rsync_from_disk.side_effect = self._mock_rsync_from_disk
     self.mock.update_fuzzer_and_data_bundles.return_value = True
     self.mock.preprocess_update_fuzzer_and_data_bundles.return_value = None
-    self.mock.backup_corpus.return_value = 'backup_link'
+    self.mock.backup_corpus.return_value = True
 
     def mocked_unpack_seed_corpus_if_needed(*args, **kwargs):
       """Mock's assert called methods are not powerful enough to ensure that
@@ -175,6 +175,7 @@ class CorpusPruningTest(unittest.TestCase, BaseTest):
     self.mock.setup_build.side_effect = self._mock_setup_build
     self.mock.get_application_id.return_value = 'project'
     self.maxDiff = None
+    self.backup_bucket = os.environ['BACKUP_BUCKET'] or ''
 
   def test_preprocess_existing_task_running(self):
     """Preprocess test when another task is running."""
@@ -250,7 +251,7 @@ class CorpusPruningTest(unittest.TestCase, BaseTest):
     self.assertDictEqual(
         {
             'corpus_backup_location':
-                'backup_link',
+                uworker_input.corpus_pruning_task_input.dated_backup_gcs_url,
             'corpus_location':
                 'gs://bucket/libFuzzer/test_fuzzer/',
             'corpus_size_bytes':
@@ -287,7 +288,8 @@ class CorpusPruningTest(unittest.TestCase, BaseTest):
   def test_get_libfuzzer_flags(self):
     """Test get_libfuzzer_flags logic."""
     fuzz_target = data_handler.get_fuzz_target('libFuzzer_test_fuzzer')
-    context = corpus_pruning_task.Context(None, fuzz_target, [])
+    context = corpus_pruning_task.Context(uworker_msg_pb2.Input(), fuzz_target,
+                                          [])
 
     runner = corpus_pruning_task.Runner(self.build_dir, context)
     flags = runner.get_libfuzzer_flags()

src/clusterfuzz/_internal/tests/core/fuzzing/corpus_manager_test.py
@@ -313,7 +313,7 @@ class CorpusBackupTest(fake_filesystem_unittest.TestCase):
 
   def _mock_make_archive(self, archive_path, backup_format, _):
     path = archive_path + '.' + backup_format
-    self.fs.create_file(path)
+    self.fs.create_file(file_path=path, contents='archive contents...')
 
     return path
 
@@ -329,14 +329,12 @@ class CorpusBackupTest(fake_filesystem_unittest.TestCase):
 
     test_helpers.patch(self, [
         'clusterfuzz._internal.base.utils.utcnow',
-        'clusterfuzz._internal.google_cloud_utils.storage.copy_blob',
-        'clusterfuzz._internal.google_cloud_utils.storage.copy_file_to',
+        'clusterfuzz._internal.google_cloud_utils.storage.upload_signed_url',
         'multiprocessing.cpu_count',
         'shutil.make_archive',
     ])
 
-    self.mock.copy_blob.return_value = True
-    self.mock.copy_file_to.return_value = True
+    self.mock.upload_signed_url.return_value = True
     self.mock.cpu_count.return_value = 2
     self.mock.make_archive.side_effect = self._mock_make_archive
     self.mock.utcnow.return_value = datetime.datetime(2017, 1, 1)
@@ -345,17 +343,10 @@ class CorpusBackupTest(fake_filesystem_unittest.TestCase):
     """Test backup_corpus."""
     libfuzzer_corpus = corpus_manager.FuzzTargetCorpus('libFuzzer', 'fuzzer')
 
-    corpus_manager.backup_corpus('backup_bucket', libfuzzer_corpus, '/dir')
+    corpus_manager.backup_corpus('signed_upload_url', libfuzzer_corpus, '/dir')
 
-    self.mock.copy_file_to.assert_has_calls([
-        mock.call('/2017-01-01.zip',
-                  'gs://backup_bucket/corpus/libFuzzer/fuzzer/2017-01-01.zip')
-    ])
-
-    self.mock.copy_blob.assert_has_calls([
-        mock.call('gs://backup_bucket/corpus/libFuzzer/fuzzer/2017-01-01.zip',
-                  'gs://backup_bucket/corpus/libFuzzer/fuzzer/latest.zip'),
-    ])
+    self.mock.upload_signed_url.assert_has_calls(
+        [mock.call(b'archive contents...', 'signed_upload_url')])
 
 
 class FileMixin:

commit 4d7389e47b8b2dcfaa818665f090aff5ccf065db

Take CORPUS_FUZZER_NAME_OVERRIDE into account when generating backup signed urls
This fixes the issue introduced in
https://github.com/google/clusterfuzz/pull/3946/files#diff-4e357c1cb796c84a085445e5c0182c2a0bc5d46c0271c1fe594a7d41947ed9d8L511

src/clusterfuzz/_internal/bot/tasks/utasks/corpus_pruning_task.py
@@ -1056,12 +1056,13 @@ def _create_backup_urls(fuzz_target: data_types.FuzzTarget,
     return
 
   timestamp = str(utils.utcnow().date())
+  engine_name = environment.get_value('CORPUS_FUZZER_NAME_OVERRIDE',
+                                      fuzz_target.engine)
   dated_backup_gcs_url = corpus_manager.gcs_url_for_backup_file(
-      backup_bucket_name, fuzz_target.engine,
-      fuzz_target.project_qualified_name(), timestamp)
+      backup_bucket_name, engine_name, fuzz_target.project_qualified_name(),
+      timestamp)
   latest_backup_gcs_url = corpus_manager.gcs_url_for_backup_file(
-      backup_bucket_name, fuzz_target.engine,
-      fuzz_target.project_qualified_name(),
+      backup_bucket_name, engine_name, fuzz_target.project_qualified_name(),
       corpus_manager.LATEST_BACKUP_TIMESTAMP)
 
   dated_backup_signed_url = storage.get_signed_upload_url(dated_backup_gcs_url)