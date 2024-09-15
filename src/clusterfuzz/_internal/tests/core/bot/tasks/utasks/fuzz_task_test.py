1b309aa5d60d98ec10f1483d2a6cd4283ebbebf0
Jonathan Metzman <metzman@chromium.org>
2024-09-14 22:55:54 -0400

    fix

--- src/clusterfuzz/_internal/tests/core/bot/tasks/utasks/fuzz_task_test.py
+++ src/clusterfuzz/_internal/tests/core/bot/tasks/utasks/fuzz_task_test.py
@@ -1490,6 +1490,8 @@ class PreprocessStoreFuzzerRunResultsTest(unittest.TestCase):
     self.mock._sign_url.side_effect = (
         lambda remote_path, method, minutes: remote_path)
     self.mock.get_signed_upload_url.return_value = self.SIGNED_URL
+    helpers.patch_environ(self)
+    os.environ['JOB_NAME'] = 'linux_chrome_asan'
 
   def test_preprocess_store_fuzzer_run_results(self):
     fuzz_task_input = uworker_msg_pb2.FuzzTaskInput()
