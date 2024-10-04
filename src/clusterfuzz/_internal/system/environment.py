9fd8a61b04483efdb3486ff54e040c9d516469d8
Vitor Guidi <vitorguidi@gmail.com>
2024-10-04 12:40:35 -0300

    Revert Kubernetes cronjob instrumentation (#4293)
    As far as Kubernetes cronjobs go, successful terminations can be
    obtained through log base metrics in GCP, with the following query:
    
    ```
    jsonPayload.involvedObject.name =~ "${cronjob_name}"
    jsonPayload.message = "Job completed"
    resource.type="k8s_cluster"
    ```
    
    This metric suffices to alert on, since the abscence of successful runs
    for 24h suffices to signal that a cron is broken. It is obtained from
    kubernetes events (kubectl get events), so we can rely on the Kubernetes
    control plane for cronjob instrumentation instead of rolling our own.
    
    Thus, this PR will revert the previous metrics added to Kubernetes
    cronjobs, for the sake of simplifying the codebase.

--- src/clusterfuzz/_internal/system/environment.py
+++ src/clusterfuzz/_internal/system/environment.py
@@ -54,11 +54,6 @@ COMMON_SANITIZER_OPTIONS = {
 }
 
 
-def is_running_on_k8s():
-  """Returns whether or not we're running on K8s."""
-  return os.getenv('IS_K8S_ENV') == 'true'
-
-
 def _eval_value(value_string):
   """Returns evaluated value."""
   try:
