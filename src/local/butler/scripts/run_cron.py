d94fba47012af704f493b3b1191963f1739c0c87
Vitor Guidi <vitorguidi@google.com>
2024-10-04 18:30:47 +0000

    Fixing lint

--- src/local/butler/scripts/run_cron.py
+++ src/local/butler/scripts/run_cron.py
@@ -13,12 +13,11 @@
 # limitations under the License.
 """Executes update task locally, so we can run it through a debugger."""
 
-import os
-
-from clusterfuzz._internal.system import environment
 from clusterfuzz._internal.cron import oss_fuzz_apply_ccs
+from clusterfuzz._internal.system import environment
 
-def execute(args):
+
+def execute(args):  #pylint: disable=unused-argument
   """Build keywords."""
   environment.set_bot_environment()
   oss_fuzz_apply_ccs.main()
