e7fab251c7b0c069061633629f34a7b31028d626
Vitor Guidi <vitorguidi@google.com>
2024-10-21 17:50:59 +0000

    Adding build age metric definition

--- src/clusterfuzz/_internal/metrics/monitoring_metrics.py
+++ src/clusterfuzz/_internal/metrics/monitoring_metrics.py
@@ -32,6 +32,18 @@ JOB_BAD_BUILD_COUNT = monitor.CounterMetric(
         monitor.BooleanField('bad_build'),
     ])
 
+JOB_BUILD_AGE = monitor.CumulativeDistributionMetric(
+    'task/fuzz/job/build_age',
+    bucketer=monitor.FixedWidthBucketer(width=0.05, num_finite_buckets=20),
+    description=('Distribution of fuzz task\'s build age. '
+                 '(grouped by fuzzer/job)'),
+    field_spec=[
+        monitor.StringField('fuzz_target'),
+        monitor.StringField('job_type'),
+        monitor.StringField('platform'),
+    ],
+)
+
 FUZZER_KNOWN_CRASH_COUNT = monitor.CounterMetric(
     'task/fuzz/fuzzer/known_crash_count',
     description=('Count of fuzz task\'s known crash count '
