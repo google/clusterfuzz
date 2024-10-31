7aa27c90e846e43bc190fde2956722461f8b9243
Jonathan Metzman <metzman@chromium.org>
2024-10-31 16:28:29 -0400

    Fix

--- src/clusterfuzz/_internal/fuzzing/corpus_manager.py
+++ src/clusterfuzz/_internal/fuzzing/corpus_manager.py
@@ -437,11 +437,12 @@ class ProtoFuzzTargetCorpus(FuzzTargetCorpus):
     logs.info('Done uploading corpus.')
     filenames_to_delete = list(filenames_to_delete_dict.values())
 
-    # Assert that we aren't making the very bad mistake of deleting the entire
-    # corpus because we messed up our determination of which files were deleted
-    # by libFuzzer during merge/pruning.
-    assert ((len(filenames_to_delete) != len(
-        self._filenames_to_delete_urls_mapping)) or not filenames_to_delete)
+    if protect_corpus:
+      # Assert that we aren't making the very bad mistake of deleting the entire
+      # corpus because we messed up our determination of which files were
+      # deleted by libFuzzer during merge/pruning.
+      assert ((len(filenames_to_delete) != len(
+          self._filenames_to_delete_urls_mapping)) or not filenames_to_delete)
 
     logs.info('Deleting files.')
     storage.delete_signed_urls(filenames_to_delete)
