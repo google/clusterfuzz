a6ede4b98716eef03bca85659c8972c1147b1fdd
Vitor Guidi <vitorguidi@google.com>
2024-10-31 17:21:19 +0000

    Fix lint, update metric description

--- src/clusterfuzz/_internal/metrics/monitoring_metrics.py
+++ src/clusterfuzz/_internal/metrics/monitoring_metrics.py
@@ -197,13 +197,13 @@ TASK_TOTAL_RUN_TIME = monitor.CounterMetric(
 
 TESTCASE_TRIAGE_DURATION = monitor.CumulativeDistributionMetric(
     'uploaded_testcase_analysis/triage_duration_secs',
-    description = ('Time elapsed between testcase upload and completion'
-                    ' of relevant tasks in the triage lifecycle.'),
+    description=('Time elapsed between testcase upload and completion'
+                 ' of relevant tasks in the testcase upload lifecycle.'),
     bucketer=monitor.GeometricBucketer(),
     field_spec=[
         monitor.StringField('step'),
         monitor.StringField('job'),
-    ],  
+    ],
 )
 
 UTASK_SUBTASK_E2E_DURATION_SECS = monitor.CumulativeDistributionMetric(
