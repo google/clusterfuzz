fc2f8fb772fdea3bca7d036e2ce6091fa2cdd8db
Jonathan Metzman <metzman@chromium.org>
2024-11-06 11:17:26 -0500

    Log PID of run_bot process
    This should allow us to figure out why certain processes are OOMing.
    
    Related: b/376094981

--- src/python/bot/startup/run_bot.py
+++ src/python/bot/startup/run_bot.py
@@ -30,7 +30,7 @@ import traceback
 from clusterfuzz._internal.base import dates
 from clusterfuzz._internal.base import errors
 from clusterfuzz._internal.base import task_utils
-from clusterfuzz._internal.base import tasks as taskslib
+from clusterfuzz._internal.base import tasks as thoaskslib
 from clusterfuzz._internal.base import untrusted
 from clusterfuzz._internal.base import utils
 from clusterfuzz._internal.bot.fuzzers import init as fuzzers_init
@@ -191,6 +191,8 @@ def main():
     sys.exit(-1)
 
   fuzzers_init.run()
+
+  logs.info(f'PID is {os.getpid()}')
 
   if environment.is_trusted_host(ensure_connected=False):
     from clusterfuzz._internal.bot.untrusted_runner import host
