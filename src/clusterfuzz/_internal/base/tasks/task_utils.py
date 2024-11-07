1cdcf325bfce1672986d6543902e62136e1e3737
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2024-11-07 15:28:36 -0500

    Get batch ready for OSS-Fuzz (#4387)
    This should not be landed before the corresponding internal config
    change.
    1. Fix utask-main scheduling for oss-fuzz.
    2. Fix configuration of subnet, network, and gce region
    3. Only test using skia for now.
    4. Add tests for this functionality.

--- src/clusterfuzz/_internal/base/tasks/task_utils.py
+++ src/clusterfuzz/_internal/base/tasks/task_utils.py
@@ -40,6 +40,9 @@ def is_remotely_executing_utasks(task=None) -> bool:
 def is_task_opted_into_uworker_execution(task):
   # TODO(metzman): Remove this after OSS-Fuzz and Chrome are at parity.
   uworker_tasks = local_config.ProjectConfig().get('uworker_tasks', [])
+  if 'skia' not in environment.get_value('JOB_NAME', ''):
+    # This is just for testing OSS-Fuzz.
+    return False
   return task in uworker_tasks
 
 
