302d1bcf18bb4263ce4647730c2c6268ddfa7a7a
Jonathan Metzman <metzman@chromium.org>
2025-02-04 10:09:41 -0500

    Experiment with spreading out corpus pruning
    Corpus pruning scheduling seems related to drops in fuzzing.
    Try to spread out pruning so we don't cause these drops.

--- src/clusterfuzz/_internal/cron/schedule_corpus_pruning.py
+++ src/clusterfuzz/_internal/cron/schedule_corpus_pruning.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 """Schedule corpus pruning tasks."""
 
+import time
+
 from clusterfuzz._internal.base import tasks
 from clusterfuzz._internal.base import utils
 from clusterfuzz._internal.datastore import data_types
@@ -37,6 +39,8 @@ def main():
   """Schedule corpus pruning tasks."""
   for task_target, job_name, queue_name in get_tasks_to_schedule():
     logs.info(f'Adding corpus pruning task {task_target}.')
+    # Do this to avoid starving fuzzing.
+    time.sleep(.25)
     tasks.add_task('corpus_pruning', task_target, job_name, queue=queue_name)
 
   logs.info('Schedule corpus pruning task succeeded.')
