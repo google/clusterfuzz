2dffda63e6509bfa6e7c4956b90fb773451bab34
Vinicius Torres <vtcosta@google.com>
2025-02-05 15:06:29 -0300

    Enable deploying chrome-tests-syncer independently (#4647)
    Decouple the chrome-tests-syncer code releases from the rest of
    clusterfuzz, in order to enable it to be deployed independently of the
    clusterfuzz instances by the chrome fuzzing team .
    
    * Extended the butler deploy script to produce a dedicated deployment
    package for "--release=chrome-tests-syncer" and to produce both the prod
    and tests-syncer packages when releasing to prod.
    * Changed chrome-tests-syncer Dockerfile/start.sh to pull from this
    dedicated deployment package.
    * Moved and edited the tests-syncer code to be executed as a k8s
    cronjob.
    * (side effect) Updated unit test to account for deploying the
    chrome-tests-syncer manifest.
    
    * Deployed a chrome-tests-syncer release using this PR and produced the
    dedicated artifact
    (https://screenshot.googleplex.com/8W88DmYwFKPkSfZ.png).
    * Built an updated chrome-tests-syncer image that pulls from this
    artifact (https://screenshot.googleplex.com/8HqEvV7bnBggBPj.png).
    
    * Created the chrome-tests-syncer cronjob (along with PR in
    clusterfuzz-config: 2428107) pulling from this updated image, applied it
    to the GKE and it ran successfully:
    
    ![image](https://github.com/user-attachments/assets/b32441f1-daae-4c12-9476-97583fad3982)
    
    * Mocked a deployment with --release=prod (by printing the command
    instead of actually uploading to gcs):
    <img width="960" alt="Screenshot 2025-02-05 at 11 48 31"
    src="https://github.com/user-attachments/assets/bd035374-07d1-453a-9db4-2ed0cd39d92f"
    />
    
    Bug: b/389048599

--- src/local/butler/package.py
+++ src/local/butler/package.py
@@ -15,6 +15,7 @@
 
 import os
 import re
+import shutil
 import sys
 import zipfile
 
@@ -135,7 +136,20 @@ def package(revision,
 
   print()
   print('%s is ready.' % target_zip_path)
-  return target_zip_path
+
+  targets_zip_paths = [target_zip_path]
+  if platform_name and release == 'prod':
+    # Copy prod package into additional releases.
+    for add_release in constants.ADDITIONAL_RELEASES:
+      add_target_zip_name = utils.get_platform_deployment_filename(
+          platform_name, release=add_release)
+      add_target_zip_path = os.path.join(target_zip_dir, add_target_zip_name)
+      _clear_zip(add_target_zip_path)
+      shutil.copy2(target_zip_path, add_target_zip_path)
+      print('\n%s is ready.' % add_target_zip_path)
+      targets_zip_paths.append(add_target_zip_path)
+
+  return targets_zip_paths
 
 
 def execute(args):
