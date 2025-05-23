7a430b9acb69f6b9bca4a7d82b98ca0b50c35822
Vitor Guidi <vitorguidi@google.com>
2025-05-23 13:57:01 +0000

    Comment on the origin of the order by statement

--- src/clusterfuzz/_internal/cron/cleanup.py
+++ src/clusterfuzz/_internal/cron/cleanup.py
@@ -196,6 +196,7 @@ def cleanup_unused_fuzz_targets_and_jobs():
 
   unused_target_jobs = data_types.FuzzTargetJob.query(
       data_types.FuzzTargetJob.last_run < last_run_cutoff)
+  # The order by last_run DESC filter is from b/418807403
   valid_target_jobs = data_types.FuzzTargetJob.query(
       data_types.FuzzTargetJob.last_run >= last_run_cutoff).order(
           -data_types.FuzzTargetJob.last_run)
