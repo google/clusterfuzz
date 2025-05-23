0fad689728779112276bcbca602b952f338a54fc
Vitor Guidi <vitorguidi@google.com>
2025-05-23 13:53:03 +0000

    Force FuzzTargetJob query to use the DESC index on last_run to work around the cleanup timeout

--- src/clusterfuzz/_internal/cron/cleanup.py
+++ src/clusterfuzz/_internal/cron/cleanup.py
@@ -197,7 +197,8 @@ def cleanup_unused_fuzz_targets_and_jobs():
   unused_target_jobs = data_types.FuzzTargetJob.query(
       data_types.FuzzTargetJob.last_run < last_run_cutoff)
   valid_target_jobs = data_types.FuzzTargetJob.query(
-      data_types.FuzzTargetJob.last_run >= last_run_cutoff)
+      data_types.FuzzTargetJob.last_run >= last_run_cutoff).order(
+          -data_types.FuzzTargetJob.last_run)
 
   to_delete = [t.key for t in unused_target_jobs]
 
