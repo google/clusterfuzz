7a936b47b9fcfb6d73fd3c2df2f33cd2380c5013
Vitor Guidi <vitorguidi@google.com>
2025-05-30 18:38:27 +0000

    Add initial functional version of entity exporter

--- src/clusterfuzz/_internal/cron/job_exporter.py
+++ src/clusterfuzz/_internal/cron/job_exporter.py
@@ -15,14 +15,15 @@
     in order to mirror the workloads on testing environments."""
 
 import os
-from clusterfuzz._internal.datastore import data_types
-from clusterfuzz._internal.bot.tasks.utasks import uworker_io
-from clusterfuzz._internal.google_cloud_utils import storage
-from clusterfuzz._internal.google_cloud_utils import blobs
+import tempfile
+
 from google.cloud import ndb
 from google.protobuf import any_pb2
 
-import tempfile
+from clusterfuzz._internal.bot.tasks.utasks import uworker_io
+from clusterfuzz._internal.datastore import data_types
+from clusterfuzz._internal.google_cloud_utils import blobs
+from clusterfuzz._internal.google_cloud_utils import storage
 
 target_entities = [
     (data_types.Fuzzer, ['blobstore_key'], 'fuzzer'),
@@ -35,59 +36,63 @@ operation_mode = os.getenv('OPERATION_MODE', None)
 
 
 class EntityMigrator:
-    def __init__(self, target_cls: ndb.Model, blobstore_keys: list[str], entity_type: str):
-        self.target_cls = target_cls
-        self.blobstore_keys = blobstore_keys
-        self._entity_type = entity_type
+  """Serializes entities to GCS, and imports them back."""
 
-    def _serialize(self, entity):
-        return uworker_io.db_entity_to_entity_message(entity)
+  def __init__(self, target_cls: ndb.Model, blobstore_keys: list[str],
+               entity_type: str):
+    self._target_cls = target_cls
+    self.blobstore_keys = blobstore_keys
+    self._entity_type = entity_type
 
-    def _deserialize(self, proto) -> ndb.Model:
-        pass
+  def _serialize(self, entity) -> str:
+    return uworker_io.entity_to_protobuf(entity).SerializeToString()
 
-    def _export_entity(self, entity):
-        assert entity is self.target_cls
-        # All entitites in question get their name from the 'name' field in datastore
-        entity_name = getattr(entity, 'name', None)
-        assert entity_name
-        entity_target_bucket_prefix = f'gs://{export_bucket}/{self._entity_type}/{entity_name}'
-        target_location = f'{entity_target_bucket_prefix}/entity.proto'
-        with tempfile.NamedTemporaryFile(mode='w+', delete=True) as tmp_file:
-            entity_pb = self._serialize(entity)
-            tmp_file.write(entity_pb)
-            storage.copy_file_to(tmp_file.name, target_location)
-        for blobstore_key in self.blobstore_keys:
-            blob_id = getattr(entity, blobstore_key, None)
-            if blob_id:
-                blob_gcs_path = blobs.get_gcs_path(blob_id)
-                blob_destination_path = f'{entity_target_bucket_prefix}/{blobstore_key}'
-                storage.copy_blob(blob_gcs_path, blob_destination_path)
+  def _deserialize(self, proto_as_str: str) -> ndb.Model:
+    deserialized_any = any_pb2.Any()  # pylint: disable=no-member
+    # Parse the bytes into the Any message
+    deserialized_any.ParseFromString(proto_as_str)
+    return uworker_io.entity_from_protobuf(deserialized_any, self._target_cls)
 
+  def _export_entity(self, entity):
+    """Exports entity as protobuf and its respective blobs to GCS."""
+    # Entitites get their name from the 'name' field in datastore
+    entity_name = getattr(entity, 'name', None)
+    assert entity_name
+    bucket_prefix = f'gs://{export_bucket}/{self._entity_type}/{entity_name}'
+    target_location = f'{bucket_prefix}/entity.proto'
+    with tempfile.NamedTemporaryFile(mode='wb+', delete=True) as tmp_file:
+      entity_pb_as_str = self._serialize(entity)
+      back_to_entity = self._deserialize(entity_pb_as_str)
+      assert entity == back_to_entity
+      tmp_file.write(entity_pb_as_str)
+      tmp_file.flush()
+      storage.copy_file_to(tmp_file.name, target_location)
+    for blobstore_key in self.blobstore_keys:
+      blob_id = getattr(entity, blobstore_key, None)
+      if blob_id:
+        blob_gcs_path = blobs.get_gcs_path(blob_id)
+        blob_destination_path = f'{bucket_prefix}/{blobstore_key}'
+        storage.copy_blob(blob_gcs_path, blob_destination_path)
 
-    def export_entities(self):
-        for entity in self.target_cls.query():
-            self._export_entity(entity)
+  def export_entities(self):
+    for entity in self._target_cls.query():
+      self._export_entity(entity)
 
-    def import_entities(self):
-        pass
+  def import_entities(self):
+    pass
 
-    
-
-
-def process_entities(operation_mode: str):
-    for (entity, blobstore_keys, entity_name) in target_entities:
-        migrator = EntityMigrator(entity, blobstore_keys, entity_name)
-        if operation_mode == 'export':
-            migrator.export_entities()
-        else:
-            migrator.import_entities()
 
 def main():
-    "Exports datastore entities and respective blobs"
+  "Exports datastore entities and respective blobs"
 
-    assert export_bucket
-    assert operation_mode in ['import', 'export']
-
-    process_entities(operation_mode)
-    return True
+  assert export_bucket
+  assert export_bucket == 'vguidi_exporting_exps'
+  assert operation_mode in ['import', 'export']
+  assert operation_mode == 'export'
+  for (entity, blobstore_keys, entity_name) in target_entities:
+    migrator = EntityMigrator(entity, blobstore_keys, entity_name)
+    if operation_mode == 'export':
+      migrator.export_entities()
+    else:
+      migrator.import_entities()
+  return True
