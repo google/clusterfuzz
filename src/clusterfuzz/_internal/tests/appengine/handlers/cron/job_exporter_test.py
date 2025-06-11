e486c8871b85c1344e2b7de05d64daca7ae2242e
Vitor Guidi <vitorguidi@google.com>
2025-06-11 22:46:47 +0000

    Add unit tests for job import, deletion case

--- src/clusterfuzz/_internal/tests/appengine/handlers/cron/job_exporter_test.py
+++ src/clusterfuzz/_internal/tests/appengine/handlers/cron/job_exporter_test.py
@@ -190,7 +190,6 @@ def _entity_list_contains_expected_entities(blob_path, expected_entities):
   return expected_entities == recovered_entities
 
 
-@unittest.skip('tmp')
 @test_utils.with_cloud_emulators('datastore')
 class TestEntitySerializationAndDeserializastion(unittest.TestCase):
   """Test the serialization and deserialization of entities."""
@@ -242,7 +241,6 @@ class TestEntitySerializationAndDeserializastion(unittest.TestCase):
     deserialized_fuzzer = entity_migrator._deserialize(serialized_fuzzer)
 
     self.assertTrue(_fuzzers_equal(fuzzer, deserialized_fuzzer))
-
 
 @test_utils.with_cloud_emulators('datastore')
 class TestEntitiesAreCorrectlyExported(unittest.TestCase):
@@ -1056,3 +1054,47 @@ class TestEntitiesAreCorrectlyImported(unittest.TestCase):
     self.assertEqual(platform, imported_job.platform)
 
     self.assertTrue(_entity_blob_was_correctly_imported(job_blob_data, imported_job.custom_binary_key))
+
+  def test_jobs_are_correctly_deleted(self):
+    job_name = 'some-job'
+    platform = 'some-platform'
+    env_string = 'some-env-string'
+    custom_binary_key = None
+
+    job = _sample_job(
+        name=job_name,
+        custom_binary_key=custom_binary_key,
+        platform=platform,
+        environment_string=env_string)
+
+    _register_entity_and_upload_blobs(
+      entity=job,
+      entity_kind='job',
+      blobstore_key_content=None,
+      sample_testcase_contents=None,
+      custom_binary_contents=None,
+      blobs_bucket=self.blobs_bucket,
+      data_bundle_blob_contents=None,
+    )
+
+    jobs = [job for job in data_types.Job.query()]
+
+    self.assertEqual(1, len(jobs))
+    imported_job = jobs[0]
+
+    self.assertEqual(env_string, imported_job.environment_string)
+    self.assertEqual(job_name, imported_job.name)
+    self.assertEqual(platform, imported_job.platform)
+    self.assertEqual(custom_binary_key, imported_job.custom_binary_key)
+
+    job_base_location = f'gs://{self.import_source_bucket}/job'
+    _upload_entity_list([], job_base_location)
+
+    entity_migrator = job_exporter.EntityMigrator(
+      data_types.Job, ['custom_binary_key'], 'job',
+      job_exporter.StorageRSync(), self.import_source_bucket)
+    entity_migrator.import_entities()
+
+    post_import_jobs = [job for job in data_types.Job.query()]
+
+    self.assertEqual(0, len(post_import_jobs))
