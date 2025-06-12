cd5dbaffc6a27eed63c26e2b6aa030f914f76e93
Carlo Lemos <carlolemos@google.com>
2025-06-12 17:22:55 +0000

    revert commit a8aafb7

--- src/clusterfuzz/_internal/bot/tasks/utasks/corpus_pruning_task.py
+++ src/clusterfuzz/_internal/bot/tasks/utasks/corpus_pruning_task.py
@@ -833,8 +833,8 @@ def _upload_corpus_crashes_zip(result: CorpusPruningResult,
 def _process_corpus_crashes(output: uworker_msg_pb2.Output):  # pylint: disable=no-member
   """Process crashes found in the corpus."""
   # TODO(metzman): Fix this function after the holiday break.
-  # if not output.corpus_pruning_task_output.crashes:
-  return
+  if not output.corpus_pruning_task_output.crashes:
+    return
   # pylint: disable=unreachable
   corpus_pruning_output = output.corpus_pruning_task_output
   crash_revision = corpus_pruning_output.crash_revision
