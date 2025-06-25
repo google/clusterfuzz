3b2d6bcf95826e94ac866fc8615b9ffdcf64698d
Vitor Guidi <vitorguidi@google.com>
2025-06-25 21:04:49 +0000

    Revert handler.py back to original state without logs

--- src/appengine/libs/handler.py
+++ src/appengine/libs/handler.py
@@ -282,18 +282,13 @@ def pubsub_push(func):
       if not bearer_token.startswith(BEARER_PREFIX):
         raise helpers.UnauthorizedError('Missing or invalid bearer token.')
 
-      helpers.log(f'token = {bearer_token}', helpers.VIEW_OPERATION)
       token = bearer_token.split(' ')[1]
       claim = id_token.verify_oauth2_token(token, google_requests.Request())
     except google.auth.exceptions.GoogleAuthError as e:
-      helpers.log(f'Failed first parse: {e}', helpers.VIEW_OPERATION)
       raise helpers.UnauthorizedError('Invalid ID token.') from e
 
-    claim_email_verified = claim.get('email_verified')
-    claim_email = claim.get('email')
-    if (not claim_email_verified or
-        claim_email != utils.service_account_email()):
-      helpers.log(f'Failed second parse: {claim_email_verified} - {claim_email} - expected {utils.service_account_email()}', helpers.VIEW_OPERATION)
+    if (not claim.get('email_verified') or
+        claim.get('email') != utils.service_account_email()):
       raise helpers.UnauthorizedError('Invalid ID token.')
 
     message = pubsub.raw_message_to_message(json.loads(request.data.decode()))
