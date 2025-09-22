ef6d95433326b9c03df83aae7b68f4416f4425e0
Vinicius da Costa <vtcosta@google.com>
2025-09-22 21:40:44 +0000

    Use fetch page for batch delete.

--- src/local/butler/scripts/migrate_fuzzer_task_events.py
+++ src/local/butler/scripts/migrate_fuzzer_task_events.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 """Migrate fuzzer-based task events to the correct entity."""
 
-import datetime
+from google.cloud import ndb
 
 from clusterfuzz._internal.datastore import data_types
 from clusterfuzz._internal.datastore import ndb_utils
@@ -38,25 +38,45 @@ def execute(args):
   logs.configure('run_bot')
 
   task_event_type = events.EventTypes.TASK_EXECUTION
-  task_names = ['fuzz', 'corpus_pruning']
-  query = data_types.TestcaseLifecycleEvent.query(
-      data_types.TestcaseLifecycleEvent.event_type == task_event_type,
-      data_types.TestcaseLifecycleEvent.task_name.IN(task_names))
+  task_names = ['corpus_pruning', 'fuzz']
 
   # If we choose to delete all entities from the old model.
   if _DELETE_ALL:
-    ndb_utils.delete_multi(ndb_utils.get_all_from_query(query, keys_only=True))
+    total_deleted = 0
+    for task_name in task_names:
+      print(f'Started deleting for {task_name}.')
+      query = data_types.TestcaseLifecycleEvent.query(
+          data_types.TestcaseLifecycleEvent.event_type == task_event_type,
+          data_types.TestcaseLifecycleEvent.task_name == task_name)
+
+      keys_batch, next_cursor, more = query.fetch_page(
+          _BATCH_SIZE, keys_only=True)
+      while keys_batch:
+        ndb.delete_multi(keys_batch)
+        total_deleted += len(keys_batch)
+        print(f"Deleted {len(keys_batch)} entities (Total: {total_deleted})")
+        if not more:
+          break
+        if total_deleted >= 10000:
+          break
+        keys_batch, next_cursor, more = query.fetch_page(
+            _BATCH_SIZE, keys_only=True, start_cursor=next_cursor)
+
     logs.info('Done deleting fuzzer-based task execution events!')
     return
 
-  # If we choose to migrate them into the new entity model.
+
+# If we choose to migrate them into the new entity model.
+  query = data_types.TestcaseLifecycleEvent.query(
+      data_types.TestcaseLifecycleEvent.event_type == task_event_type,
+      data_types.TestcaseLifecycleEvent.task_name.IN(task_names))
   to_update = []
   to_delete = []
   total_count = 0
   for event in ndb_utils.get_all_from_query(query):
     migrate_event = data_types.FuzzerTaskEvent(
         event_type=events.EventTypes.FUZZER_TASK_EXECUTION)
-    for attr in _MIGRATE_FIELDS: 
+    for attr in _MIGRATE_FIELDS:
       setattr(migrate_event, attr, getattr(event, attr))
     to_update.append(migrate_event)
     to_delete.append(event.key)
