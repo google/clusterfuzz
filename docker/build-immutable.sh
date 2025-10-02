eb4b0d6442d054b721de3d3378e85430cc247641
Javan Lacerda <javanlacerda@google.com>
2025-10-02 19:20:49 +0000

    fix
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>

--- docker/build-immutable.sh
+++ docker/build-immutable.sh
@@ -22,10 +22,10 @@ if [ -n "$1" ]; then
 fi
 
 for image_name in "${IMAGES[@]}"; do
-
+  echo $PWD
   CURRENT_CLUSTERFUZZ_REVISION="$(cat /workspace/revision.txt)"
   image_dir=${image_name#gcr.io/clusterfuzz-images/}
-  docker build --build-arg CLUSTERFUZZ_SOURCE_DIR=${PWD} -t "$image_name":${CURRENT_CLUSTERFUZZ_REVISION} "$image_dir"
+  docker build --build-arg CLUSTERFUZZ_SOURCE_DIR=$PWD -t "$image_name":${CURRENT_CLUSTERFUZZ_REVISION} "$image_dir"
   docker push "$image_name":${CURRENT_CLUSTERFUZZ_REVISION}
 
 done
