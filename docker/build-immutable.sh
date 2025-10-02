11edce44301c2a43a00776217130c1f631305e60
Javan Lacerda <javanlacerda@google.com>
2025-10-02 15:21:20 +0000

    remove from deploy
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>
    
    remove hello world
    
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>
    
    endline
    
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>
    
    dev -> prod
    
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>
    
    create loop
    
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>
    
    --amend

--- docker/build-immutable.sh
+++ docker/build-immutable.sh
@@ -1,24 +1,31 @@
+#!/bin/bash -ex
 # Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-FROM gcr.io/clusterfuzz-images/chromium/base
 
-ENV IMMUTABLE_IMAGE=true
+IMAGES=(
+  gcr.io/clusterfuzz-images/chromium/base/immutable
+)
 
-ARG CLUSTERFUZZ_SOURCE_DIR
+if [ -n "$1" ]; then
+  cd /workspace/clusterfuzz
+fi
 
-COPY ${CLUSTERFUZZ_SOURCE_DIR} /data/clusterfuzz
+for image_name in "${IMAGES[@]}"; do
 
-RUN cd /data/clusterfuzz && ./local/install_deps.bash
+  CURRENT_CLUSTERFUZZ_REVISION="$(cat /workspace/revision.txt)"
+  image_dir=${image_name#gcr.io/clusterfuzz-images/}
+  docker build --build-arg CLUSTERFUZZ_SOURCE_DIR=$${PWD} -t "$image_name":${CURRENT_CLUSTERFUZZ_REVISION} "$image_dir"
+  docker push "$image_name":${CURRENT_CLUSTERFUZZ_REVISION}
 
-CMD ["echo", "Hello World!"]
+done
