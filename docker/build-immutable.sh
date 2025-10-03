da6b597602550cc331a0361a9efb69773d0a4ae1
Javan Lacerda <javanlacerda@google.com>
2025-10-03 11:27:45 -0300

    Implement immutable image for chrome (#4971)
    fix b/448629855
    
    It implements the immutable image for chromium base. 
    
    The strategy for achieving this goes through the following:
    * Create a feature flag based on env vars to switch on or of the step to
    download the Clusterfuzz source code from the GCS bucket in the
    `setup_clusterfuzz.sh` script.
    * Create a Dockerfile for buiding the immutable image, that gets
    together the base image with the Clusterfuzz source code and
    configuration.
    * Create a build script for buiding the immutable images, pointing to
    the expected Dockerfiles.
    
    Successful build in dev:
    [build
    link](https://pantheon.corp.google.com/cloud-build/builds;region=us-central1/34cfb4c0-ce41-464b-a2a0-5f69ed0ccc51?e=-13802955&mods=logs_tg_prod&project=clusterfuzz-development)
    <img width="1183" height="874" alt="image"
    src="https://github.com/user-attachments/assets/bd79e708-0b0f-4907-a6e1-cae803941f46"
    />
    
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>

---
+++ docker/build-immutable.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -ex
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+IMAGES=(
+  gcr.io/clusterfuzz-images/chromium/base/immutable
+)
+
+if [ -n "$1" ]; then
+  cd /workspace/clusterfuzz
+fi
+
+for image_name in "${IMAGES[@]}"; do
+  echo $PWD
+  CURRENT_CLUSTERFUZZ_REVISION="$(cat /workspace/revision.txt)"
+  image_dir=docker/${image_name#gcr.io/clusterfuzz-images/}
+  docker build --build-arg CLUSTERFUZZ_SOURCE_DIR=. -t "$image_name":${CURRENT_CLUSTERFUZZ_REVISION} -f "$image_dir/Dockerfile" .
+  docker push "$image_name":${CURRENT_CLUSTERFUZZ_REVISION}
+
+done
