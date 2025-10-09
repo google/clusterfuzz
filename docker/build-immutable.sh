5a40abb45c50feab2fb1dec71973abfd2ec9e5f2
Javan Lacerda <javanlacerda@google.com>
2025-10-09 17:35:10 +0000

    delete src/appengine/config before build
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>

--- docker/build-immutable.sh
+++ docker/build-immutable.sh
@@ -32,6 +32,10 @@ if [ -n "$1" ]; then
   cd /workspace/clusterfuzz
 fi
 
+# Deleting the current config that is used for testing purposes.
+# It will be replaced by the project config during the image build.
+rm -rf src/appengine/config
+
 # Loop through each image name in the IMAGES array.
 for image_name in "${IMAGES[@]}"; do
   # Read the ClusterFuzz revision from the revision.txt file. This is used to
