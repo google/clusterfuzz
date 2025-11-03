4c8cff307c5fc8920c776367188c35ba928229fa
PauloVLB <paulovlb@google.com>
2025-11-03 19:34:36 +0000

    refactor: fix lint and add instructions for runinng tests

--- cli/casp/src/casp/tests/commands/test_init.py
+++ cli/casp/src/casp/tests/commands/test_init.py
@@ -14,7 +14,7 @@
 """Tests for the init command.
 
    For running all the tests, use (from the root of the project):
-   python -m unittest discover -s cli/casp/src/casp/tests -v
+   python -m unittest discover -s cli/casp/src/casp/tests -p test_init.py -v
 """
 
 import os
