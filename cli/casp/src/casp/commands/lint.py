a2f5383acf7f1a64d7964b18de49d78c43fbb0b8
PauloVLB <paulovlb@google.com>
2025-11-19 17:26:20 +0000

    refactor: improve lint help

--- cli/casp/src/casp/commands/lint.py
+++ cli/casp/src/casp/commands/lint.py
@@ -11,12 +11,49 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Lint command."""
+"""Run lint command."""
 
+import subprocess
+import sys
+
+from casp.utils import local_butler
 import click
 
 
-@click.command(name='lint', help='Lint changed code in current branch.')
-def cli():
-  """Lint changed code in current branch."""
-  click.echo('To be implemented...')
+@click.command(
+    name='lint',
+    help=('Runs linting checks on the codebase. '
+          'By default, it lints changed code in the current branch. '
+          'You can also specify a file or directory to lint.'))
+@click.argument('path', required=False, type=click.Path(exists=True))
+@click.option(
+    '--path',
+    '-p',
+    'path_option',
+    help='The file or directory to run the lint command in.',
+    default=None,
+    type=click.Path(exists=True),
+    show_default=True)
+def cli(path: str, path_option: str) -> None:
+  """Runs linting checks on the codebase. 
+  By default, it lints changed code in the current branch. 
+  You can also specify a file or directory to lint."""
+  target_dir = path_option or path
+
+  try:
+    if target_dir:
+      command = local_butler.build_command('lint', path=target_dir)
+    else:
+      command = local_butler.build_command('lint')
+  except FileNotFoundError:
+    click.echo('butler.py not found in this directory.', err=True)
+    sys.exit(1)
+
+  try:
+    subprocess.run(command, check=True)
+  except FileNotFoundError:
+    click.echo('python not found in PATH.', err=True)
+    sys.exit(1)
+  except subprocess.CalledProcessError as e:
+    click.echo(f'Error running butler.py lint: {e}', err=True)
+    sys.exit(1)
