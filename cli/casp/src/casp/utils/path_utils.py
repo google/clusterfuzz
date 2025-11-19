8c5a2a2ce1388534bd851617d9a98643ff1ff9a9
PauloVLB <paulovlb@google.com>
2025-11-19 16:13:52 +0000

    fix: typo

--- cli/casp/src/casp/utils/path_utils.py
+++ cli/casp/src/casp/utils/path_utils.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Manages the CASP path search and manegement.
+"""Manages the CASP path search and management.
 
 This module provides a set of utilities related 
 to file and directory path manipulation and discovery.
