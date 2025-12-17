3792e74cb37f081b8846a27f7c00f616ad24509d
Javan Lacerda <javanlacerda@google.com>
2025-12-17 17:07:15 +0000

    ci: Add GitHub Action for Kubernetes e2e tests
    This commit introduces a new GitHub Action workflow to run the Kubernetes end-to-end test on every pull request.
    
    The workflow leverages a new script, , which is responsible for setting up the test environment and running the test. This follows the existing CI conventions in the project.

---
+++ local/tests/kubernetes_e2e_test.bash
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+#
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is for running the Kubernetes end-to-end test in CI.
+
+# Install kind.
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# Install pipenv.
+pip install pipenv
+
+# Install dependencies.
+pipenv install --dev
+
+# Run the test.
+export PYTHONPATH=$PYTHONPATH:$(pwd)/src
+pipenv run python3 src/clusterfuzz/_internal/tests/core/platforms/kubernetes/service_e2e_test.py
