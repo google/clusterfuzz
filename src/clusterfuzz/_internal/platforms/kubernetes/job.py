7dfe80656a37ccc24837fb67f271a57b19f88e34
Javan Lacerda <javanlacerda@google.com>
2025-12-17 15:55:20 +0000

    feat: Implement Kubernetes job client and service
    This commit introduces the Kubernetes job client and service, analogous to the existing GCP Batch client and service.
    
    Changes include:
    - Added 'kubernetes' dependency to .
    - Implemented  in  to interact with the Kubernetes API for job creation and management, supporting both raw Kubernetes jobs and Kata Containers.
    - Created  to encapsulate the job creation logic.
    - Created  as a public entrypoint for creating Kubernetes jobs.
    - Added a  file for the new Kubernetes platform directory to manage dependencies.

---
+++ src/clusterfuzz/_internal/platforms/kubernetes/job.py
@@ -0,0 +1,35 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Kubernetes job creation helpers."""
+
+from typing import List
+
+from clusterfuzz._internal.batch.kubernetes import KubernetesJobClient
+from clusterfuzz._internal.metrics import logs
+
+
+def create_job(job_name: str, container_image: str, job_spec_file: str,
+               input_urls: List[str]):
+  """Creates and starts a Kubernetes job.
+
+  Args:
+    job_name: The name of the Kubernetes job.
+    container_image: The container image to use for the job.
+    job_spec_file: The path to the Kubernetes job specification file (YAML).
+    input_urls: A list of URLs to be passed as environment variables to the
+      job's container.
+  """
+  client = KubernetesJobClient(job_name, container_image, job_spec_file)
+  client.create_job(None, input_urls)
+  logs.info(f'Created Kubernetes job id={job_name}.')
---
+++ src/clusterfuzz/_internal/platforms/kubernetes/service.py
@@ -0,0 +1,32 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Kubernetes service."""
+
+from typing import List
+
+from clusterfuzz._internal.platforms.kubernetes import job
+
+
+def create_job(job_name: str, container_image: str, job_spec_file: str,
+               input_urls: List[str]):
+  """Creates a Kubernetes job.
+
+  Args:
+    job_name: The name of the Kubernetes job.
+    container_image: The container image to use for the job.
+    job_spec_file: The path to the Kubernetes job specification file (YAML).
+    input_urls: A list of URLs to be passed as environment variables to the
+      job's container.
+  """
+  job.create_job(job_name, container_image, job_spec_file, input_urls)
