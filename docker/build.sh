d77bab0a3d339577acbfd72a0e6a16d396243ef9
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2025-12-19 09:28:34 -0500

    Delete tester images (#5091)
    Progrollout replaced it.
    Fixes: #4320

--- docker/build.sh
+++ docker/build.sh
@@ -19,7 +19,6 @@ IMAGES=(
   gcr.io/clusterfuzz-images/chromium/base
   gcr.io/clusterfuzz-images/chromium/builder
   gcr.io/clusterfuzz-images/chromium/high-end
-  gcr.io/clusterfuzz-images/chromium/tester
   gcr.io/clusterfuzz-images/chromium/tests-syncer
   gcr.io/clusterfuzz-images/oss-fuzz/base
   gcr.io/clusterfuzz-images/oss-fuzz/host-high-end
