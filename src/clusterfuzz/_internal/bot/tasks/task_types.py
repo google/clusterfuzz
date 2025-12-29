10a272b68efc8c05ec454b454049c4e8f97e51e6
Javan Lacerda <javanlacerda@google.com>
2025-12-29 14:52:20 +0000

    moving fuzz to remote task
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>

--- src/clusterfuzz/_internal/bot/tasks/task_types.py
+++ src/clusterfuzz/_internal/bot/tasks/task_types.py
@@ -150,7 +150,7 @@ class UTask(BaseUTask):
     command = task_utils.get_command_from_module(self.module.__name__)
     # TODO(metzman): This is really complicated because of the need to test
     # remote execution. This is no longer a need, so simplify this.
-    if not (environment.is_tworker() or is_remote_utask(command, job_type)):
+    if command != 'fuzz' and not (environment.is_tworker() or is_remote_utask(command, job_type)):
       self.execute_locally(task_argument, job_type, uworker_env)
       return
 
