a0e1cb4823d1f867ce54ca2e5fbab2e296921bfa
Vinicius da Costa <vtcosta@google.com>
2026-02-18 22:58:16 +0000

    Fix unit test

--- src/clusterfuzz/_internal/tests/appengine/handlers/cron/oss_fuzz_cc_groups_test.py
+++ src/clusterfuzz/_internal/tests/appengine/handlers/cron/oss_fuzz_cc_groups_test.py
@@ -41,7 +41,7 @@ class OssFuzzCcGroupsTest(unittest.TestCase):
     project1 = data_types.OssFuzzProject(
         name='project1', ccs=['member2@example.com'])
     project2 = data_types.OssFuzzProject(
-        name='project2', ccs=['member3@example.com'])
+        name='project2', ccs=['member2@example.com', 'member3@example.com'])
     project1.put()
     project2.put()
 
