0b9e68ae390d76ae9f475165b1957ca10689f50b
Dylan Jew <dylanjew@users.noreply.github.com>
2026-04-20 16:39:50 -0400

    [BlackboxAudit] Add butler script to download a fuzzer config (#5244)
    Adds script to download a JSON config with the parameters required to
    upload a fuzzer.
    
    Along with #5236 allows us to edit configs without going through the UI.
    
    We want to store these configs in VCS so that we have a history of the
    jobs that a fuzzer is enabled for
    
    ### Testing
    I ran this with my local server and the prod config with `python
    butler.py run --non-dry-run -c ../clusterfuzz-config/configs/internal
    download_fuzzer_config --script_args ifratric-browserfuzzer-v3`
    and
    `python butler.py run --non-dry-run --local -c configs/test
    download_fuzzer_config --script_args dylanj_test_fuzzer`
    
    Then uploaded a test fuzzer locally with `python butler.py run
    --non-dry-run -c configs/test/ --local upload_fuzzer --script_args
    --fuzzer-name=dylanj_test_fuzzer
    --fuzzer-archive-path=../dylanj_b0ring_webidl_fuzzer.zip
    --config-path=test_config.json`

--- src/clusterfuzz/_internal/tests/appengine/handlers/fuzzers_test.py
+++ src/clusterfuzz/_internal/tests/appengine/handlers/fuzzers_test.py
@@ -21,6 +21,7 @@ import flask
 import webtest
 
 from clusterfuzz._internal.datastore import data_types
+from clusterfuzz._internal.google_cloud_utils import storage
 from clusterfuzz._internal.tests.test_libs import helpers as test_helpers
 from clusterfuzz._internal.tests.test_libs import test_utils
 from handlers import fuzzers
@@ -107,3 +108,79 @@ class CreateHandlerTest(unittest.TestCase):
     self.assertEqual(fuzzer.name, fuzzer_name)
     self.assertEqual(fuzzer.revision, 0)
     self.assertEqual(fuzzer.created_at, self.mock_time)
+
+
+@test_utils.with_cloud_emulators('datastore')
+class EditHandlerTest(unittest.TestCase):
+  """Tests EditHandler updates an existing Fuzzer"""
+
+  def setUp(self):
+    test_helpers.patch(self, [
+        'libs.access.has_access',
+        'libs.auth.get_current_user',
+        'libs.helpers.get_user_email',
+        'handlers.fuzzers.datetime',
+        'handlers.fuzzers.EditHandler.get_upload',
+        'handlers.fuzzers.EditHandler._get_executable_path',
+        'handlers.fuzzers.EditHandler._get_launcher_script',
+    ])
+    self.mock.has_access.return_value = True
+    self.mock.get_current_user().email = 'test@user.com'
+    self.mock.get_user_email.return_value = 'test@user.com'
+    self.mock.get_upload.return_value = storage.GcsBlobInfo(
+        bucket='test-bucket', object_path='key', filename='file.zip', size=123)
+
+    self.mock._get_executable_path.return_value = 'executable'
+    self.mock._get_launcher_script.return_value = 'launcher'
+
+    self.mock_time = datetime.datetime(2026, 1, 1, tzinfo=None)
+    self.mock.datetime.datetime.now.return_value = self.mock_time
+    self.mock.datetime.timezone = datetime.timezone
+
+    flaskapp = flask.Flask('testflask')
+    flaskapp.add_url_rule(
+        '/fuzzers/edit', view_func=fuzzers.EditHandler.as_view('/fuzzers/edit'))
+    self.app = webtest.TestApp(flaskapp)
+
+  def test_update_fuzzer(self):
+    """Test fuzzers/edit updates the fuzzer"""
+
+    fuzzer_name = 'test_fuzzer'
+    fuzzer = data_types.Fuzzer(
+        name=fuzzer_name,
+        jobs=[],
+        revision=1,
+        timeout=10,
+    )
+    fuzzer.put()
+
+    request_payload = {
+        'additional_environment_string': 'args=123',
+        'csrf_token': form.generate_csrf_token(),
+        'data_bundle_name': 'test_bundle',
+        'external_contribution': True,
+        'jobs': [],
+        'key': fuzzer.key.id(),
+        'max_testcases': 100,
+        'name': fuzzer_name,
+        'timeout': 30,
+    }
+
+    self.app.post_json('/fuzzers/edit', request_payload)
+
+    fuzzer = data_types.Fuzzer.query(
+        data_types.Fuzzer.name == fuzzer_name).get()
+
+    self.assertEqual(
+        fuzzer.get_config_dict(), {
+            'additional_environment_string': 'args=123',
+            'data_bundle_name': 'test_bundle',
+            'external_contribution': True,
+            'executable_path': 'executable',
+            'launcher_script': 'launcher',
+            'jobs': [],
+            'max_testcases': 100,
+            'revision': 2,
+            'source': 'test@user.com',
+            'timeout': 30,
+        })
