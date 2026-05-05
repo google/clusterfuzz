618c19ffd2ad16eaf2e328ec515dc83fb931d842
Ivan Barba <ibarba@google.com>
2026-05-05 03:49:38 +0000

    Implement environment preparation in uworker preprocess

--- src/local/butler/uworker_preprocess.py
+++ src/local/butler/uworker_preprocess.py
@@ -15,34 +15,93 @@
 
 import os
 import sys
+import uuid
 
 from clusterfuzz._internal.bot.tasks import utasks
 from clusterfuzz._internal.bot.tasks.utasks import fuzz_task
 from clusterfuzz._internal.config import local_config
+from clusterfuzz._internal.datastore import data_types
 from clusterfuzz._internal.datastore import ndb_init
 from clusterfuzz._internal.system import environment
 from clusterfuzz._internal.metrics import logs
 
 
-def execute(args):
-  """Executes the uworker_preprocess command."""
+def _get_job_environment(job_name):
+  """Fetches the job entity and returns its environment variables."""
+  job = data_types.Job.query(data_types.Job.name == job_name).get()
+  if job:
+    return job.get_environment()
+  else:
+    print(f"Error: Job {job_name} not found in Datastore.")
+    sys.exit(1)
+
+
+def _get_fuzzer_environment(fuzzer_name, job_name):
+  """Fetches fuzzer entity and returns its additional environment variables."""
+  if environment.is_engine_fuzzer_job(job_name):
+    return {}
+    
+  fuzzer = data_types.Fuzzer.query(data_types.Fuzzer.name == fuzzer_name).get()
+  if not fuzzer:
+    print(f"Error: Fuzzer {fuzzer_name} not found in Datastore.")
+    sys.exit(1)
+
+    
+  additional_default_variables = ''
+  additional_variables_for_job = ''
+  
+  if fuzzer.additional_environment_string:
+    for env_variable_line in fuzzer.additional_environment_string.splitlines():
+      if '=' in env_variable_line and ':' in env_variable_line.split('=', 1)[0]:
+        fuzzer_job_name, environment_definition = env_variable_line.split(':', 1)
+        if fuzzer_job_name == job_name:
+          additional_variables_for_job += '\n%s' % environment_definition
+        continue
+      additional_default_variables += '\n%s' % env_variable
+      
+  env_string = additional_default_variables + additional_variables_for_job
+  return environment.parse_environment_definition(env_string)
+
+
+def _get_uworker_env(args):
+  """Prepares the complete environment variables for the payload."""
+  uworker_env = _get_job_environment(args.job)
+  uworker_env.update(_get_fuzzer_environment(args.fuzzer, args.job))
+  
+  # Replicate what process_command_impl does in a real tworker
+  uworker_env['TASK_NAME'] = 'fuzz'
+  uworker_env['TASK_ARGUMENT'] = args.fuzzer
+  uworker_env['JOB_NAME'] = args.job
+  
+  # Add logging metadata to be carried over to uworker_main
+  uworker_env['CF_TASK_NAME'] = 'fuzz'
+  uworker_env['CF_TASK_JOB_NAME'] = args.job
+  uworker_env['CF_TASK_ARGUMENT'] = args.fuzzer
+  uworker_env['CF_TASK_ID'] = str(uuid.uuid4())
+  
+  return uworker_env
+
+
+def _early_setup(args):
+  """Early setup needed for config and logs."""
   sys.path.insert(0, os.path.abspath(os.path.join('src', 'appengine')))
   sys.path.insert(
       0, os.path.abspath(os.path.join('src', 'appengine', 'third_party')))
   
-  os.environ['CONFIG_DIR_OVERRIDE'] = os.path.abspath(os.path.expanduser(args.config_dir))
+  environment.set_value('CONFIG_DIR_OVERRIDE', os.path.abspath(os.path.expanduser(args.config_dir)))
+  environment.set_value('LOG_TO_CONSOLE', True)
   local_config.ProjectConfig().set_environment()
-  
-  # We want to act as a tworker, so we configure logs accordingly.
   logs.configure('run_bot')
+
+
+def execute(args):
+  """Executes the uworker_preprocess command."""
+  _early_setup(args)
   
   print(f'Running preprocess for fuzzer: {args.fuzzer}, job: {args.job}')
   
   with ndb_init.context():
-    uworker_env = {}
-    
-    environment.set_value('TASK_NAME', 'fuzz')
-    environment.set_value('JOB_NAME', args.job)
+    uworker_env = _get_uworker_env(args)
     
     # tworker_preprocess expects: (module, task_argument, job_type, uworker_env)
     # For fuzz task, task_argument is fuzzer_name.
