5bd9eafe87595c57276feea92aedca8f997e86e3
Ivan Barba <ibarba@google.com>
2026-05-28 09:25:06 +0000

    Fix formatting in feature_flags.py

--- src/clusterfuzz/_internal/base/feature_flags.py
+++ src/clusterfuzz/_internal/base/feature_flags.py
@@ -41,8 +41,6 @@ class FeatureFlags(Enum):
   # TODO(b/516836939): Migrate feature flag to static config value.
   SWARMING_MAX_PENDING_TASKS = 'swarming_max_pending_tasks'
 
-
-
   @property
   def flag(self):
     """Get the feature flag."""
