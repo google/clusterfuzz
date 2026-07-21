41641d6fd5deef0a0fe2353e1b08ce521514670c
Paulo Borges <paulovlb@google.com>
2026-07-21 19:24:26 +0000

    Check by the new field and reduce nasting

--- src/clusterfuzz/_internal/base/tasks/__init__.py
+++ src/clusterfuzz/_internal/base/tasks/__init__.py
@@ -234,15 +234,18 @@ def get_regular_task(queue=None):
       return None
 
     task = get_task_from_message(messages[0], queue)
-    if task:
-      if task.command == 'fuzz' and not environment.is_tworker():
-        fuzzer = data_types.Fuzzer.query(
-            data_types.Fuzzer.name == task.argument).get()
-        if fuzzer and not fuzzer.trusted:
-          logs.warning(
-              f'Skipping untrusted fuzzer {task.argument} on long-lived bot.')
-          continue
-      return task
+    if not task:
+      continue
+
+    if task.command == 'fuzz' and not environment.is_tworker():
+      fuzzer = data_types.Fuzzer.query(
+          data_types.Fuzzer.name == task.argument).get()
+      if fuzzer and not fuzzer.trusted:
+        logs.warning(
+            f'Skipping untrusted fuzzer {task.argument} on long-lived bot.')
+        continue
+
+    return task
 
 
 def get_machine_template_for_queue(queue_name):
