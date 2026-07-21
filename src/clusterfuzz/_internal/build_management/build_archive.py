a6a2648f1dbacdbd4ba21fd3358152a1735cc9e5
Victor Liu <67168434+notvictorl@users.noreply.github.com>
2026-07-21 12:21:00 -0400

    Remove .exe on targets before appending .runtime_deps

--- src/clusterfuzz/_internal/build_management/build_archive.py
+++ src/clusterfuzz/_internal/build_management/build_archive.py
@@ -432,7 +432,12 @@ class ChromeBuildArchive(DefaultBuildArchive):
   def get_target_dependencies(
       self, fuzz_target: str) -> List[archive.ArchiveMemberInfo]:
     target_path = self.get_path_for_target(fuzz_target)
-    deps_file = f'{target_path}.runtime_deps'
+    if not target_path:
+      logs.warning(f'Target path not found for {fuzz_target}')
+      return super().get_target_dependencies(fuzz_target)
+
+    target_name = target_path.removesuffix('.exe')
+    deps_file = f'{target_name}.runtime_deps'
     if not self.file_exists(deps_file):
       logs.warning(f'runtime_deps file not found for {target_path}')
       return super().get_target_dependencies(fuzz_target)
