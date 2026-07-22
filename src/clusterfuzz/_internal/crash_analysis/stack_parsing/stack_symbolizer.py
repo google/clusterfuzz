6dafaa2924a92c1371f777ff2594015ee26ab315
Giovanni Ortuño Urquidi <ortuno@google.com>
2026-07-22 10:52:55 -0400

    symbolizer: Temporarily increase llvm-symbolizer cache size

--- src/clusterfuzz/_internal/crash_analysis/stack_parsing/stack_symbolizer.py
+++ src/clusterfuzz/_internal/crash_analysis/stack_parsing/stack_symbolizer.py
@@ -261,6 +261,8 @@ class LLVMSymbolizer(Symbolizer):
         '--inlining=%s' % stack_inlining,
     ]
     if self.system == 'darwin':
+      # TODO(crbug.com/532093354): Remove --cache-size once llvm-symbolizer is fixed.
+      cmd.append('--cache-size=5000000000')
       for hint in self.dsym_hints:
         cmd.append('--dsym-hint=%s' % hint)
 
