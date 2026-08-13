eca66e162f073886a2636526c3e06785ed65562c
Titouan Rigoudy <titouan@google.com>
2026-08-13 20:20:29 +0200

    Extract Cloud Build CI execution to local/tests/cloudbuild_ci.bash
    Avoid Cloud Build substitution parsing errors on shell environment variables
    by extracting the CI setup and test invocation into a dedicated script.
    
    TAG=agy
    CONV=1906a716-4a06-47e6-b2fb-fc2038c78ef6

---
+++ local/tests/cloudbuild_ci.bash
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+#
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+pip install pipx==1.10.0
+pipx install uv==0.12.3
+export PATH=$PATH:/root/.local/bin
+
+uv sync
+uv run butler.py bootstrap
+uv run butler.py lint
+uv run local/tests/run_all_tests
