9a644e5244881ce17deb78cead18b858257f7fc9
Ivan Barba <ibarba@google.com>
2026-08-28 23:48:47 +0000

    [Dev deployments] allow upload of custom images to custom projects

--- docker/build.sh
+++ docker/build.sh
@@ -32,6 +32,9 @@ IMAGES=(
 VERSION_TAG="latest"
 GIT_HASH_ARG=""
 PUSH="true"
+SKIP_LATEST_TAG="false"
+TARGET_IMAGE=""
+REGISTRY_PREFIX=${REGISTRY_PREFIX:-"gcr.io/clusterfuzz-images"}
 NEEDS_ROOT_PIPFILE=false
 
 # Set up a trap to clean up Pipfiles on exit.
@@ -57,18 +60,41 @@ fi
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --no-push) PUSH="false";;
+        --skip-latest-tag) SKIP_LATEST_TAG="true";;
+        --image) TARGET_IMAGE="$2"; shift;;
+        --image=*) TARGET_IMAGE="${1#*=}";;
+        --registry-prefix) REGISTRY_PREFIX="$2"; shift;;
+        --registry-prefix=*) REGISTRY_PREFIX="${1#*=}";;
+        --project) REGISTRY_PREFIX="gcr.io/$2"; shift;;
+        --project=*) REGISTRY_PREFIX="gcr.io/${1#*=}";;
         "") ;; # Ignore empty arguments, which can be passed by Cloud Build.
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
+if [ -n "$TARGET_IMAGE" ]; then
+  FILTERED_IMAGES=()
+  for img in "${IMAGES[@]}"; do
+    if [[ "$img" == *"$TARGET_IMAGE"* ]]; then
+      FILTERED_IMAGES+=("$img")
+    fi
+  done
+  if [ ${#FILTERED_IMAGES[@]} -eq 0 ]; then
+    echo "No images matching target '$TARGET_IMAGE'"
+    exit 1
+  fi
+  IMAGES=("${FILTERED_IMAGES[@]}")
+fi
+
 function docker_push {
   if [ "$PUSH" == "true" ]; then
-    docker push "$image_with_version_tag"
+    if [ "$SKIP_LATEST_TAG" != "true" ]; then
+      docker push "$image_with_version_tag"
+    fi
     docker push "$image_with_stamp"
   else
-    echo "Skipping push for $image_with_version_tag."
+    echo "Skipping push for $image_with_stamp."
   fi
 }
 
@@ -85,8 +111,9 @@ else
   stamp="$VERSION_TAG-$GIT_HASH-$DATE_STAMP"
 fi
 
-for image_name in "${IMAGES[@]}"; do
-  image_dir=${image_name#gcr.io/clusterfuzz-images/}
+for image_path in "${IMAGES[@]}"; do
+  image_dir=${image_path#gcr.io/clusterfuzz-images/}
+  image_name="$REGISTRY_PREFIX/$image_dir"
   
   if [ "$VERSION_TAG" == "latest" ]; then
     dockerfile="$image_dir/Dockerfile"
@@ -102,14 +129,18 @@ for image_name in "${IMAGES[@]}"; do
   image_with_version_tag="$image_name:$VERSION_TAG"
   image_with_stamp="$image_name:$stamp"
 
-  # Copy Pipfile to base for the ubuntu-24.04 build, as it's
-  # needed but not in the build context.
-  if [[ "$image_dir" == "base" && "$dockerfile" == *"ubuntu-24-04"* ]]; then
+  # Copy Pipfile to base as it's needed but not in the base build context.
+  if [[ "$image_dir" == "base" ]]; then
     NEEDS_ROOT_PIPFILE=true
     cp ../Pipfile ../Pipfile.lock base/
   fi
 
-  docker build -t "$image_with_version_tag" -f "$dockerfile" "$image_dir"
+  if [ "$SKIP_LATEST_TAG" == "true" ]; then
+    docker build -t "$image_with_stamp" -f "$dockerfile" "$image_dir"
+  else
+    docker build -t "$image_with_version_tag" -f "$dockerfile" "$image_dir"
+    docker tag "$image_with_version_tag" "$image_with_stamp"
+  fi
 
   # Clean up the copied files.
   if [[ "$NEEDS_ROOT_PIPFILE" == "true" ]]; then
@@ -117,7 +148,6 @@ for image_name in "${IMAGES[@]}"; do
     NEEDS_ROOT_PIPFILE=false
   fi
 
-  docker tag "$image_with_version_tag" "$image_with_stamp"
   docker_push
 done
 
